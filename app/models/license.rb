8e7204e4e1097ba34fbd260e491ef3337525645d
ezekg <ezekg@yahoo.com>
2016-05-23 09:56:53 -0500

    ensure serialized data is an array

--- app/models/license.rb
+++ app/models/license.rb
@@ -1,5 +1,5 @@
 class License < ApplicationRecord
   belongs_to :user
   belongs_to :policy
-  serialize :active_machines
+  serialize :active_machines, Array
 end
--- app/models/policy.rb
+++ app/models/policy.rb
@@ -1,5 +1,5 @@
 class Policy < ApplicationRecord
   belongs_to :account
   has_many :licenses
-  serialize :pool
+  serialize :pool, Array
 end
