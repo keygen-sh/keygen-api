08cec308ec7508d22e063aaea47d146f553d5437
ezekg <ezekg@yahoo.com>
2016-05-29 21:48:16 -0500

    add auth tokens to users

---
+++ app/models/concerns/tokenable.rb
@@ -0,0 +1,28 @@
+module Tokenable
+  extend ActiveSupport::Concern
+
+  included do
+    before_create :create_auth_tokens!
+  end
+
+  def reset_auth_tokens!
+    create_auth_tokens!
+    save
+  end
+
+  protected
+
+  def generate_token(token_name)
+    loop do
+      token = SecureRandom.hex
+      break token unless self.class.exists? "#{token_name}": token
+    end
+  end
+
+  private
+
+  def create_auth_tokens!
+    self.auth_token       = generate_token :auth_token
+    self.reset_auth_token = generate_token :reset_auth_token
+  end
+end
---
+++ db/migrate/20160530024204_add_auth_token_to_users.rb
@@ -0,0 +1,6 @@
+class AddAuthTokenToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :auth_token, :string
+    add_column :users, :reset_auth_token, :string
+  end
+end
