ae47da3d848a547cbfa26fb4515fd194040f859f
ezekg <ezekg@yahoo.com>
2016-06-01 10:09:28 -0500

    add optimistic locking to policies for license pool

--- app/models/policy.rb
+++ app/models/policy.rb
@@ -2,4 +2,26 @@ class Policy < ApplicationRecord
   belongs_to :account
   has_many :licenses
   serialize :pool, Array
+
+  def pop
+    begin
+      return nil if pool.empty?
+      v = pool.pop
+      self.save!
+      return v
+    rescue ActiveRecord::StaleObjectError
+      self.reload
+      retry
+    end
+  end
+
+  def <<(license_key)
+    begin
+      pool << license_key
+      self.save!
+    rescue ActiveRecord::StaleObjectError
+      self.reload
+      retry
+    end
+  end
 end
---
+++ db/migrate/20160601145430_add_lock_version_to_policies.rb
@@ -0,0 +1,5 @@
+class AddLockVersionToPolicies < ActiveRecord::Migration[5.0]
+  def change
+    add_column :policies, :lock_version, :integer, default: 0, null: false
+  end
+end
