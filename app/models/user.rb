2e6f5e675c2f30609fa267676601ead93d6f9a6c
ezekg <ezekg@yahoo.com>
2016-06-04 21:14:56 -0500

    ensure validations are scoped by account

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
     presence: true,
     length: { maximum: 255 },
     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
-    uniqueness: { case_sensitive: false }
+    uniqueness: { case_sensitive: false, scope: :account_id }
 
   def architect?
     self.role == "architect"
