bbaa4b0ede3a307456848b50f7a08f54c5a93645
ezekg <ezekg@yahoo.com>
2016-06-04 21:03:37 -0500

    add validations to account model

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,7 +4,15 @@ class Account < ApplicationRecord
   has_many :users
   has_one :billing, as: :customer
 
+  before_save -> { self.subdomain = subdomain.downcase }
+
   validates :name, presence: true
+  validates :subdomain,
+    presence: true,
+    exclusion: { in: %w(www api), message: "%{value} is reserved." },
+    uniqueness: { case_sensitive: false },
+    format: { with: /\A[\w_]+\Z/i },
+    length: { maximum: 255 }
 
   def admins
     self.users.select &:admin?
