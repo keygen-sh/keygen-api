7f97c60b35aa0a835a6b8d8db4d0473c048c4066
ezekg <ezekg@yahoo.com>
2016-06-04 22:17:12 -0500

    before_filter->before_action

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,7 +1,6 @@
 module Api::V1
   class ProductsController < ApiController
-    before_filter :set_current_account
-
+    before_action :set_current_account
     before_action :set_product, only: [:show, :update, :destroy]
 
     # GET /products
