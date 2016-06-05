1c770c2b4de47c9cf127ce98d525cc4f6c31ac30
ezekg <ezekg@yahoo.com>
2016-06-04 22:18:01 -0500

    remove plans and accounts endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  scope module: 'api' do
+  scope module: "api" do
     namespace :v1 do
-      resources :plans
-      resources :accounts
+      # resources :plans
+      # resources :accounts
       resources :products
       resources :users
       resources :policies
