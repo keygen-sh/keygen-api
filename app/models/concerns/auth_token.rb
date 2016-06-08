aff492f1f51cd37208b53f782bcfb18413268a20
ezekg <ezekg@yahoo.com>
2016-06-07 21:40:24 -0500

    use bang methods only when saving

--- app/models/concerns/auth_token.rb
+++ app/models/concerns/auth_token.rb
@@ -2,11 +2,11 @@ module AuthToken
   extend ActiveSupport::Concern
 
   included do
-    before_create :create_auth_tokens!
+    before_create :create_auth_tokens
   end
 
   def reset_auth_tokens!
-    create_auth_tokens!
+    create_auth_tokens
     save
   end
 
@@ -21,7 +21,7 @@ module AuthToken
 
   private
 
-  def create_auth_tokens!
+  def create_auth_tokens
     self.auth_token       = generate_token :auth_token
     self.reset_auth_token = generate_token :reset_auth_token
   end
