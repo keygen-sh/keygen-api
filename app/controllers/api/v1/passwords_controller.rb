3d6a4762aa9da2ea67e3140baccc11e0f663d253
ezekg <ezekg@yahoo.com>
2016-06-07 20:07:31 -0500

    add password reset controller and endpoint

---
+++ app/controllers/api/v1/passwords_controller.rb
@@ -0,0 +1,21 @@
+module Api::V1
+  class PasswordsController < BaseController
+    scope_by_subdomain
+
+    before_action :set_user, only: [:reset]
+
+    def reset
+      @user.send_password_reset if @user
+    end
+
+    private
+
+    def set_user
+      @user = @current_account.users.find_by_email email_params
+    end
+
+    def email_params
+      params.require :email
+    end
+  end
+end
