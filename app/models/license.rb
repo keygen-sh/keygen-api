bdeb5071705cfb3b0be3f33b647108bc6d98b685
ezekg <ezekg@yahoo.com>
2016-06-10 09:25:28 -0500

    refactor license validation

--- app/models/license.rb
+++ app/models/license.rb
@@ -13,6 +13,13 @@ class License < ApplicationRecord
     uniqueness: { scope: :policy_id }
 
   def license_valid?
-    (expiry.nil? || expiry > DateTime.now) && active_machines.length
+    # Check if license is expired
+    return false unless expiry.nil? || expiry > DateTime.now
+    # Check if license allows floating and if not, should have single activation
+    return true if policy.floating && active_machines.length == 1
+    # Assume floating, should have at least 1 activation but no more than policy allows
+    return true if active_machines.length >= 1 && active_machines.length <= policy.max_activations
+    # Otherwise, assume invalid
+    return false
   end
 end
