35ec4902024bbfb7f3dbe04d07fc3c6777ee9e08
ezekg <ezekg@yahoo.com>
2016-06-10 23:01:41 -0500

    fix user actions base controller

--- app/controllers/api/v1/users/actions/password_controller.rb
+++ app/controllers/api/v1/users/actions/password_controller.rb
@@ -1,5 +1,5 @@
 module Api::V1::Users::Actions
-  class PasswordController < BaseController
+  class PasswordController < Api::V1::BaseController
     before_action :scope_by_subdomain!
     before_action :authenticate_with_token!, only: [:update_password]
     before_action :set_user, only: [:update_password, :reset_password]
