1f3730db3a9bf323bb73e63d81f5d650a5cc5725
ezekg <ezekg@yahoo.com>
2016-06-10 22:51:42 -0500

    add authorization to plans

--- app/controllers/api/v1/plans_controller.rb
+++ app/controllers/api/v1/plans_controller.rb
@@ -1,23 +1,27 @@
 module Api::V1
   class PlansController < Api::V1::BaseController
-    before_action :authenticate_with_token!
+    before_action :authenticate_with_token!, only: [:create, :update, :destroy]
     before_action :set_plan, only: [:show, :update, :destroy]
 
     # GET /plans
     def index
       @plans = Plan.all
+      authorize @plans
 
       render json: @plans
     end
 
     # GET /plans/1
     def show
+      authorize @plan
+
       render json: @plan
     end
 
     # POST /plans
     def create
       @plan = Plan.new plan_params
+      authorize @plan
 
       if @plan.save
         render json: @plan, status: :created, location: v1_plan_url(@plan)
@@ -28,6 +32,8 @@ module Api::V1
 
     # PATCH/PUT /plans/1
     def update
+      authorize @plan
+
       if @plan.update(plan_params)
         render json: @plan
       else
@@ -37,6 +43,8 @@ module Api::V1
 
     # DELETE /plans/1
     def destroy
+      authorize @plan
+
       @plan.destroy
     end
 
