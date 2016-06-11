3aadde258a29c9e53e76ca09e0c7a4eb7a64bdab
ezekg <ezekg@yahoo.com>
2016-06-10 23:53:10 -0500

    remove accounts from plans output

--- app/serializers/plan_serializer.rb
+++ app/serializers/plan_serializer.rb
@@ -2,8 +2,6 @@ class PlanSerializer < BaseSerializer
   attributes :id, :name, :price, :max_products, :max_users, :max_policies,
              :max_licenses
 
-  has_many :accounts
-
   def id
     object.hashid
   end
