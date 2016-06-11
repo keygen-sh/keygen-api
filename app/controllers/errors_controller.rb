02a8ef6b0bb5e7211886cfeffe1a46b637ed67d6
ezekg <ezekg@yahoo.com>
2016-06-10 23:50:30 -0500

    add global error handling

---
+++ app/controllers/errors_controller.rb
@@ -0,0 +1,15 @@
+class ErrorsController < ApplicationController
+  STATUS_CODES = Rack::Utils::HTTP_STATUS_CODES
+
+  before_action :set_status
+
+  def show
+    skip_authorization
+
+    self.send "render_#{@status}"
+  end
+
+  def set_status
+    @status = STATUS_CODES[params[:code].to_i || 500].parameterize.underscore
+  end
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,7 +10,10 @@ Rails.application.configure do
   config.eager_load = false
 
   # Show full error reports.
-  config.consider_all_requests_local = true
+  config.consider_all_requests_local = false
+
+  # Route exceptions to error controller.
+  config.exceptions_app = self.routes
 
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
