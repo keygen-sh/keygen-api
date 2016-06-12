713b8ebc66aba93d137838bf682c67305be77657
ezekg <ezekg@yahoo.com>
2016-06-12 14:27:48 -0500

    fix license validity check
    - closes #9

--- app/models/license.rb
+++ app/models/license.rb
@@ -17,9 +17,9 @@ class License < ApplicationRecord
     # Check if license is expired
     return false unless expiry.nil? || expiry > DateTime.now
     # Check if license allows floating and if not, should have single activation
-    return true if policy.floating && active_machines.length == 1
+    return true if !policy.floating && active_machines.length == 1
     # Assume floating, should have at least 1 activation but no more than policy allows
-    return true if active_machines.length >= 1 && active_machines.length <= policy.max_activations
+    return true if policy.floating && active_machines.length >= 1 && active_machines.length <= policy.max_activations
     # Otherwise, assume invalid
     return false
   end
