feb23075a0822e729f8e288b5a4c67f3b4603148
ezekg <ezekg@yahoo.com>
2016-06-13 08:53:02 -0500

    fix account activation check

--- app/controllers/concerns/account_scope.rb
+++ app/controllers/concerns/account_scope.rb
@@ -1,22 +1,18 @@
 module AccountScope
   extend ActiveSupport::Concern
 
-  included do
-    before_action :verify_account_is_activated
-  end
-
   def scope_by_subdomain!
     @current_account = Account.find_by_subdomain! request.subdomains.first
+
+    if @current_account.activated?
+      @current_account
+    else
+      render_forbidden({
+        title: "Current account is not activated",
+        detail: "You must activate your account before accessing this resource"
+      })
+    end
   rescue ActiveRecord::RecordNotFound
     render_not_found detail: "The requested resource requires a valid account subdomain"
-  end
-
-  private
-
-  def verify_account_is_activated
-    if @current_account && !@current_account.activated?
-      render_forbidden title: "Current account is not activated",
-        detail: "Please activate your account before accessing this resource"
-    end
   end
 end
