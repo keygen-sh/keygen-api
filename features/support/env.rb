4d65e98d07a80bde061797d98de6006a360f29d0
ezekg <ezekg@yahoo.com>
2016-06-13 10:49:10 -0500

    add cucumber for testing

---
+++ features/support/env.rb
@@ -0,0 +1,10 @@
+require 'cucumber/rails'
+require 'cucumber/api_steps'
+
+ActionController::Base.allow_rescue = false
+
+begin
+  DatabaseCleaner.strategy = :transaction
+rescue NameError
+  raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
+end
