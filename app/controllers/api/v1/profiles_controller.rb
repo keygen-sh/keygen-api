fe80dfc048c5b57356129dadee4b80c1610f3976
ezekg <ezekg@yahoo.com>
2016-06-25 16:13:11 -0500

    simplify profile controller

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -2,19 +2,12 @@ module Api::V1
   class ProfilesController < Api::V1::BaseController
     before_action :scope_by_subdomain!
     before_action :authenticate_with_token!
-    before_action :set_user, only: [:show]
 
     # GET /profile
     def show
-      authorize @user
+      authorize @current_user
 
-      render json: @user
-    end
-
-    private
-
-    def set_user
-      @user = @current_user
+      render json: @current_user
     end
   end
 end
