3bbf2c6ebd4fd64b3b3657ce0a8344a4c5710152
ezekg <ezekg@yahoo.com>
2016-08-09 10:38:41 -0500

    add product name validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,6 +8,7 @@ class Product < ApplicationRecord
 
   validates_associated :account, message: -> (_, obj) { obj[:value].errors.full_messages.first.downcase }
   validates :account, presence: { message: "must exist" }
+  validates :name, presence: true
 
   scope :page, -> (page = {}) {
     paginate(page[:number]).per page[:size]
