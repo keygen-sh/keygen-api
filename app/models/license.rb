3e6b70dcf824598cf9b5bc6bdf507f1a59ed7dd7
ezekg <ezekg@yahoo.com>
2016-08-23 17:14:49 -0500

    add validation for strict license policies

--- app/models/license.rb
+++ app/models/license.rb
@@ -33,7 +33,9 @@ class License < ApplicationRecord
   def license_valid?
     # Check if license is expired
     return false unless expiry.nil? || expiry > DateTime.now
-    # Check if license allows floating and if not, should have single activation
+    # Check if license policy is strict, e.g. enforces reporting of machine usage
+    return true unless policy.strict
+    # Check if license policy allows floating and if not, should have single activation
     return true if !policy.floating && machines.count == 1
     # Assume floating, should have at least 1 activation but no more than policy allows
     return true if policy.floating && machines.count >= 1 && machines.count <= policy.max_activations
