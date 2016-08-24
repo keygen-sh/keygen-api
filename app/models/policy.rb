0be86a1e7a73ea5d0fae671a68a2e30f08c9aa84
ezekg <ezekg@yahoo.com>
2016-08-24 08:55:15 -0500

    remove implicit foreign key

--- app/models/policy.rb
+++ app/models/policy.rb
@@ -2,7 +2,7 @@ class Policy < ApplicationRecord
   belongs_to :account
   belongs_to :product
   has_many :licenses, dependent: :destroy
-  has_many :pool, foreign_key: "policy_id", class_name: "Key", dependent: :destroy
+  has_many :pool, class_name: "Key", dependent: :destroy
 
   validates_associated :account, message: -> (_, obj) { obj[:value].errors.full_messages.first.downcase }
   validates :account, presence: { message: "must exist" }
