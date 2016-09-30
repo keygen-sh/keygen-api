962369a8ca4ef741ea5f29c02f10f5554d6c34ab
ezekg <ezekg@yahoo.com>
2016-09-30 11:07:53 -0500

    add validation on role names

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,12 +1,9 @@
 class Role < ApplicationRecord
   has_and_belongs_to_many :tokens, join_table: :tokens_roles
-  belongs_to :resource,
-             polymorphic: true,
-             optional: true
+  belongs_to :resource, polymorphic: true, optional: true
 
-  validates :resource_type,
-            inclusion: { in: Rolify.resource_types },
-            allow_nil: true
+  validates :resource_type, inclusion: { in: Rolify.resource_types }, allow_nil: true
+  validates :name, inclusion: { in: %w[admin user product], message: "%{value} is not a valid role" }
 
   scopify
 end
