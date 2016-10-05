541ed8b1342d3ff7a754225a93b6a623e507294b
ezekg <ezekg@yahoo.com>
2016-10-05 10:59:41 -0500

    clean up password reset logic

--- app/models/concerns/password_resetable.rb
+++ app/models/concerns/password_resetable.rb
@@ -2,21 +2,11 @@ module PasswordResetable
   extend ActiveSupport::Concern
   include Tokenable
 
-  included do
-    before_create :create_password_reset_token
-  end
-
   def send_password_reset
     self.password_reset_sent_at = Time.zone.now
-    create_password_reset_token
+    self.password_reset_token   = generate_token :password_reset_token
     save
 
     UserMailer.password_reset(self).deliver_later
-  end
-
-  private
-
-  def create_password_reset_token
-    self.password_reset_token = generate_token :password_reset_token
   end
 end
