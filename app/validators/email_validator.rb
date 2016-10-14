7960dee239a75a7ff5aa487997f3275874042b86
ezekg <ezekg@yahoo.com>
2016-10-14 16:34:18 -0500

    add email validator

---
+++ app/validators/email_validator.rb
@@ -0,0 +1,13 @@
+class EmailValidator < ActiveModel::EachValidator
+
+  def validate_each(record, attribute, value)
+    record.errors.add attribute, "must be a valid email" unless valid_email?(value)
+  end
+
+  private
+
+  def valid_email?(value)
+    return false if value.nil?
+    value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  end
+end
