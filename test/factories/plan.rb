ec91a7058e398b4ad5533d2dc6e8769827f7fae6
ezekg <ezekg@yahoo.com>
2016-10-16 18:19:10 -0500

    increase max resource limits for fixtures

--- test/factories/plan.rb
+++ test/factories/plan.rb
@@ -2,9 +2,9 @@ FactoryGirl.define do
   factory :plan do
     name { Faker::Company.buzzword }
     price { Faker::Number.number(4) }
-    max_users { Faker::Number.between(1, 5000) }
-    max_policies { Faker::Number.between(1, 5000) }
-    max_licenses { Faker::Number.between(1, 5000) }
-    max_products { Faker::Number.between(1, 5000) }
+    max_users { Faker::Number.between(50, 5000) }
+    max_policies { Faker::Number.between(50, 5000) }
+    max_licenses { Faker::Number.between(50, 5000) }
+    max_products { Faker::Number.between(50, 5000) }
   end
 end
