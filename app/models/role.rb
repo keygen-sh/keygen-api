7b891bd394c1dc614d740587e4275266bd78f54a
ezekg <ezekg@yahoo.com>
2016-10-17 09:00:08 -0500

    extract allowed roles into constant

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,9 +1,11 @@
 class Role < ApplicationRecord
+  ALLOWED_ROLES = %w[admin user product].freeze
+
   has_and_belongs_to_many :tokens, join_table: :tokens_roles
   belongs_to :resource, polymorphic: true, optional: true
 
   validates :resource_type, inclusion: { in: Rolify.resource_types }, allow_nil: true
-  validates :name, inclusion: { in: %w[admin user product], message: "%{value} is not a valid role" }
+  validates :name, inclusion: { in: ALLOWED_ROLES, message: "%{value} is not a valid role" }
 
   scopify
 end
