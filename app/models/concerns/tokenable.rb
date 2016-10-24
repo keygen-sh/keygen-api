a52eabbf145ab598c6e038bd8d3067c17578f01e
ezekg <ezekg@yahoo.com>
2016-10-24 12:57:35 -0500

    add protection against token timing attacks

--- app/models/concerns/tokenable.rb
+++ app/models/concerns/tokenable.rb
@@ -13,15 +13,32 @@ module Tokenable
     loop do
       raw = SecureRandom.hex 32
       raw = yield raw if block_given?
-      enc = BCrypt::Password.create raw
+      enc = ::BCrypt::Password.create raw
       break [raw, enc] unless self.class.exists? attribute => enc
     end
   end
 
   def compare_encrypted_token(attribute, token)
-    enc = BCrypt::Password.new self.send(attribute)
-    enc == token
+    hashed_token = self.send attribute
+    bcrypt = ::BCrypt::Password.new hashed_token
+    token = ::BCrypt::Engine.hash_secret token, bcrypt.salt
+
+    secure_compare token, hashed_token
   rescue
     false
+  end
+
+  private
+
+  # Constant-time comparison algorithm to prevent timing attacks
+  # @see https://github.com/plataformatec/devise/blob/88724e10adaf9ffd1d8dbfbaadda2b9d40de756a/lib/devise/encryptor.rb?ts=2#L12
+  # @see https://github.com/plataformatec/devise/blob/7b000390a066d89b9cc474b22aa8afff6f5c85b7/lib/devise.rb?ts=2#L485
+  def secure_compare(a, b)
+    return false if a.blank? || b.blank? || a.bytesize != b.bytesize
+    l = a.unpack "C#{a.bytesize}"
+
+    res = 0
+    b.each_byte { |byte| res |= byte ^ l.shift }
+    res == 0
   end
 end
