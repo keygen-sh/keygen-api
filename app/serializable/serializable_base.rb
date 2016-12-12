98586cf0ad70fd7be57479a87c95202777746b8e
ezekg <ezekg@yahoo.com>
2016-12-12 09:51:46 -0600

    update key transforms and mime-types for jsonapi compat
    - related to #58

--- app/serializable/serializable_base.rb
+++ app/serializable/serializable_base.rb
@@ -1,3 +1,9 @@
+require "jsonapi/serializable/resource/key_transform"
+
 class SerializableBase < JSONAPI::Serializable::Resource
+  prepend JSONAPI::Serializable::Resource::KeyTransform
+
+  self.key_transform = -> (key) { key.to_s.camelize :lower }
+
   id { @object.hashid }
 end
--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,4 +1,6 @@
-# Be sure to restart your server when you modify this file.
-
-# Add new mime types for use in respond_to blocks:
-# Mime::Type.register "text/richtext", :rtf
+Mime::Type.unregister :json
+Mime::Type.register "application/vnd.api+json", :json, %W[
+  application/vnd.api+json
+  application/json
+  text/x-json
+]
