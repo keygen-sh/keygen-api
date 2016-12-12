9ac9015f3c5d45cc0f9de81ec72e53bfb503083e
ezekg <ezekg@yahoo.com>
2016-12-12 09:51:46 -0600

    update key transforms and mime-types for jsonapi compat
    - related to #58

--- config/initializers/json_param_key_transform.rb
+++ config/initializers/json_param_key_transform.rb
@@ -1,12 +1,15 @@
-require "active_model_serializers/key_transform"
+# Transform JSON request param keys from camelCase to snake_case
 
-# Transform JSON request param keys from JSON-conventional camelCase to
-# Rails-conventional snake_case
-ActionDispatch::Request.parameter_parsers[:json] = lambda { |raw_post|
-  # Modified from action_dispatch/http/parameters.rb
+# Modified from action_dispatch/http/parameters.rb
+parser = -> (raw_post) {
   data = ActiveSupport::JSON.decode raw_post
-  data = { _json: data } unless data.is_a?(Hash)
-
-  # Transform camelCase param keys to snake_case:
+  data = { _json: data } unless data.is_a? Hash
   data.deep_transform_keys! &:underscore
+  data.with_indifferent_access
 }
+
+# Redefine parameter parser for JSON mime-type
+ActionDispatch::Request.parameter_parsers[:json] = parser
+
+# Redefine parameter parser for JSONAPI mime-type
+JSONAPI::Rails::Railtie::PARSER = parser
