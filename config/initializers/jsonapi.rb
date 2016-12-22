cd4695631964f1505e4a744b1ce3f3bf7ae3db1f
ezekg <ezekg@yahoo.com>
2016-12-22 10:11:22 -0600

    update dependencies (also use my proposed fork of jsonapi)

---
+++ config/initializers/jsonapi.rb
@@ -0,0 +1,21 @@
+# Register JSON API mime-type
+Mime::Type.register "application/vnd.api+json", :jsonapi, %W[
+  application/vnd.api+json
+  application/json
+  text/x-json
+]
+
+# Register parameter parser for JSON API mime-type to transform param keys from
+# camel case to snake case
+ActionDispatch::Request.parameter_parsers[:jsonapi] = -> (raw_post) {
+  data = ActiveSupport::JSON.decode raw_post
+  data = { _json: data } unless data.is_a? Hash
+  data.deep_transform_keys! &:underscore
+  data.with_indifferent_access
+}
+
+# Make sure jsonapi/rails doesn't overwrite our registrations
+JSONAPI::Rails.configure do |config|
+  config.register_parameter_parser = false
+  config.register_mime_type = false
+end
