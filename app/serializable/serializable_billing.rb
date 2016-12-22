42d0a69c884408f11085152c062d3e1aa105b08a
ezekg <ezekg@yahoo.com>
2016-12-21 23:30:24 -0600

    fix serialized billings link path

--- app/serializable/serializable_billing.rb
+++ app/serializable/serializable_billing.rb
@@ -29,6 +29,6 @@ class SerializableBilling < SerializableBase
   end
 
   link :self do
-    @url_helpers.v1_account_key_path @object.account, @object
+    @url_helpers.billing_v1_account_path @object.account
   end
 end
