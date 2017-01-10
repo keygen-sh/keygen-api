bb9eda75c0f07a681b7e9f4f654dd364b776880b
ezekg <ezekg@yahoo.com>
2017-01-09 19:22:43 -0600

    add challenge for lets encrypt cert

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # Lets Encrypt cert challenge
+  get ".well-known/acme-challenge/#{ENV["LETS_ENCRYPT_CHALLENGE"]}", to: -> (*) { [200, {}, [ENV["LETS_ENCRYPT_ANSWER"]]] }
+
   # constraints -> (req) { req.format.symbol != :jsonapi } do
   #   match "*bad_request", to: "errors#show", code: 400, via: [:get, :post, :patch, :put, :delete, :head, :options]
   # end
