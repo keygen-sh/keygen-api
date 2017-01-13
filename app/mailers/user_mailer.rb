1d5309479baa93f1474027bf65ee5b44cfb7a346
ezekg <ezekg@yahoo.com>
2017-01-13 16:24:46 -0600

    update default from name for user-facing emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,9 @@
 class UserMailer < ApplicationMailer
+  default from: "Keygen Support <hello@keygen.sh>"
 
   def password_reset(user:, token:)
     @user, @token = user, token
 
-    mail to: user.email, subject: "Password reset requested"
+    mail to: user.email, subject: "Password reset requested for Keygen account"
   end
 end
