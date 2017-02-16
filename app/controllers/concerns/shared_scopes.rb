5885c757c96fa62a83df42c526748322a6a1fa56
ezekg <ezekg@yahoo.com>
2017-02-16 15:03:04 -0600

    update sort order for resource indexes

--- app/controllers/concerns/shared_scopes.rb
+++ app/controllers/concerns/shared_scopes.rb
@@ -10,6 +10,15 @@ module SharedScopes
       end
     end
 
+    has_scope :order, default: "desc", only: :index do |controller, resource, sort|
+      case sort.to_s.upcase
+      when "DESC"
+        resource.reorder "created_at DESC"
+      when "ASC"
+        resource.reorder "created_at ASC"
+      end
+    end
+
     # Kaminari's pagination will override any limit that was set previously,
     # so we're placing it after the limit scope.
     has_scope :page, type: :hash, using: [:number, :size], only: :index
