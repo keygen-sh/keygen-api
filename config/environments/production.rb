730a17fc613a483d3355b663f69887d3dc3e3e2a
ezekg <ezekg@yahoo.com>
2017-02-17 11:55:12 -0600

    add memery cache store to prod env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,7 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  config.cache_store = :memory_store
 
   # Route exceptions to error controller.
   config.exceptions_app = self.routes
