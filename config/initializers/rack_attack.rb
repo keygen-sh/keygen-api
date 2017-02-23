41b4dcc887358a94ed45e4c5c5368176bd91c7f0
ezekg <ezekg@yahoo.com>
2017-02-23 10:18:50 -0600

    update rate limiting to be a bit more lenient

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -9,17 +9,17 @@ class Rack::Attack
     '127.0.0.1' == req.ip || '::1' == req.ip
   end
 
-  # Allow an IP address to make 5 requests every 5 seconds
-  throttle('req/ip', limit: 5, period: 5) do |req|
+  # Allow an IP address to make 5 requests every 1 second
+  throttle('req/ip', limit: 5, period: 1.second) do |req|
     req.ip
   end
 
   # Send the following response to throttled clients
-  self.throttled_response = ->(env) {
-    retry_after = (env['rack.attack.match_data'] || {})[:period]
+  self.throttled_response = -> (env) {
+    retry_after = (env['rack.attack.match_data'] || {})[:period].to_i rescue 0
     [
       429,
-      {'Content-Type' => 'application/vnd.api+json', 'Retry-After' => retry_after.to_s},
+      { 'Content-Type' => 'application/vnd.api+json', 'Retry-After' => retry_after.to_s },
       [{
         'errors': [{
           'title': 'Throttle limit reached',
