648c2bb5ac5e9b7c846c7a7ce2edbfcf5b16fd12
ezekg <ezekg@yahoo.com>
2017-02-27 12:35:25 -0600

    add reserved slugs to accounts
    - closes #104

--- app/models/concerns/sluggable.rb
+++ app/models/concerns/sluggable.rb
@@ -1,6 +1,8 @@
 module Sluggable
   extend ActiveSupport::Concern
 
+  EXCLUDED_SLUGS = %w[actions action].freeze
+
   included do
     # Redefine finder to search by slug and id
     def self.find(id)
