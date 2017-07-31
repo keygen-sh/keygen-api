b9a707c353a8c7256ebd4c49ec2be30421f1476a
ezekg <ezekg@yahoo.com>
2017-07-31 12:02:46 -0500

    send from support@keygen.sh instead of personal email

--- app/mailers/account_mailer.rb
+++ app/mailers/account_mailer.rb
@@ -1,5 +1,5 @@
 class AccountMailer < ApplicationMailer
-  default from: "Zeke from Keygen <zeke@keygen.sh>"
+  default from: "Keygen <support@keygen.sh>"
   layout "account_mailer"
 
   def pricing_change(account:)
