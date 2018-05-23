535799d570cc7413388aab06aa5aedb1af4417dc
ezekg <ezekg@yahoo.com>
2018-05-23 09:49:51 -0500

    remove annotate gem

--- app/models/role.rb
+++ app/models/role.rb
@@ -4,21 +4,3 @@ class Role < ApplicationRecord
   validates :name, inclusion: { in: %w[user admin], message: "must be a valid user role" }, if: -> { resource.is_a? User }
   validates :name, inclusion: { in: %w[product], message: "must be a valid product role" }, if: -> { resource.is_a? Product }
 end
-
-# == Schema Information
-#
-# Table name: roles
-#
-#  id            :uuid             not null, primary key
-#  name          :string
-#  resource_type :string
-#  created_at    :datetime
-#  updated_at    :datetime
-#  resource_id   :uuid
-#
-# Indexes
-#
-#  index_roles_on_id_and_created_at                             (id,created_at) UNIQUE
-#  index_roles_on_name_and_created_at                           (name,created_at)
-#  index_roles_on_resource_id_and_resource_type_and_created_at  (resource_id,resource_type,created_at)
-#
--- app/models/webhook_endpoint.rb
+++ app/models/webhook_endpoint.rb
@@ -7,19 +7,3 @@ class WebhookEndpoint < ApplicationRecord
   validates :account, presence: { message: "must exist" }
   validates :url, url: { protocols: %w[https] }, presence: true
 end
-
-# == Schema Information
-#
-# Table name: webhook_endpoints
-#
-#  id         :uuid             not null, primary key
-#  url        :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  account_id :uuid
-#
-# Indexes
-#
-#  index_webhook_endpoints_on_account_id_and_created_at         (account_id,created_at)
-#  index_webhook_endpoints_on_id_and_created_at_and_account_id  (id,created_at,account_id) UNIQUE
-#
