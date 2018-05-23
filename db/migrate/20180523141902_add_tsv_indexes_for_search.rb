b733ca7de185158968eed48b1a56de9d1dce15b4
ezekg <ezekg@yahoo.com>
2018-05-23 10:31:42 -0500

    update to use tsv index expressions instead of triggers

---
+++ db/migrate/20180523141902_add_tsv_indexes_for_search.rb
@@ -0,0 +1,20 @@
+class AddTsvIndexesForSearch < ActiveRecord::Migration[5.0]
+  INDEXES = {
+    products: %i[id name metadata],
+    policies: %i[id name metadata],
+    licenses: %i[id key metadata],
+    machines: %i[id fingerprint name metadata],
+    users: %i[id email first_name last_name metadata],
+    keys: %i[id key]
+  }
+
+  def change
+    INDEXES.each do |table, columns|
+      columns.each do |column|
+        add_index table, %[to_tsvector('pg_catalog.simple', coalesce(#{column}::TEXT, ''))],
+          name: "#{table}_tsv_#{column}_idx",
+          using: :gin
+      end
+    end
+  end
+end
