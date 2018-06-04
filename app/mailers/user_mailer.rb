7c1c67e324153787564340ce5fdc5e0a6d52304c
ezekg <ezekg@yahoo.com>
2018-06-04 15:33:45 -0500

    fix issue with incorrect password reset expiry

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,6 +5,7 @@ class UserMailer < ApplicationMailer
   def password_reset(user:, token:)
     @user, @token = user, token
     @account = @user.account
+    @expiration_date = @user.password_reset_sent_at + 24.hours
 
     mail to: user.email, subject: "Password reset requested for your #{@account.name} account"
   end
