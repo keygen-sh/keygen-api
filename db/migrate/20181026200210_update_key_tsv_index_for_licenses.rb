87557da6ef7f6a0efa5c360c1c6e01a05611cd3b
ezekg <ezekg@yahoo.com>
2018-10-26 15:27:01 -0500

    update license tsv indexes to only search first 128 chars

---
+++ db/migrate/20181026200210_update_key_tsv_index_for_licenses.rb
@@ -0,0 +1,17 @@
+class UpdateKeyTsvIndexForLicenses < ActiveRecord::Migration[5.0]
+  def up
+    remove_index :licenses, name: :licenses_tsv_key_idx
+
+    add_index :licenses, %[to_tsvector('pg_catalog.simple', left(coalesce(key::TEXT, ''), 128))],
+      name: "licenses_tsv_key_idx",
+      using: :gin
+  end
+
+  def down
+    remove_index :licenses, name: :licenses_tsv_key_idx
+
+    add_index :licenses, %[to_tsvector('pg_catalog.simple', coalesce(key::TEXT, ''))],
+      name: "licenses_tsv_key_idx",
+      using: :gin
+  end
+end
