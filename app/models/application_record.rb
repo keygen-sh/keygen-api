38cdd284c4016f00e7a60e39d5c11374125826c9
ezekg <ezekg@yahoo.com>
2018-10-31 13:58:44 -0500

    update default scope to avoid ambiguous col err

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,7 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  default_scope -> { order "created_at ASC" }
+  default_scope -> { order created_at: :asc }
 
   def destroy_async
     DestroyModelWorker.perform_async self.class.name, self.id
