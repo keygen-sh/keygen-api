4c87cad321591d295acd1820295ecd97c5eb78a7
ezekg <ezekg@yahoo.com>
2018-10-31 13:09:40 -0500

    fix issue where the resource could be deleted

--- app/workers/record_metric_worker.rb
+++ app/workers/record_metric_worker.rb
@@ -7,15 +7,23 @@ class RecordMetricWorker
 
   def perform(metric, account_id, resource_type, resource_id)
     account = Account.find account_id
-    resource = resource_type.classify.constantize.find resource_id
+
+    # TODO(ezekg) Should probably scope this to the account
+    resource = resource_type.classify.constantize.find_by id: resource_id
+    data =
+      if resource.present?
+        { resource: resource.id }.tap { |data|
+          %w[product policy license user].map(&:to_sym).each do |r|
+            data[r] = resource.send(r)&.id if resource.respond_to? r
+          end
+        }.compact
+      else
+        { resource: resource_id }
+      end
 
     account.metrics.create(
       metric: metric,
-      data: { resource: resource.id }.tap { |data|
-        %w[product policy license user].map(&:to_sym).each do |r|
-          data[r] = resource.send(r)&.id if resource.respond_to? r
-        end
-      }.compact
+      data: data
     )
   end
 end
