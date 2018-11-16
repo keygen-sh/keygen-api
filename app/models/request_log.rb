7e34a6e627fc0915a67b4ddf80a5a0ca74073bd4
ezekg <ezekg@yahoo.com>
2018-11-16 11:50:28 -0600

    add current period scope to logs

--- app/models/request_log.rb
+++ app/models/request_log.rb
@@ -6,4 +6,11 @@ class RequestLog < ApplicationRecord
   belongs_to :account
 
   validates :account, presence: { message: "must exist" }
+
+  scope :current_period, -> {
+    date_start = 2.weeks.ago.beginning_of_day
+    date_end = Time.current.end_of_day
+
+    where created_at: (date_start..date_end)
+  }
 end
