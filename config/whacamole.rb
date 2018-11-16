4159b4042d34b0781f2a4aaeda155e06da7c1771
ezekg <ezekg@yahoo.com>
2018-11-15 21:43:43 -0600

    move whacamole config to env vars

--- config/whacamole.rb
+++ config/whacamole.rb
@@ -1,6 +1,6 @@
 Whacamole.configure ENV['WHACAMOLE_APP_NAME'] do |config|
   config.api_token = ENV['WHACAMOLE_API_TOKEN']
   config.dynos = %w[web]
-  config.restart_threshold = 500
-  config.restart_window = 30*60
+  config.restart_threshold = ENV.fetch('WHACAMOLE_RESTART_THRESHOLD') { 500 }.to_i
+  config.restart_window = ENV.fetch('WHACAMOLE_RESTART_WINDOW') { 30 * 60 }.to_i
 end
