dc163ad448f6cb3b0eaa081ef274b8c40218cc67
ezekg <ezekg@yahoo.com>
2018-11-20 18:13:52 -0600

    update report styling

--- spec/mailers/previews/report_mailer_preview.rb
+++ spec/mailers/previews/report_mailer_preview.rb
@@ -1,5 +1,18 @@
 class ReportMailerPreview < ActionMailer::Preview
   def request_limits
-    ReportMailer.request_limits date: Date.yesterday, reports: []
+    ReportMailer.request_limits date: Date.yesterday, reports: [
+      OpenStruct.new(
+        request_count: 3_401,
+        request_limit: 5_000,
+        account: Account.new(id: SecureRandom.uuid, name: 'Foo', slug: 'foo', plan: Plan.new(name: 'Business')),
+        admin: User.new(first_name: 'John', last_name: 'Doe', email: 'john@example.com')
+      ),
+      OpenStruct.new(
+        request_count: 6_199,
+        request_limit: 5_000,
+        account: Account.new(id: SecureRandom.uuid, name: 'Bar', slug: 'bar', plan: Plan.new(name: 'Business')),
+        admin: User.new(first_name: 'Jane', last_name: 'Doe', email: 'jane@example.com')
+      )
+    ]
   end
 end
