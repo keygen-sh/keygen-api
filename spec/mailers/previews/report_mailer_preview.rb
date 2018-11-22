3392fa005f66d599207b985bd9b3a99a85aea7f5
ezekg <ezekg@yahoo.com>
2018-11-21 18:06:13 -0600

    add subscription state to daily req report

--- spec/mailers/previews/report_mailer_preview.rb
+++ spec/mailers/previews/report_mailer_preview.rb
@@ -4,13 +4,13 @@ class ReportMailerPreview < ActionMailer::Preview
       OpenStruct.new(
         request_count: 3_401,
         request_limit: 5_000,
-        account: Account.new(id: SecureRandom.uuid, name: 'Foo', slug: 'foo', plan: Plan.new(name: 'Business')),
+        account: Account.new(id: SecureRandom.uuid, name: 'Foo', slug: 'foo', plan: Plan.new(name: 'Business'), billing: Billing.new(state: 'subscribed')),
         admin: User.new(first_name: 'John', last_name: 'Doe', email: 'john@example.com')
       ),
       OpenStruct.new(
         request_count: 6_199,
         request_limit: 5_000,
-        account: Account.new(id: SecureRandom.uuid, name: 'Bar', slug: 'bar', plan: Plan.new(name: 'Business')),
+        account: Account.new(id: SecureRandom.uuid, name: 'Bar', slug: 'bar', plan: Plan.new(name: 'Business'), billing: Billing.new(state: 'trialing')),
         admin: User.new(first_name: 'Jane', last_name: 'Doe', email: 'jane@example.com')
       )
     ]
