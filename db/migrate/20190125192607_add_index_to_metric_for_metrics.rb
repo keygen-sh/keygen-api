e58bc74680723dfce60a1ca2cde450a387c20a2f
ezekg <ezekg@yahoo.com>
2019-01-25 13:35:08 -0600

    add index for metrics.metric

---
+++ db/migrate/20190125192607_add_index_to_metric_for_metrics.rb
@@ -0,0 +1,7 @@
+class AddIndexToMetricForMetrics < ActiveRecord::Migration[5.2]
+  disable_ddl_transaction!
+
+  def change
+    add_index :metrics, [:metric, :account_id, :created_at], algorithm: :concurrently
+  end
+end
