fef7c03b96d4158edf129c065699d3d4703c95a5
ezekg <ezekg@yahoo.com>
2019-04-08 08:48:54 -0500

    fix issue with storing payment source

--- app/controllers/api/v1/stripe_controller.rb
+++ app/controllers/api/v1/stripe_controller.rb
@@ -52,13 +52,16 @@ module Api::V1
         return unless source.object == 'card' ||
                       source.type == 'card'
 
+        card = source.card
+        return unless card
+
         billing = Billing.find_by customer_id: source.customer
         return unless billing
 
         billing.update(
-          card_expiry: DateTime.new(source.exp_year.to_i, source.exp_month.to_i),
-          card_brand: source.brand,
-          card_last4: source.last4
+          card_expiry: DateTime.new(card.exp_year, card.exp_month),
+          card_brand: card.brand,
+          card_last4: card.last4
         )
       when "customer.subscription.trial_will_end"
         subscription = event.data.object
