c94a57fca2006652680383fdf297ddd74282ac69
ezekg <ezekg@yahoo.com>
2019-07-19 17:07:20 -0500

    add configurable puma keep alive timeout

--- config/puma.rb
+++ config/puma.rb
@@ -21,6 +21,9 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # processes).
 workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
+# Specifies connection keep-alive/idle timeout
+persistent_timeout ENV.fetch("RAILS_KEEP_ALIVE_TIMEOUT") { 20 }
+
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
