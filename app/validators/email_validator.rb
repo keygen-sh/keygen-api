2c64df6cd5c221c1632d768b8daf47eed0dcd325
ezekg <ezekg@yahoo.com>
2019-08-07 10:20:48 -0500

    fix email validator

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -3,7 +3,7 @@
 class EmailValidator < ActiveModel::EachValidator
 
   def validate_each(record, attribute, value)
-    record.errors.add attribute, :invalid, message: "must be a valid email" unless EmailValidator.valid_email?(value)
+    record.errors.add attribute, :invalid, message: "must be a valid email" unless EmailValidator.valid?(value)
   end
 
   def self.valid?(value)
