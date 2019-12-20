59925fd861944eaaa163e0ce43649947fb74ce84
ezekg <ezekg@yahoo.com>
2019-12-19 22:00:25 -0600

    fix issue where dist reqs were counted/logged

--- lib/keygen/middleware.rb
+++ lib/keygen/middleware.rb
@@ -21,7 +21,7 @@ module Keygen
     end
 
     class RequestCountLogger
-      IGNORED_ORIGINS = %w[https://app.keygen.sh].freeze
+      IGNORED_ORIGINS = %w[https://app.keygen.sh https://dist.keygen.sh].freeze
 
       def initialize(app)
         @app = app
@@ -48,7 +48,7 @@ module Keygen
     end
 
     class RequestLogger
-      IGNORED_ORIGINS = %w[https://app.keygen.sh].freeze
+      IGNORED_ORIGINS = %w[https://app.keygen.sh https://dist.keygen.sh].freeze
       IGNORED_RESOURCES = %w[
         webhook_endpoints
         webhook_events
