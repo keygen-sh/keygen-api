cdb1cb8a8969fd3a1afb94be13454d19b3b5a902
Zeke Gabrielse <ezekg@yahoo.com>
2020-09-16 15:14:47 -0500

    fix issue where policies were not eager loaded

--- app/controllers/api/v1/machines_controller.rb
+++ app/controllers/api/v1/machines_controller.rb
@@ -18,7 +18,7 @@ module Api::V1
 
     # GET /machines
     def index
-      @machines = policy_scope apply_scopes(current_account.machines.preload(:product))
+      @machines = policy_scope apply_scopes(current_account.machines.preload(:product, :policy))
       authorize @machines
 
       render jsonapi: @machines
