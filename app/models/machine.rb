6bf8655aacef57c5c5753b95eb2d06525d71f8b9
Zeke Gabrielse <ezekg@yahoo.com>
2020-09-16 15:14:47 -0500

    fix issue where policies were not eager loaded

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -59,7 +59,7 @@ class Machine < ApplicationRecord
   scope :policy, -> (id) { joins(license: [:policy]).where policies: { id: id } }
 
   def heartbeat_duration
-    policy.heartbeat_duration || HEARTBEAT_TTL
+    policy&.heartbeat_duration || HEARTBEAT_TTL
   end
 
   def heartbeat_not_started?
