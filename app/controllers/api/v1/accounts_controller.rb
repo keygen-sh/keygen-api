9c7b0162683e1b67e24ef8f318e8125667d81be8
Zeke Gabrielse <ezekg@yahoo.com>
2020-09-17 14:00:23 -0500

    add autogen account slug and make names optional

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -20,6 +20,34 @@ module Api::V1
       @account = Account.new account_params
       authorize @account
 
+      # Use the admin's email domain as a fallback account name (maybe use Clearbit here?)
+      admin, _ = account_params.fetch(:users_attributes, [])
+      email = admin&.fetch(:email)
+
+      # Extract the domain so that we can for the account's name and slug
+      domain = email&.[](/[^@]+@(.+)/, 1)
+
+      if @account.slug.nil?
+        # Parameterize the domain, i.e. keygen.sh => keygen-sh
+        slug = domain&.parameterize
+
+        # Generate a random slug if the current one is nil, a public email
+        # service, or if an account with that slug already exists.
+        if slug.nil? || PUBLIC_EMAIL_SERVICES.include?(domain) || Account.exists?(slug: slug)
+          trek_word = Faker::TvShows::StarTrek.location
+          sw_word =  Faker::Movies::StarWars.planet
+          color = Faker::Color.color_name
+
+          slug = "acct-#{color}-#{sw_word}-#{trek_word}-#{SecureRandom.hex(4)}".downcase.parameterize
+        end
+
+        @account.slug = slug
+      end
+
+      if @account.name.nil?
+        @account.name = domain
+      end
+
       if @account.save
         render jsonapi: @account, status: :created, location: v1_account_url(@account)
       else
@@ -63,9 +91,9 @@ module Api::V1
       on :create do
         param :data, type: :hash do
           param :type, type: :string, inclusion: %w[account accounts]
-          param :attributes, type: :hash do
-            param :name, type: :string
-            param :slug, type: :string
+          param :attributes, type: :hash, optional: true do
+            param :name, type: :string, optional: true
+            param :slug, type: :string, optional: true
             param :protected, type: :boolean, optional: true
           end
           param :relationships, type: :hash do
@@ -80,8 +108,8 @@ module Api::V1
                 items type: :hash do
                   param :type, type: :string, inclusion: %w[user users]
                   param :attributes, type: :hash do
-                    param :first_name, type: :string
-                    param :last_name, type: :string
+                    param :first_name, type: :string, optional: true
+                    param :last_name, type: :string, optional: true
                     param :email, type: :string
                     param :password, type: :string
                   end
