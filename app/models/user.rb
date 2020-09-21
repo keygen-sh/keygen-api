c844bea1e9a162ebda3b65418f1e37a8d6d0fd32
Zeke Gabrielse <ezekg@yahoo.com>
2020-09-21 13:41:22 -0500

    fix issue where second factor couldn't be enabled

--- app/models/user.rb
+++ app/models/user.rb
@@ -80,9 +80,9 @@ class User < ApplicationRecord
   end
 
   def verify_second_factor(otp)
-    return false if second_factors.enabled.empty?
+    return false if second_factors.empty?
 
-    second_factor = second_factors.enabled.last
+    second_factor = second_factors.last
 
     second_factor.verify(otp)
   end
