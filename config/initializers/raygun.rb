c38c4472a8cac991a1b652b8572c06c5923cc47d
Zeke Gabrielse <ezekg@yahoo.com>
2020-09-24 07:40:52 -0500

    add affected user to raygun

--- config/initializers/raygun.rb
+++ config/initializers/raygun.rb
@@ -2,5 +2,6 @@
 
 Raygun.setup do |config|
   config.api_key = ENV["RAYGUN_APIKEY"]
+  config.affected_user_method = :current_bearer
   config.filter_parameters = Rails.application.config.filter_parameters
 end
