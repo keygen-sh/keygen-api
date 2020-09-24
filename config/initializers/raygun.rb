71b857e8369cda726a8c70918e48589d5dc4bf18
Zeke Gabrielse <ezekg@yahoo.com>
2020-09-24 08:03:18 -0500

    update raygun api key name

--- config/initializers/raygun.rb
+++ config/initializers/raygun.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Raygun.setup do |config|
-  config.api_key = ENV["RAYGUN_APIKEY"]
+  config.api_key = ENV["RAYGUN_API_KEY"]
   config.affected_user_method = :current_bearer
   config.filter_parameters = Rails.application.config.filter_parameters
 end
