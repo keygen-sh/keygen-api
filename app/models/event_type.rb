f9891253338af1ae0d869a1a09c66af0edc700bf
Zeke Gabrielse <ezekg@yahoo.com>
2020-09-24 15:10:00 -0500

    fix failing tests due to caching event types

--- app/models/event_type.rb
+++ app/models/event_type.rb
@@ -54,21 +54,21 @@ class EventType < ApplicationRecord
     *
   ].freeze
 
-  def self.cache_key(id)
-    [:event_types, id].join ":"
+  def self.cache_key(event)
+    [:event_types, event].join ":"
   end
 
   def cache_key
-    EventType.cache_key id
+    EventType.cache_key event
   end
 
-  def self.clear_cache!(id)
-    key = EventType.cache_key id
+  def self.clear_cache!(event)
+    key = EventType.cache_key event
 
     Rails.cache.delete key
   end
 
   def clear_cache!
-    EventType.clear_cache! id
+    EventType.clear_cache! event
   end
 end
