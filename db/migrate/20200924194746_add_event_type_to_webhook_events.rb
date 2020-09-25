a64f684275a288a0069a48fc350fe52a65134262
Zeke Gabrielse <ezekg@yahoo.com>
2020-09-24 22:45:11 -0500

    add event type columns

--- db/migrate/20200924194746_add_event_type_to_webhook_events.rb
+++ db/migrate/20200924194746_add_event_type_to_webhook_events.rb
@@ -2,19 +2,9 @@ class AddEventTypeToWebhookEvents < ActiveRecord::Migration[5.2]
   disable_ddl_transaction!
 
   def up
-    add_column :webhook_events, :event_type_id, :uuid, null: true
-
-    # Update all webhook events to have an event type association before
-    # we add the foreign key constraint
-    WebhookEvent.connection.update('
-      UPDATE webhook_events AS we
-        SET event_type_id = et.id
-      FROM event_types AS et
-        WHERE we.event = et.event
-    ')
-
-    change_column :webhook_events, :event_type_id, :uuid, null: false
-
+    unless column_exists?(:webhook_events, :event_type_id)
+      add_column :webhook_events, :event_type_id, :uuid, null: true
+    end
     add_index :webhook_events, :event_type_id, algorithm: :concurrently
   end
 
