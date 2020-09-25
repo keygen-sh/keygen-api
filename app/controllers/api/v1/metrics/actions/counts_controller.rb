93428f218dfbe67b860ab95ff60ed5b987d8d9f0
Zeke Gabrielse <ezekg@yahoo.com>
2020-09-24 21:20:33 -0500

    fix timezone issue with count date ranges

--- app/controllers/api/v1/metrics/actions/counts_controller.rb
+++ app/controllers/api/v1/metrics/actions/counts_controller.rb
@@ -27,7 +27,7 @@ module Api::V1::Metrics::Actions
       json = Rails.cache.fetch(cache_key, expires_in: 15.minutes) do
         conn = ActiveRecord::Base.connection
 
-        dates = 13.days.ago.to_date..Date.today
+        dates = 13.days.ago.to_date..Time.current.to_date
         sql =
           if events.present?
             <<~SQL
