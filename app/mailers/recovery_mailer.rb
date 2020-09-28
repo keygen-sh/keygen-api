2953f4fb3835a6ecb70087ee9bf53cf07f86b94b
Zeke Gabrielse <ezekg@yahoo.com>
2020-09-28 12:27:17 -0500

    fix issue where account may have been deleted

--- app/mailers/recovery_mailer.rb
+++ app/mailers/recovery_mailer.rb
@@ -5,7 +5,10 @@ class RecoveryMailer < ApplicationMailer
   layout "recovery_mailer"
 
   def recover_accounts_for_email(email:)
-    @users = User.preload(:account).joins(:role).where(email: email.to_s.downcase, roles: { name: [:admin, :developer, :sales_agent, :support_agent] })
+    @users = User.preload(:account)
+                 .joins(:role)
+                 .where(email: email.to_s.downcase, roles: { name: [:admin, :developer, :sales_agent, :support_agent] })
+                 .where('EXISTS (SELECT null FROM "accounts" WHERE "accounts"."id" = "users"."account_id")')
     return unless @users.any?
 
     mail to: email, subject: "Keygen account recovery for #{email}"
