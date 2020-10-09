5d0b7e30425c9fbc0c4d27e39fa09a680e8faed0
Zeke Gabrielse <ezekg@yahoo.com>
2020-10-09 11:46:37 -0500

    update email address search to allow searching by domain name

--- config/initializers/pg_search.rb
+++ config/initializers/pg_search.rb
@@ -25,13 +25,16 @@ module PgSearch
 
   # FIXME(ezekg) This is incredibly hacky, but since our keys can get quite
   #              large due to cryptographic signatures, this limits key search
-  #              space to the first 128 chars.
+  #              space to the first 128 chars. We're also making email tokenization
+  #              more usable by allowing searches by domain name.
   class Configuration::Column
     def to_sql
       if "#{table_name}.#{column_name}" == '"machines"."fingerprint"' ||
          "#{table_name}.#{column_name}" == '"licenses"."key"' ||
          "#{table_name}.#{column_name}" == '"keys"."key"'
         "\"left\"(coalesce(#{expression}::text, ''), 128)"
+      elsif "#{table_name}.#{column_name}" == '"users"."email"'
+        "\"replace\"(coalesce(#{expression}::text, ''), '@', ' ')"
       else
         "coalesce(#{expression}::text, '')"
       end
