94a53317cb56a106ce073e71c74a5c35e47940b6
Zeke Gabrielse <ezekg@yahoo.com>
2020-10-30 16:50:18 -0500

    add admin and product count to daily limits email

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -10,13 +10,17 @@ class ReportMailer < ApplicationMailer
     @reports = reports
       .map do |report|
         report.request_limit_exceeded = (report.request_count > report.request_limit) rescue false
+        report.product_limit_exceeded = (report.product_count > report.product_limit) rescue false
+        report.admin_limit_exceeded = (report.admin_count > report.admin_limit) rescue false
         report
       end
       .sort_by do |report|
         [
           # NOTE(ezekg) Ruby can't sort on boolean fields
           report.request_limit_exceeded ? 1 : 0,
-          report.request_count
+          report.product_limit_exceeded ? 1 : 0,
+          report.admin_limit_exceeded ? 1 : 0,
+          report.request_count,
         ]
       end
       .reverse
