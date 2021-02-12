7cfd4477f1e7e285faac1774cf79ec55f8712d8e
Zeke Gabrielse <ezekg@yahoo.com>
2021-02-12 15:36:29 -0600

    add tsv indexes for ids

--- db/migrate/20210212154113_add_search_indexes_to_request_logs.rb
+++ db/migrate/20210212154113_add_search_indexes_to_request_logs.rb
@@ -2,8 +2,8 @@ class AddSearchIndexesToRequestLogs < ActiveRecord::Migration[5.2]
   disable_ddl_transaction!
 
   def change
-    add_index :request_logs, :request_id, name: :request_logs_tsv_request_id_idx, algorithm: :concurrently, using: :gin
+    add_index :request_logs, "to_tsvector('simple'::regconfig, (request_id)::text)", name: :request_logs_tsv_request_id_idx, algorithm: :concurrently, using: :gin
+    add_index :request_logs, "to_tsvector('simple'::regconfig, (ip)::text)", name: :request_logs_tsv_ip_idx, algorithm: :concurrently, using: :gin
     add_index :request_logs, :url, name: :request_logs_tsv_url_idx, algorithm: :concurrently, using: :gin
-    add_index :request_logs, :ip, name: :request_logs_tsv_ip_idx, algorithm: :concurrently, using: :gin
   end
 end
