7a98944db58c5140764588b0d909a060311373e9
Zeke Gabrielse <ezekg@yahoo.com>
2021-02-16 10:07:59 -0600

    add scheduled daily workers to prune old resources

---
+++ app/workers/prune_request_logs_worker.rb
@@ -0,0 +1,24 @@
+class PruneRequestLogsWorker
+  include Sidekiq::Worker
+
+  sidekiq_options queue: :cron, unique: :until_executed
+
+  def perform
+    accounts = Account.joins(:request_logs)
+                      .where('request_logs.created_at < ?', 90.days.ago)
+                      .group('accounts.id')
+                      .having('count(request_logs.id) > 0')
+
+    accounts.find_each do |account|
+      loop do
+        logs = account.request_logs
+                      .where('created_at < ?', 90.days.ago)
+
+        count = logs.limit(1_000)
+                    .delete_all
+
+        break if count == 0
+      end
+    end
+  end
+end
