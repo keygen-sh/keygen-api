cf03cafff45e444cd40fafc742b8a2171d86ae68
Zeke Gabrielse <ezekg@yahoo.com>
2021-02-17 07:29:37 -0600

    add index to request log resource

--- db/migrate/20210212154113_add_search_indexes_to_request_logs.rb
+++ db/migrate/20210212154113_add_search_indexes_to_request_logs.rb
@@ -4,6 +4,6 @@ class AddSearchIndexesToRequestLogs < ActiveRecord::Migration[5.2]
   def change
     add_index :request_logs, "to_tsvector('simple'::regconfig, (request_id)::text)", name: :request_logs_tsv_request_id_idx, algorithm: :concurrently, using: :gin
     add_index :request_logs, "to_tsvector('simple'::regconfig, (ip)::text)", name: :request_logs_tsv_ip_idx, algorithm: :concurrently, using: :gin
-    add_index :request_logs, :url, name: :request_logs_tsv_url_idx, algorithm: :concurrently, using: :gin
+    add_index :request_logs, :url, algorithm: :concurrently, using: :gin
   end
 end
