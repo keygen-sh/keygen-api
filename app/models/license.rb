3028ba07e282538065ac10f65caa97914587722c
Zeke Gabrielse <ezekg@yahoo.com>
2021-02-18 14:59:28 -0600

    replace slow search queries with ilike

--- app/models/license.rb
+++ app/models/license.rb
@@ -68,6 +68,12 @@ class License < ApplicationRecord
   validates :metadata, length: { maximum: 64, message: "too many keys (exceeded limit of 64 keys)" }
   validates :uses, numericality: { greater_than_or_equal_to: 0 }
 
+  # FIXME(ezekg) Hack to override pg_search with more performant query
+  # TODO(ezekg) Rip out pg_search
+  scope :search_key, -> (term) {
+    where('key ILIKE ?', "%#{term}%")
+  }
+
   scope :active, -> (start_date = 90.days.ago) { where 'created_at >= :start_date OR last_validated_at >= :start_date', start_date: start_date }
   scope :suspended, -> (status = true) { where suspended: ActiveRecord::Type::Boolean.new.cast(status) }
   scope :unassigned, -> (status = true) {
