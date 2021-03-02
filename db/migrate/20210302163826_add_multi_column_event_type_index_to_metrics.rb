7dff9ed0ed7c5d8ae57270978336ff0f967be81e
Zeke Gabrielse <ezekg@yahoo.com>
2021-03-02 10:43:08 -0600

    add multicol index for common metrics query

---
+++ db/migrate/20210302163826_add_multi_column_event_type_index_to_metrics.rb
@@ -0,0 +1,11 @@
+class AddMultiColumnEventTypeIndexToMetrics < ActiveRecord::Migration[5.2]
+  disable_ddl_transaction!
+
+  def change
+    add_index :metrics, [:account_id, :created_at, :event_type_id], {
+      name: :metrics_account_created_event_type_idx,
+      order: { created_at: :desc },
+      algorithm: :concurrently
+    }
+  end
+end
