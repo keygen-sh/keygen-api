45c0d1f7997a4c95a2d72f6add1649a0d8bdaf63
Zeke Gabrielse <ezekg@yahoo.com>
2021-03-17 09:33:37 -0500

    fix possible email collision in tests

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
   factory :user do
     first_name { Faker::Name.name }
     last_name { Faker::Name.name }
-    email { Faker::Internet.safe_email }
+    email { [SecureRandom.hex(4), Faker::Internet.safe_email].join('-') }
     password "password"
 
     association :account
