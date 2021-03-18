58db1ea1a09445dba697cae0bbe203c0ed18a2ae
Zeke Gabrielse <ezekg@yahoo.com>
2021-03-17 21:47:36 -0500

    remove broken migration

--- db/migrate/20210318021634_add_unique_index_to_key_and_account_for_licenses.rb
+++
@@ -1,7 +0,0 @@
-class AddUniqueIndexToKeyAndAccountForLicenses < ActiveRecord::Migration[5.2]
-  disable_ddl_transaction!
-
-  def change
-    add_index :licenses, [:account_id, :key], name: :licenses_account_id_key_unique_idx, unique: true, algorithm: :concurrently
-  end
-end
