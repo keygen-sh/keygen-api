20c160c28fad44bfe8a689cc51058a75761273e0
Zeke Gabrielse <ezekg@yahoo.com>
2021-03-17 21:45:05 -0500

    update unique index on key to use md5

--- db/migrate/20210318021634_add_unique_index_to_key_and_account_for_licenses.rb
+++ db/migrate/20210318021634_add_unique_index_to_key_and_account_for_licenses.rb
@@ -2,6 +2,6 @@ class AddUniqueIndexToKeyAndAccountForLicenses < ActiveRecord::Migration[5.2]
   disable_ddl_transaction!
 
   def change
-    add_index :licenses, [:account_id, :key], unique: true, algorithm: :concurrently
+    add_index :licenses, [:account_id, :key], name: :licenses_account_id_key_unique_idx, unique: true, algorithm: :concurrently
   end
 end
