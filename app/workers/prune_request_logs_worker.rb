44d0c7fb2c7c7e636105b67da94d1c5825214af9
Zeke Gabrielse <ezekg@yahoo.com>
2021-04-01 16:53:08 -0500

    update log and metric retention periods

--- app/workers/prune_request_logs_worker.rb
+++ app/workers/prune_request_logs_worker.rb
@@ -5,14 +5,14 @@ class PruneRequestLogsWorker
 
   def perform
     accounts = Account.joins(:request_logs)
-                      .where('request_logs.created_at < ?', 30.days.ago)
+                      .where('request_logs.created_at < ?', 2.weeks.ago)
                       .group('accounts.id')
                       .having('count(request_logs.id) > 0')
 
     accounts.find_each do |account|
       loop do
         logs = account.request_logs
-                      .where('created_at < ?', 30.days.ago.beginning_of_day)
+                      .where('created_at < ?', 2.weeks.ago.beginning_of_day)
 
         count = logs.limit(1_000)
                     .delete_all
