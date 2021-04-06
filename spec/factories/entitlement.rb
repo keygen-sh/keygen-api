94063da57f3dfe5aecc144c6b4da6b0e72fa4efa
Zeke Gabrielse <ezekg@yahoo.com>
2021-04-06 16:13:19 -0500

    update fake entitlement names

--- spec/factories/entitlement.rb
+++ spec/factories/entitlement.rb
@@ -3,6 +3,6 @@
 FactoryGirl.define do
   factory :entitlement do
     code { "#{Faker::Company.buzzword.parameterize.underscore}_#{SecureRandom.hex(2)}".upcase }
-    name { Faker::Company.name }
+    name { Faker::Company.industry }
   end
 end
