29a0445831c2446c5b361bfd3bbfaeb8d128a9ac
Zeke Gabrielse <ezekg@yahoo.com>
2021-04-17 08:27:38 -0500

    add if exists check

--- db/migrate/20210417125404_add_unique_index_to_email_for_users.rb
+++ db/migrate/20210417125404_add_unique_index_to_email_for_users.rb
@@ -2,7 +2,7 @@ class AddUniqueIndexToEmailForUsers < ActiveRecord::Migration[6.1]
   disable_ddl_transaction!
 
   def change
-    remove_index :users, [:email, :account_id, :created_at]
+    remove_index :users, [:email, :account_id, :created_at], if_exists: true
 
     add_index :users, [:email, :account_id], unique: true, algorithm: :concurrently
   end
