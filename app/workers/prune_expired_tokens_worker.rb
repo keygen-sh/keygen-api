a47daf84052db9aaed74c30964a8b9e3df18881e
Zeke Gabrielse <ezekg@yahoo.com>
2021-04-21 21:25:39 -0500

    update token prune timeframe

--- app/workers/prune_expired_tokens_worker.rb
+++ app/workers/prune_expired_tokens_worker.rb
@@ -5,7 +5,7 @@ class PruneExpiredTokensWorker
 
   def perform
     loop do
-      tokens = Token.where('expiry is not null and expiry < ?', 6.months.ago)
+      tokens = Token.where('expiry is not null and expiry < ?', 90.days.ago)
                     .reorder(created_at: :asc)
 
       count = tokens.limit(10_000)
