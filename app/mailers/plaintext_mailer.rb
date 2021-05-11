0f2e9b079907d2dbc0f022dbb8422d57de96dac9
Zeke Gabrielse <ezekg@yahoo.com>
2021-05-11 11:37:29 -0500

    update trial ending soon mailers

--- app/mailers/plaintext_mailer.rb
+++ app/mailers/plaintext_mailer.rb
@@ -9,10 +9,8 @@ class PlaintextMailer < ApplicationMailer
     mail(
       content_type: "text/plain",
       to: admin.email,
-      subject: "Have a quick question for you",
+      subject: "Are you stuck?",
       body: <<~TXT
-        Hey team,
-
         I noticed that you created a Keygen account but there hasn't been much API activity on it lately. Is there anything I can do to help get things kickstarted?
 
         Just want to make sure you aren't stuck on anything. As a technical founder, it's easy for me to overlook roadblocks for new users. :)
@@ -23,7 +21,7 @@ class PlaintextMailer < ApplicationMailer
     )
   end
 
-  def trial_ending_soon(account:)
+  def trial_ending_soon_without_payment_method(account:)
     admin = account.admins.first
 
     mail(
@@ -31,16 +29,34 @@ class PlaintextMailer < ApplicationMailer
       to: admin.email,
       subject: "Quick heads up",
       body: <<~TXT
-        Hey team,
+        Just wanted to let you know that your free trial ends in 2 days. I also noticed that you don't have a payment method added yet. Is there anything I can do to help get one added?
 
-        I noticed that your trial is ending soon but there's no payment method added yet. Is there anything I can do to help get one added? I'd also be happy to extend your trial -- just let me know.
+        Hopefully you're getting some value out of Keygen and aren't stuck on anything? You can reply to this email directly with any feedback or questions.
 
-        Hopefully you're getting some value out of Keygen and aren't stuck on anything?
+        I'd be more than happy to extend your trial an extra 30 days if you're still in the dev phase -- just let me know.
 
         --
         Zeke, Founder <https://keygen.sh>
 
         p.s. I know COVID has affected many businesses, so please let me know if there's anything I can do to help. :)
+      TXT
+    )
+  end
+
+  def trial_ending_soon_with_payment_method(account:)
+    admin = account.admins.first
+
+    mail(
+      content_type: "text/plain",
+      to: admin.email,
+      subject: "Your trial is ending",
+      body: <<~TXT
+        I hope you've seen how Keygen can help your business save time and money with our software licensing API. Just wanted to give you a quick heads up that your free trial ends in 2 days.
+
+        If Keygen has not met your expectations in anyway, we'd like to know about it. You can reply to this email directly with any feedback or questions.
+
+        --
+        Zeke, Founder <https://keygen.sh>
       TXT
     )
   end
@@ -53,8 +69,6 @@ class PlaintextMailer < ApplicationMailer
       to: admin.email,
       subject: "Keygen <> #{account.name}",
       body: <<~TXT
-        Hey team,
-
         I saw your first payment went through earlier and I just wanted to reach out real quick to thank you for your business.
 
         Keygen is a bootstrapped company and we love to connect with our customers. Is there anything we can do better?
