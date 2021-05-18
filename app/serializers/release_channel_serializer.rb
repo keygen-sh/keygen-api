645a65e7f146270ef1494d257b69072534a1072c
Zeke Gabrielse <ezekg@yahoo.com>
2021-05-18 11:06:44 -0500

    add relationship endpoints for channel and platform

---
+++ app/serializers/release_channel_serializer.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class ReleaseChannelSerializer < BaseSerializer
+  type 'release-channels'
+
+  attribute :name
+  attribute :key
+  attribute :created do
+    @object.created_at
+  end
+  attribute :updated do
+    @object.updated_at
+  end
+
+  relationship :account do
+    linkage always: true do
+      { type: :accounts, id: @object.account_id }
+    end
+    link :related do
+      @url_helpers.v1_account_path @object.account_id
+    end
+  end
+end
