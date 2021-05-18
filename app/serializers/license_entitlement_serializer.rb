0d9d2191544c6ee331e027b17a7f97b5259b9cb9
Zeke Gabrielse <ezekg@yahoo.com>
2021-05-18 12:04:53 -0500

    fix formatting

--- app/serializers/license_entitlement_serializer.rb
+++ app/serializers/license_entitlement_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class LicenseEntitlementSerializer < BaseSerializer
-  type "license-entitlements"
+  type 'license-entitlements'
 
   attribute :created do
     @object.created_at
--- app/serializers/request_log_serializer.rb
+++ app/serializers/request_log_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class RequestLogSerializer < BaseSerializer
-  type "request-logs"
+  type 'request-logs'
 
   attribute :request_id
   attribute :url
