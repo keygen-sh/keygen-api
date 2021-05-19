a42ee467d48f874080b00d48ca636ada8103db32
Zeke Gabrielse <ezekg@yahoo.com>
2021-05-19 17:16:08 -0500

    add member update action to releases

--- config/initializers/jsonapi.rb
+++ config/initializers/jsonapi.rb
@@ -12,8 +12,11 @@ Mime::Type.register "application/vnd.api+json", :jsonapi, %W[
 ActionDispatch::Request.parameter_parsers[:json]    =
 ActionDispatch::Request.parameter_parsers[:jsonapi] = -> (raw_post) {
   data = ActiveSupport::JSON.decode raw_post
-  data = { _json: data } unless data.is_a? Hash
+  data = { _json: data } unless
+    data.is_a? Hash
+
   data.deep_transform_keys! { |k| k.to_s.underscore.parameterize(separator: '_') }
+
   data.with_indifferent_access
 }
 
