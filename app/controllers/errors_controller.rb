6f92a75fd7fa61795956fa68e031549666dc1229
Zeke Gabrielse <ezekg@yahoo.com>
2021-06-11 08:19:56 -0500

    add default content type to errors controller

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -3,16 +3,20 @@
 class ErrorsController < ApplicationController
   STATUS_CODES = Rack::Utils::HTTP_STATUS_CODES.transform_values { |v| v.encode('utf-8') }
 
-  before_action :set_status
+  before_action :validate_accept_and_add_content_type_headers!
+  before_action :set_status_code
 
   def show
     skip_authorization
-    self.send "render_#{@status}"
+
+    self.send "render_#{status_code}"
   end
 
   private
 
-  def set_status
-    @status = STATUS_CODES[params[:code].to_i || 500].parameterize.underscore
+  attr_accessor :status_code
+
+  def set_status_code
+    @status_code = STATUS_CODES[params[:code].to_i || 500].parameterize.underscore
   end
 end
