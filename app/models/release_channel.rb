db85cdbe73a71e06c36c1d9754e5f09b2886fee8
Zeke Gabrielse <ezekg@yahoo.com>
2021-06-22 16:04:20 -0500

    add base for releases index tests

--- app/models/release_channel.rb
+++ app/models/release_channel.rb
@@ -16,4 +16,28 @@ class ReleaseChannel < ApplicationRecord
     presence: true,
     uniqueness: { message: 'already exists', scope: :account_id },
     inclusion: { in: %w[stable rc beta alpha dev] }
+
+  def stable?
+    key == 'stable'
+  end
+
+  def pre_release?
+    !stable?
+  end
+
+  def rc?
+    key == 'rc'
+  end
+
+  def beta?
+    key == 'beta'
+  end
+
+  def alpha?
+    key == 'alpha'
+  end
+
+  def dev?
+    key == 'dev'
+  end
 end
