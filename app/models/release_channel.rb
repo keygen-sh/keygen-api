a9cec6eca736f4da4453878d242da47c2b0bb397
Zeke Gabrielse <ezekg@yahoo.com>
2021-07-08 11:32:15 -0500

    add scopes to channel and platform models

--- app/models/release_channel.rb
+++ app/models/release_channel.rb
@@ -8,6 +8,12 @@ class ReleaseChannel < ApplicationRecord
     inverse_of: :release_channels
   has_many :releases,
     inverse_of: :channel
+  has_many :products,
+    through: :releases
+  has_many :licenses,
+    through: :products
+  has_many :users,
+    through: :licenses
 
   validates :account,
     presence: { message: 'must exist' }
@@ -16,6 +22,18 @@ class ReleaseChannel < ApplicationRecord
     presence: true,
     uniqueness: { message: 'already exists', scope: :account_id },
     inclusion: { in: %w[stable rc beta alpha dev] }
+
+  scope :for_product, -> id {
+    joins(:products).where(products: { id: id }).distinct
+  }
+
+  scope :for_user, -> id {
+    joins(:users).where(users: { id: id }).distinct
+  }
+
+  scope :for_license, -> id {
+    joins(:licenses).where(licenses: { id: id }).distinct
+  }
 
   def stable?
     key == 'stable'
--- app/models/release_platform.rb
+++ app/models/release_platform.rb
@@ -8,6 +8,12 @@ class ReleasePlatform < ApplicationRecord
     inverse_of: :release_platforms
   has_many :releases,
     inverse_of: :platform
+  has_many :products,
+    through: :releases
+  has_many :licenses,
+    through: :products
+  has_many :users,
+    through: :licenses
 
   validates :account,
     presence: { message: 'must exist' }
@@ -15,4 +21,16 @@ class ReleasePlatform < ApplicationRecord
   validates :key,
     presence: true,
     uniqueness: { message: 'already exists', scope: :account_id }
+
+  scope :for_product, -> id {
+    joins(:products).where(products: { id: id }).distinct
+  }
+
+  scope :for_user, -> id {
+    joins(:users).where(users: { id: id }).distinct
+  }
+
+  scope :for_license, -> id {
+    joins(:licenses).where(licenses: { id: id }).distinct
+  }
 end
