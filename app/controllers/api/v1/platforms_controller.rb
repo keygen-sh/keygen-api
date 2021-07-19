b82ea9d4c851d9efc5f206bc90216316ff635f73
Zeke Gabrielse <ezekg@yahoo.com>
2021-07-19 09:48:41 -0500

    update platforms list to only show those with releases
    - closes #444

--- app/controllers/api/v1/platforms_controller.rb
+++ app/controllers/api/v1/platforms_controller.rb
@@ -8,7 +8,7 @@ module Api::V1
     before_action :set_platform, only: [:show]
 
     def index
-      platforms = policy_scope apply_scopes(current_account.release_platforms)
+      platforms = policy_scope apply_scopes(current_account.release_platforms.with_releases)
       authorize platforms
 
       render jsonapi: platforms
