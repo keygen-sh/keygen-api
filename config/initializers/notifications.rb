27ab56b2e578f7ec8bbba26c44cf4b5cfc68588c
Zeke Gabrielse <ezekg@yahoo.com>
2021-08-26 07:29:19 -0500

    update rate limiting error code

--- config/initializers/notifications.rb
+++ config/initializers/notifications.rb
@@ -2,6 +2,6 @@
 
 ActiveSupport::Notifications.subscribe("rack.attack") do |name, start, finish, request_id, req|
   if req.env['rack.attack.match_type'] == :throttle
-    Keygen.logger.info "Throttled IP: #{req.ip}"
+    Keygen.logger.info "[rack_attack] Rate limited: request_id=#{request_id} ip=#{req.ip}"
   end
 end
