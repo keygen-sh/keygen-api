5a88d2e63cc51ff6521d291849956ad0ce55cb4f
Zeke Gabrielse <ezekg@yahoo.com>
2021-09-01 16:48:47 -0500

    fix typo

--- lib/typed_parameters.rb
+++ lib/typed_parameters.rb
@@ -131,7 +131,7 @@ class TypedParameters
         when :string
           :string
         else
-          Keygen.logger.error("[typed_parameters] Unknown type: request_id=#{context.request.request_id} type=#{type_sym}")
+          Keygen.logger.error("[typed_parameters] Unknown type: request_id=#{context.request.request_id} type=#{t_sym}")
 
           :null
         end
