947497ca3fe6e0f3315801bb76db313766e4e833
Zeke Gabrielse <ezekg@yahoo.com>
2021-09-09 09:23:33 -0500

    add comment to clarify use

--- app/validators/scope_validator.rb
+++ app/validators/scope_validator.rb
@@ -2,10 +2,12 @@
 
 class ScopeValidator < ActiveModel::EachValidator
   def validate_each(parent, name, child)
-    return if child.nil?
+    return if
+      child.nil?
 
     key = options.fetch(:by).to_s
 
+    # Assert that parent scope matches child scope (i.e. account_id)
     parent.errors.add(name, :blank, message: 'must exist') unless
       parent.attributes[key] == child.attributes[key]
   end
