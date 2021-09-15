215bf11c19ed43ff869c71270fbb5f67c1ad7f26
Zeke Gabrielse <ezekg@yahoo.com>
2021-09-15 10:11:16 -0500

    update password reset from email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UserMailer < ApplicationMailer
-  default from: "Keygen Support <support@keygen.sh>"
+  default from: "Keygen <noreply@keygen.sh>"
   layout "user_mailer"
 
   def password_reset(user:, token:)
