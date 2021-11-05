861e512c65cfdbe5c2fbd945d6d3286415db184b
Zeke Gabrielse <ezekg@yahoo.com>
2021-11-05 11:27:13 -0500

    add index to distribution strat

--- db/migrate/20211105145230_add_distribution_strategy_to_products.rb
+++ db/migrate/20211105145230_add_distribution_strategy_to_products.rb
@@ -1,5 +1,7 @@
 class AddDistributionStrategyToProducts < ActiveRecord::Migration[6.1]
   def change
     add_column :products, :distribution_strategy, :string, null: true
+
+    add_index :products, :distribution_strategy
   end
 end
