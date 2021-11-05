e765e903b9e09f30c38fb117b9aeec56db5fbaa2
Zeke Gabrielse <ezekg@yahoo.com>
2021-11-05 12:09:30 -0500

    add dist strat to product serializer

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -4,6 +4,7 @@ class ProductSerializer < BaseSerializer
   type :products
 
   attribute :name
+  attribute :distribution_strategy
   attribute :url
   attribute :platforms
   attribute :metadata do
