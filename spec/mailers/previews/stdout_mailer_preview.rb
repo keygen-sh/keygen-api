d875f472409d0d0804d8fff68ba68cd9bd9b225c
Zeke Gabrielse <ezekg@yahoo.com>
2021-11-29 11:21:17 -0600

    arrays start at zero

--- spec/mailers/previews/stdout_mailer_preview.rb
+++ spec/mailers/previews/stdout_mailer_preview.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class StdoutMailerPreview < ActionMailer::Preview
-  def issue_one
-    StdoutMailer.issue_one(subscriber: User.first)
+  def issue_zero
+    StdoutMailer.issue_zero(subscriber: User.first)
   end
 end
