79023138fa6b5e5573fa308b8fb92e198a22d90b
Zeke Gabrielse <ezekg@yahoo.com>
2021-12-01 14:14:48 -0600

    fix missing account scope assert

--- app/policies/license_policy.rb
+++ app/policies/license_policy.rb
@@ -93,6 +93,8 @@ class LicensePolicy < ApplicationPolicy
   end
 
   def validate_by_id?
+    assert_account_scoped!
+
     bearer.has_role?(:admin, :developer, :sales_agent, :support_agent) ||
       resource.user == bearer ||
       resource.product == bearer ||
