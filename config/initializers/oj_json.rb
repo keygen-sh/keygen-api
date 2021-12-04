70de02e85c8ac00381a38214a4722ab871814db4
Zeke Gabrielse <ezekg@yahoo.com>
2021-12-04 14:53:25 -0600

    revert "fix html entity escaping in json"
    This reverts commit 40d99de125e48bdec7d66885a5e843fd57a0ff6c.

--- config/initializers/oj_json.rb
+++ config/initializers/oj_json.rb
@@ -8,8 +8,3 @@ Oj::Rails.tap do |oj|
   oj.optimize
   oj.mimic_JSON
 end
-
-# After setting Oj as the default JSON serializer, disable
-# Rails' default behavior for escaping HTML entities in
-# JSON, i.e. & => \u0026.
-ActiveSupport::JSON::Encoding.escape_html_entities_in_json = false
