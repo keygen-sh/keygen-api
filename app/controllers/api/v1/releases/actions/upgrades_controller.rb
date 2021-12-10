99d9d6c6b0f1499d4d890457c25a6d7c40583d36
Zeke Gabrielse <ezekg@yahoo.com>
2021-12-10 11:49:29 -0600

    increase ttl for uploads and upgrades

--- app/controllers/api/v1/releases/actions/upgrades_controller.rb
+++ app/controllers/api/v1/releases/actions/upgrades_controller.rb
@@ -81,7 +81,7 @@ module Api::V1::Releases::Actions
       if upgrade.next_release.present?
         authorize upgrade.next_release, :download?
 
-        download = ReleaseDownloadService.call(account: current_account, release: upgrade.next_release, ttl: 10.minutes, upgrade: true)
+        download = ReleaseDownloadService.call(account: current_account, release: upgrade.next_release, ttl: 1.hour, upgrade: true)
         meta     = {
           current: upgrade.current_version,
           next: upgrade.next_version,
