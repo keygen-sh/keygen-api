589b8a43c8a56ee8c1113a47fd9f4930f617c87d
Zeke Gabrielse <ezekg@yahoo.com>
2021-12-27 16:18:03 -0600

    remove superfluous delegation

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,14 +3,7 @@
 class Event < ApplicationRecord
   belongs_to :account
   belongs_to :event_type
-  belongs_to :resource,
-    polymorphic: true
-  belongs_to :whodunnit,
-    polymorphic: true
-  belongs_to :request_log,
-    optional: true
-
-  delegate :requestor,
-    to: :request_log,
-    allow_nil: true
+  belongs_to :resource,    polymorphic: true
+  belongs_to :whodunnit,   polymorphic: true
+  belongs_to :request_log, optional: true
 end
