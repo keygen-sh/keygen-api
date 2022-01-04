ec07700ab6c816fb8d68df0b45c7a3e2e29a54d3
Zeke Gabrielse <ezekg@yahoo.com>
2022-01-04 07:53:00 -0600

    fix issue where request id count be supplied by client

--- app/controllers/concerns/current_request_attributes.rb
+++ app/controllers/concerns/current_request_attributes.rb
@@ -3,7 +3,7 @@ module CurrentRequestAttributes
 
   included do
     before_action do
-      Current.request_id = request.uuid
+      Current.request_id = request.request_id = SecureRandom.uuid
       Current.host       = request.host
       Current.ip         = request.ip
     end
