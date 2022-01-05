82260385866ec0c43725782bfc78ce810b15cd6e
Zeke Gabrielse <ezekg@yahoo.com>
2022-01-05 07:17:00 -0600

    update current attrs to explicitly reset on account change

--- app/models/current.rb
+++ app/models/current.rb
@@ -7,4 +7,13 @@ class Current < ActiveSupport::CurrentAttributes
   attribute :request_id,
             :host,
             :ip
+
+  def account=(account)
+    super
+
+    # Ensure these are always reset when account is changed
+    self.bearer   = nil
+    self.token    = nil
+    self.resource = nil
+  end
 end
