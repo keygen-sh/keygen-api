4418d290d68061ddec94327891cc9207de1f1176
Zeke Gabrielse <ezekg@yahoo.com>
2022-01-05 14:13:58 -0600

    add support for x-request-id header

--- app/controllers/concerns/current_request_attributes.rb
+++ app/controllers/concerns/current_request_attributes.rb
@@ -3,7 +3,12 @@ module CurrentRequestAttributes
 
   included do
     before_action do
-      Current.request_id = request.request_id = SecureRandom.uuid
+      # Allow custom request IDs to be specified via X-Request-Id, but only if
+      # they match the UUID v4 format. This is used by Heroku.
+      request.request_id = SecureRandom.uuid unless
+        request.request_id.match?(UUID_REGEX)
+
+      Current.request_id = request.request_id
       Current.host       = request.host
       Current.ip         = request.ip
     end
