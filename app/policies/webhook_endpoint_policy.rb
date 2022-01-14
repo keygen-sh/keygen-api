95e869f03121336edccdd6f96f56efa98fcc0912
Zeke Gabrielse <ezekg@yahoo.com>
2022-01-14 12:21:52 -0600

    allow product tokens to manage webhooks
    - closes #563

--- app/policies/webhook_endpoint_policy.rb
+++ app/policies/webhook_endpoint_policy.rb
@@ -5,30 +5,30 @@ class WebhookEndpointPolicy < ApplicationPolicy
   def index?
     assert_account_scoped!
 
-    bearer.has_role?(:admin, :developer)
+    bearer.has_role?(:admin, :developer, :product)
   end
 
   def show?
     assert_account_scoped!
 
-    bearer.has_role?(:admin, :developer)
+    bearer.has_role?(:admin, :developer, :product)
   end
 
   def create?
     assert_account_scoped!
 
-    bearer.has_role?(:admin, :developer)
+    bearer.has_role?(:admin, :developer, :product)
   end
 
   def update?
     assert_account_scoped!
 
-    bearer.has_role?(:admin, :developer)
+    bearer.has_role?(:admin, :developer, :product)
   end
 
   def destroy?
     assert_account_scoped!
 
-    bearer.has_role?(:admin, :developer)
+    bearer.has_role?(:admin, :developer, :product)
   end
 end
