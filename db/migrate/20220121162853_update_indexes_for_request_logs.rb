da2718fa6602aa021c20a7f2c9b5553ba6001537
Zeke Gabrielse <ezekg@yahoo.com>
2022-01-21 11:11:11 -0600

    fix migration

--- db/migrate/20220121162853_update_indexes_for_request_logs.rb
+++ db/migrate/20220121162853_update_indexes_for_request_logs.rb
@@ -7,10 +7,9 @@ class UpdateIndexesForRequestLogs < ActiveRecord::Migration[6.1]
     remove_index :request_logs, :request_logs_request_id_created_idx, algorithm: :concurrently, if_exists: true
     remove_index :request_logs, :request_logs_top_requestor_idx, algorithm: :concurrently, if_exists: true
     remove_index :request_logs, :request_logs_top_resource_idx, algorithm: :concurrently, if_exists: true
-    remove_index :request_logs, %i[account_id created_at], algorithm: :concurrently, if_exists: true
 
     # NOTE(ezekg) Account ID is first so that the index can work on a
     #             smaller dataset before filtering by created at.
-    add_index :request_logs, %i[account_id created_at], algorithm: :concurrently
+    add_index :request_logs, %i[account_id created_at], algorithm: :concurrently, if_not_exists: true
   end
 end
