9b8915dc14f01a98b92700703f0a9f143f2ce692
Zeke Gabrielse <ezekg@yahoo.com>
2022-03-01 09:26:08 -0600

    add base for groups

---
+++ app/models/group.rb
@@ -0,0 +1,17 @@
+class Group < ApplicationRecord
+  belongs_to :account
+
+  has_many :licenses, -> { where(member_type: License.name) },
+    class_name: 'GroupMember'
+  has_many :users, -> { where(member_type: User.name) },
+    class_name: 'GroupMember'
+  has_many :machines, -> { where(member_type: Machine.name) },
+    class_name: 'GroupMember'
+  has_many :members,
+    class_name: 'GroupMember'
+  has_many :owners,
+    class_name: 'GroupOwner'
+
+  validates :account,
+    presence: true
+end
