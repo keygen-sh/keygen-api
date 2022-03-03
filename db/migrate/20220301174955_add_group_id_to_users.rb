6c4cebdb5ad7ae7f37c87cca99ba75eba7e025b0
Zeke Gabrielse <ezekg@yahoo.com>
2022-03-03 17:09:09 -0600

    add concurrent group index migrations

--- db/migrate/20220301174955_add_group_id_to_users.rb
+++ db/migrate/20220301174955_add_group_id_to_users.rb
@@ -1,7 +1,5 @@
 class AddGroupIdToUsers < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :group_id, :uuid, null: true
-
-    add_index :users, :group_id
   end
 end
--- db/migrate/20220301174958_add_group_id_to_licenses.rb
+++ db/migrate/20220301174958_add_group_id_to_licenses.rb
@@ -1,7 +1,5 @@
 class AddGroupIdToLicenses < ActiveRecord::Migration[6.1]
   def change
     add_column :licenses, :group_id, :uuid, null: true
-
-    add_index :licenses, :group_id
   end
 end
