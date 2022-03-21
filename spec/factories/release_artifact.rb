101290a72bea0b4da8448697dfe716b3ba2019b2
Zeke Gabrielse <ezekg@yahoo.com>
2022-03-21 13:45:38 -0500

    fix failing upgrade service tests

---
+++ spec/factories/release_artifact.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+FactoryGirl.define do
+  factory :release_artifact do
+    key { SecureRandom.hex }
+
+    account nil
+    product nil
+    release
+
+    after :build do |artifact, evaluator|
+      artifact.account ||= evaluator.account.presence || create(:account)
+      artifact.product ||= evaluator.product.presence || create(:product, account: artifact.account)
+      artifact.release ||= evaluator.release.presence || create(:release)
+    end
+  end
+end
