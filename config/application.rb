45bcd0e895dba30e8b9d2db5193543a35eefa24d
Zeke Gabrielse <ezekg@yahoo.com>
2022-03-31 16:17:22 -0500

    update app to use rails 7 defaults

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ Bundler.require *Rails.groups
 
 module Keygen
   class Application < Rails::Application
-    config.load_defaults 6.1
+    config.load_defaults 7.0
 
     config.generators do |generator|
       # Use UUIDs for table primary keys
