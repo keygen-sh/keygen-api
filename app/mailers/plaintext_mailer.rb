bc2b2d504dd59ec8b5e44f35cfbba9d1c5d349c9
Zeke Gabrielse <ezekg@yahoo.com>
2022-04-19 13:41:19 -0500

    update timeframe

--- app/mailers/plaintext_mailer.rb
+++ app/mailers/plaintext_mailer.rb
@@ -227,7 +227,7 @@ class PlaintextMailer < ApplicationMailer
 
           Quick reminder that we'll be increasing our prices soon. Again, this is the first time we've ever increased prices for our customers, and we don't take this event lightly. We'll be increasing prices across the board due to increased operating costs.
 
-          Below are the new prices, going into effect on May 1st, 2022 (next week). Your account will automatically be upgraded, unless you're on a free tier or you cancel (and we'd really hate that -- so please reach out).
+          Below are the new prices, going into effect on May 1st, 2022 (less than 2 weeks). Your account will automatically be upgraded, unless you're on a free tier or you cancel (and we'd really hate that -- so please reach out).
 
           - Dev 0: no changes (i.e. it's still free)
           - Tier 0: $19 to $29/mo ($290/yr)
