3e59f1be2d5dfde3e5e8322d18f40277fb8cf9b0
Zeke Gabrielse <ezekg@yahoo.com>
2022-04-21 07:52:40 -0500

    move current version to app controller

--- config/initializers/versionist.rb
+++ config/initializers/versionist.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_dependency Rails.root.join "lib", "versionist"
+require_dependency Rails.root.join('lib', 'versionist')
 
 Versionist.configure do |config|
   config.logger = Keygen.logger
