d97f164e6b5c4daa0f60b8476134e743c6b6eff3
Zeke Gabrielse <ezekg@yahoo.com>
2022-04-21 11:19:25 -0500

    update migrations to use pattern matching

--- app/migrations/artifact_has_many_to_has_one_for_release_migration.rb
+++ app/migrations/artifact_has_many_to_has_one_for_release_migration.rb
@@ -1,35 +1,43 @@
 # frozen_string_literal: true
 
-class ArtifactHasManyToHasOneForReleaseMigration < Versionist::Migration
+class ArtifactHasManyToHasOneForReleaseMigration < Versionist::Migration[1.1]
   description "transforms a Release's artifacts from a has-many to has-one relationship"
 
   routes :v1_account_release,
          :v1_account_product_release
 
-  response do |res|
+  transform do |data|
     next unless
-      res.successful?
+      data.present?
 
-    body = JSON.parse(res.body, symbolize_names: true)
-    data = body[:data]
+    case data
+    in data: { type: 'releases', id: release_id, relationships: { account: { data: { type: 'accounts', id: account_id } } } }
+      artifact = ReleaseArtifact.find_by(
+        release_id:,
+        account_id:,
+      )
 
-    account_id = data[:relationships][:account][:data][:id]
-    release_id = data[:id]
+      data[:data][:relationships].tap do |rels|
+        rels[:artifact] = {
+          data: artifact.present? ? { type: 'artifacts', id: artifact.id } : nil,
+          links: { related: url_helpers.v1_account_release_legacy_artifact_path(account_id, release_id) },
+        }
 
-    artifact = ReleaseArtifact.find_by(
-      release_id:,
-      account_id:,
-    )
-
-    data[:relationships].tap do |rels|
-      rels[:artifact] = {
-        data: artifact.present? ? { type: 'artifacts', id: artifact.id } : nil,
-        links: { related: v1_account_release_legacy_artifact_path(account_id, release_id) },
-      }
-
-      rels.delete(:artifacts)
+        rels.delete(:artifacts)
+      end
     end
 
-    res.body = JSON.generate(body)
+    data
+  end
+
+  response do |res|
+    return unless
+      res.present? && res.successful?
+
+    data = JSON.parse(res.body, symbolize_names: true)
+
+    transform!(data)
+
+    res.body = JSON.generate(data)
   end
 end
