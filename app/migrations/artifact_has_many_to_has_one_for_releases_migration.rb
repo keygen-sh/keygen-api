ff2d2bc234d33b5b7fca6065e65b0131f9d584ff
Zeke Gabrielse <ezekg@yahoo.com>
2022-04-22 15:07:44 -0500

    rewrite versionist

--- app/migrations/artifact_has_many_to_has_one_for_releases_migration.rb
+++ app/migrations/artifact_has_many_to_has_one_for_releases_migration.rb
@@ -1,26 +1,21 @@
 # frozen_string_literal: true
 
-class ArtifactHasManyToHasOneForReleasesMigration < Versionist::Migration[1.1]
+class ArtifactHasManyToHasOneForReleasesMigration < Versionist::Migration
   description "transforms a collection of Releases' artifacts from has-many to has-one relationships"
 
-  routes :v1_account_releases,
-         :v1_account_product_releases
-
-  transform do |data|
-    next unless
-      data.present?
-
-    case data
+  migrate if: -> body { body in data: [*] } do |body|
+    case body
     in data: [*, { type: 'releases', id: _, relationships: { account: { data: { type: 'accounts', id: _ } } } }, *]
-      account_ids = data[:data].collect { _1[:relationships][:account][:data][:id] }.compact.uniq
-      release_ids = data[:data].collect { _1[:id] }.compact.uniq
+      account_ids = body[:data].collect { _1[:relationships][:account][:data][:id] }.compact.uniq
+      release_ids = body[:data].collect { _1[:id] }.compact.uniq
 
       artifacts = ReleaseArtifact.distinct_on(:release_id)
+                                 .select(:id)
                                  .where(account_id: account_ids, release_id: release_ids)
                                  .reorder(:release_id, created_at: :desc)
                                  .group_by(&:release_id)
 
-      data[:data].each do |release|
+      body[:data].each do |release|
         case release
         in type: 'releases', id: release_id, relationships: { account: { data: { type: 'accounts', id: account_id } } }
           artifact = artifacts[release_id]&.first
@@ -28,25 +23,26 @@ class ArtifactHasManyToHasOneForReleasesMigration < Versionist::Migration[1.1]
           release[:relationships].tap do |rels|
             rels[:artifact] = {
               data: artifact.present? ? { type: 'artifacts', id: artifact.id } : nil,
-              links: { related: url_helpers.v1_account_release_legacy_artifact_path(account_id, release_id) },
+              links: {
+                related: v1_account_release_legacy_artifact_path(account_id, release_id),
+              },
             }
 
             rels.delete(:artifacts)
           end
         end
       end
+    else
     end
-
-    data
   end
 
-  response do |res|
+  response if: -> res { res.request.params in controller: 'api/v1/releases' | 'api/v1/products/relationships/releases', action: 'index' } do |res|
     next unless
       res.successful?
 
     data = JSON.parse(res.body, symbolize_names: true)
 
-    transform!(data)
+    migrate!(data)
 
     res.body = JSON.generate(data)
   end
