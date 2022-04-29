120a2e9bfd59dc93680d0146ef07ed8386f4d6d3
Zeke Gabrielse <ezekg@yahoo.com>
2022-04-29 15:20:59 -0500

    refactor legacy controller organization

--- app/controllers/api/v1/releases/relationships/v0/artifacts_controller.rb
+++ app/controllers/api/v1/releases/relationships/v0/artifacts_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-module Api::V1::Releases::Relationships
-  class ArtifactController < Api::V1::BaseController
+module Api::V1::Releases::Relationships::V0
+  class ArtifactsController < Api::V1::BaseController
     before_action :scope_to_current_account!
     before_action :require_active_subscription!
     before_action :authenticate_with_token!, except: %i[index show]
