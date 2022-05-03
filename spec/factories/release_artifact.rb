05231ecfc08a7e76142c90b50c25d91f47ad0607
Zeke Gabrielse <ezekg@yahoo.com>
2022-05-03 10:54:38 -0500

    add migrations for moving releases attrs

--- spec/factories/release_artifact.rb
+++ spec/factories/release_artifact.rb
@@ -2,16 +2,25 @@
 
 FactoryBot.define do
   factory :release_artifact, aliases: %i[artifact] do
-    key { SecureRandom.hex }
+    filename { nil }
+    filesize { Faker::Number.between(from: 0, to: 1.gigabyte.to_i) }
 
     account { nil }
     product { nil }
     release { nil }
+    platform { nil }
+    filetype { nil }
 
     after :build do |artifact, evaluator|
-      artifact.account ||= evaluator.account.presence
-      artifact.product ||= evaluator.product.presence || build(:product, account: artifact.account)
-      artifact.release ||= evaluator.release.presence || build(:release, account: artifact.account)
+      artifact.account  ||= evaluator.account.presence
+      artifact.product  ||= evaluator.product.presence || build(:product, account: artifact.account)
+      artifact.release  ||= evaluator.release.presence || build(:release, account: artifact.account)
+      artifact.platform ||= evaluator.platform.presence || build(:platform, account: artifact.account)
+      artifact.filetype ||= evaluator.filetype.presence || build(:filetype, account: artifact.account)
+
+      # Add dependant attributes after associations are set in stone
+      artifact.filename ||=
+        "#{artifact.release.name}-#{artifact.release.version}.#{artifact.filetype.key}"
     end
   end
 end
