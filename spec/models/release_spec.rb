20f1835392d08c529ec86f234a96dc980ecd647f
Zeke Gabrielse <ezekg@yahoo.com>
2022-05-10 10:28:02 -0500

    add more release upgrade tests

--- spec/models/release_spec.rb
+++ spec/models/release_spec.rb
@@ -84,6 +84,12 @@ describe Release, type: :model do
           1.0.1
           1.1.3
           1.9.42
+          2.0.0-alpha.1
+          2.0.0-alpha.2
+          2.0.0-alpha.3
+          2.0.0-beta.1
+          2.0.0-rc.1
+          2.1.0-rc.1
         ]
 
         versions.each { create(:release, :published, version: _1, product:, account:) }
@@ -96,48 +102,362 @@ describe Release, type: :model do
       end
     end
 
-    context 'when there is an upgrade available' do
-      subject { create(:release, :published, version: '2.0.0', product:, account:) }
+    context 'when there is a draft upgrade available' do
+      subject { create(:release, :published, version: '1.0.0', product:, account:) }
 
+       before do
+        create(:release, :draft, version: '2.0.0', product:, account:)
+      end
+
+      it 'should not upgrade' do
+        upgrade = subject.upgrade!
+
+        expect(upgrade).to be_nil
+      end
+    end
+
+    context 'when there is a yanked upgrade available' do
+      subject { create(:release, :published, version: '1.0.0', product:, account:) }
+
+       before do
+        create(:release, :yanked, version: '2.0.0', product:, account:)
+      end
+
+      it 'should not upgrade' do
+        upgrade = subject.upgrade!
+
+        expect(upgrade).to be_nil
+      end
+    end
+
+    context 'when there is a an upgrade available' do
       before do
         versions = %w[
           1.0.0-beta
           1.0.0-beta.2
           1.0.0-beta+exp.sha.6
-          1.0.0-alpha.beta
-          1.0.0+20130313144700
-          1.0.0-alpha
-          1.0.0-beta.11
-          1.0.0-alpha.1
-          1.0.0
-          1.0.0+21AF26D3
-          1.0.0-beta+exp.sha.5114f85
-          1.0.0-rc.1
-          1.0.0-alpha+001
-          1.0.1
+          1.1.0-alpha.beta
+          1.1.0+20130313144700
+          1.1.0-alpha
+          1.1.0-beta.11
+          1.1.0-alpha.1
+          1.1.0
+          1.1.0+21AF26D3
+          1.1.0-beta+exp.sha.5114f85
+          1.1.0-rc.1
+          1.1.0-alpha+001
+          1.1.1
           1.1.3
           1.1.21
           2.0.1
           2.0.2
           2.1.0
-          2.1.2
+          2.1.2-alpha.1
+          2.1.9-rc.1
           2.1.9
           2.9.0
-          2.11.0
+          2.11.0-beta.1
           3.0.0
+          3.0.2
         ]
 
         versions.each { create(:release, :published, version: _1, product:, account:) }
       end
 
-      it 'should upgrade to the latest version' do
-        upgrade = subject.upgrade!
-        assert upgrade
+      context 'when upgrading from the stable channel' do
+        subject { create(:release, :published, version: '3.0.1', product:, account:) }
 
-        expect(upgrade.version).to eq '3.0.0'
+        context 'when the upgrade is for the stable channel' do
+          before { create(:release, :published, version: '3.1.0', product:, account:) }
+
+          it 'should upgrade to the latest version' do
+            upgrade = subject.upgrade!
+            assert upgrade
+
+            expect(upgrade.version).to eq '3.1.0'
+          end
+        end
+
+        context 'when the upgrade is for the rc channel' do
+          before { create(:release, :published, version: '3.1.0-rc.1', product:, account:) }
+
+          it 'should not upgrade to the rc release' do
+            upgrade = subject.upgrade!
+            assert upgrade
+
+            expect(upgrade.version).to eq '3.0.2'
+          end
+        end
+
+        context 'when the upgrade is for the beta channel' do
+          before { create(:release, :published, version: '3.1.0-beta.1', product:, account:) }
+
+          it 'should not upgrade to the beta release' do
+            upgrade = subject.upgrade!
+            assert upgrade
+
+            expect(upgrade.version).to eq '3.0.2'
+          end
+        end
+
+        context 'when the upgrade is for the alpha channel' do
+          before { create(:release, :published, version: '3.1.0-alpha.1', product:, account:) }
+
+          it 'should not upgrade to the alpha release' do
+            upgrade = subject.upgrade!
+            assert upgrade
+
+            expect(upgrade.version).to eq '3.0.2'
+          end
+        end
+
+        context 'when the upgrade is for the dev channel' do
+          before { create(:release, :published, version: '3.1.0-dev.1', product:, account:) }
+
+          it 'should not upgrade to the dev release' do
+            upgrade = subject.upgrade!
+            assert upgrade
+
+            expect(upgrade.version).to eq '3.0.2'
+          end
+        end
+      end
+
+      context 'when upgrading from the rc channel' do
+        subject { create(:release, :published, version: '3.0.1-rc.1', product:, account:) }
+
+        context 'when the upgrade is for the stable channel' do
+          before { create(:release, :published, version: '3.1.0', product:, account:) }
+
+          it 'should upgrade to the latest version' do
+            upgrade = subject.upgrade!
+            assert upgrade
+
+            expect(upgrade.version).to eq '3.1.0'
+          end
+        end
+
+        context 'when the upgrade is for the rc channel' do
+          before { create(:release, :published, version: '3.1.0-rc.1', product:, account:) }
+
+          it 'should upgrade to the rc release' do
+            upgrade = subject.upgrade!
+            assert upgrade
+
+            expect(upgrade.version).to eq '3.1.0-rc.1'
+          end
+        end
+
+        context 'when the upgrade is for the beta channel' do
+          before { create(:release, :published, version: '3.1.0-beta.1', product:, account:) }
+
+          it 'should not upgrade to the beta release' do
+            upgrade = subject.upgrade!
+            assert upgrade
+
+            expect(upgrade.version).to eq '3.0.2'
+          end
+        end
+
+        context 'when the upgrade is for the alpha channel' do
+          before { create(:release, :published, version: '3.1.0-alpha.1', product:, account:) }
+
+          it 'should not upgrade to the alpha release' do
+            upgrade = subject.upgrade!
+            assert upgrade
+
+            expect(upgrade.version).to eq '3.0.2'
+          end
+        end
+
+        context 'when the upgrade is for the dev channel' do
+          before { create(:release, :published, version: '3.1.0-dev.1', product:, account:) }
+
+          it 'should not upgrade to the dev release' do
+            upgrade = subject.upgrade!
+            assert upgrade
+
+            expect(upgrade.version).to eq '3.0.2'
+          end
+        end
+      end
+
+      context 'when upgrading from the beta channel' do
+        subject { create(:release, :published, version: '3.0.1-beta.1', product:, account:) }
+
+        context 'when the upgrade is for the stable channel' do
+          before { create(:release, :published, version: '3.1.0', product:, account:) }
+
+          it 'should upgrade to the latest version' do
+            upgrade = subject.upgrade!
+            assert upgrade
+
+            expect(upgrade.version).to eq '3.1.0'
+          end
+        end
+
+        context 'when the upgrade is for the rc channel' do
+          before { create(:release, :published, version: '3.1.0-rc.1', product:, account:) }
+
+          it 'should upgrade to the rc release' do
+            upgrade = subject.upgrade!
+            assert upgrade
+
+            expect(upgrade.version).to eq '3.1.0-rc.1'
+          end
+        end
+
+        context 'when the upgrade is for the beta channel' do
+          before { create(:release, :published, version: '3.1.0-beta.1', product:, account:) }
+
+          it 'should upgrade to the beta release' do
+            upgrade = subject.upgrade!
+            assert upgrade
+
+            expect(upgrade.version).to eq '3.1.0-beta.1'
+          end
+        end
+
+        context 'when the upgrade is for the alpha channel' do
+          before { create(:release, :published, version: '3.1.0-alpha.1', product:, account:) }
+
+          it 'should not upgrade to the alpha release' do
+            upgrade = subject.upgrade!
+            assert upgrade
+
+            expect(upgrade.version).to eq '3.0.2'
+          end
+        end
+
+        context 'when the upgrade is for the dev channel' do
+          before { create(:release, :published, version: '3.1.0-dev.1', product:, account:) }
+
+          it 'should not upgrade to the dev release' do
+            upgrade = subject.upgrade!
+            assert upgrade
+
+            expect(upgrade.version).to eq '3.0.2'
+          end
+        end
+      end
+
+      context 'when upgrading from the alpha channel' do
+        subject { create(:release, :published, version: '3.0.1-alpha.1', product:, account:) }
+
+        context 'when the upgrade is for the stable channel' do
+          before { create(:release, :published, version: '3.1.0', product:, account:) }
+
+          it 'should upgrade to the latest version' do
+            upgrade = subject.upgrade!
+            assert upgrade
+
+            expect(upgrade.version).to eq '3.1.0'
+          end
+        end
+
+        context 'when the upgrade is for the rc channel' do
+          before { create(:release, :published, version: '3.1.0-rc.1', product:, account:) }
+
+          it 'should upgrade to the rc release' do
+            upgrade = subject.upgrade!
+            assert upgrade
+
+            expect(upgrade.version).to eq '3.1.0-rc.1'
+          end
+        end
+
+        context 'when the upgrade is for the beta channel' do
+          before { create(:release, :published, version: '3.1.0-beta.1', product:, account:) }
+
+          it 'should upgrade to the beta release' do
+            upgrade = subject.upgrade!
+            assert upgrade
+
+            expect(upgrade.version).to eq '3.1.0-beta.1'
+          end
+        end
+
+        context 'when the upgrade is for the alpha channel' do
+          before { create(:release, :published, version: '3.1.0-alpha.1', product:, account:) }
+
+          it 'should upgrade to the alpha release' do
+            upgrade = subject.upgrade!
+            assert upgrade
+
+            expect(upgrade.version).to eq '3.1.0-alpha.1'
+          end
+        end
+
+        context 'when the upgrade is for the dev channel' do
+          before { create(:release, :published, version: '3.1.0-dev.1', product:, account:) }
+
+          it 'should not upgrade to the dev release' do
+            upgrade = subject.upgrade!
+            assert upgrade
+
+            expect(upgrade.version).to eq '3.0.2'
+          end
+        end
+      end
+
+      context 'when upgrading from the dev channel' do
+        subject { create(:release, :published, version: '3.0.1-dev.1', product:, account:) }
+
+        context 'when the upgrade is for the stable channel' do
+          before { create(:release, :published, version: '3.1.0', product:, account:) }
+
+          it 'should not upgrade to the latest version' do
+            upgrade = subject.upgrade!
+
+            expect(upgrade).to be_nil
+          end
+        end
+
+        context 'when the upgrade is for the rc channel' do
+          before { create(:release, :published, version: '3.1.0-rc.1', product:, account:) }
+
+          it 'should not upgrade to the rc release' do
+            upgrade = subject.upgrade!
+
+            expect(upgrade).to be_nil
+          end
+        end
+
+        context 'when the upgrade is for the beta channel' do
+          before { create(:release, :published, version: '3.1.0-beta.1', product:, account:) }
+
+          it 'should not upgrade to the beta release' do
+            upgrade = subject.upgrade!
+
+            expect(upgrade).to be_nil
+          end
+        end
+
+        context 'when the upgrade is for the alpha channel' do
+          before { create(:release, :published, version: '3.1.0-alpha.1', product:, account:) }
+
+          it 'should not upgrade to the alpha release' do
+            upgrade = subject.upgrade!
+
+            expect(upgrade).to be_nil
+          end
+        end
+
+        context 'when the upgrade is for the dev channel' do
+          before { create(:release, :published, version: '3.1.0-dev.1', product:, account:) }
+
+          it 'should upgrade to the dev release' do
+            upgrade = subject.upgrade!
+            assert upgrade
+
+            expect(upgrade.version).to eq '3.1.0-dev.1'
+          end
+        end
       end
 
       context 'when using a constraint' do
+        subject { create(:release, :published, version: '2.0.0', product:, account:) }
+
         it 'should raise for an invalid constraint' do
           expect { subject.upgrade!(constraint: 'invalid') }.to raise_error Semverse::InvalidConstraintFormat
         end
@@ -146,14 +466,14 @@ describe Release, type: :model do
           upgrade = subject.upgrade!(constraint: '2')
           assert upgrade
 
-          expect(upgrade.version).to eq '2.11.0'
+          expect(upgrade.version).to eq '2.9.0'
         end
 
         it 'should upgrade to the latest v2.x version' do
           upgrade = subject.upgrade!(constraint: '2.0')
           assert upgrade
 
-          expect(upgrade.version).to eq '2.11.0'
+          expect(upgrade.version).to eq '2.9.0'
         end
 
         it 'should upgrade to the latest v2.0.x version' do
