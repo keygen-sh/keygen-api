867fe1ce1bf3c66106b88a8f0a3c724bff02fa69
Zeke Gabrielse <ezekg@yahoo.com>
2022-05-11 10:51:48 -0500

    add publish and yank actions for releases

---
+++ app/controllers/api/v1/releases/actions/publishings_controller.rb
@@ -0,0 +1,48 @@
+module Api::V1::Releases::Actions
+  class PublishingsController < Api::V1::BaseController
+    before_action :scope_to_current_account!
+    before_action :require_active_subscription!
+    before_action :authenticate_with_token!
+    before_action :set_release
+
+    def publish
+      authorize release
+
+      release.publish!
+
+      BroadcastEventService.call(
+        event: 'release.published',
+        account: current_account,
+        resource: release,
+      )
+
+      render jsonapi: release
+    end
+
+    def yank
+      authorize release
+
+      release.yank!
+
+      BroadcastEventService.call(
+        event: 'release.yanked',
+        account: current_account,
+        resource: release,
+      )
+
+      render jsonapi: release
+    end
+
+    private
+
+    attr_reader :release
+
+    def set_release
+      scoped_releases = policy_scope(current_account.releases)
+
+      @release = scoped_releases.find(params[:id])
+
+      Current.resource = release
+    end
+  end
+end
