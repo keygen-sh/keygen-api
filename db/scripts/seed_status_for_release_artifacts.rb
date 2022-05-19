81336549a217970450d174d7f54e38ab7b9655c6
Zeke Gabrielse <ezekg@yahoo.com>
2022-05-19 14:34:24 -0500

    fix example

--- db/scripts/seed_status_for_release_artifacts.rb
+++ db/scripts/seed_status_for_release_artifacts.rb
@@ -1,4 +1,4 @@
-# heroku run rails runner db/scripts/seed_status_for_releases.rb
+# heroku run rails runner db/scripts/seed_status_for_release_artifacts.rb
 
 Keygen.logger.info { "[scripts.seed_status_for_release_artifacts] Starting" }
 
