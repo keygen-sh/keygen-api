d442faaea6bf3dba69bb7a2993f7668e777a8099
Zeke Gabrielse <ezekg@yahoo.com>
2022-05-31 16:43:42 -0500

    add filters to artifacts list

--- app/controllers/api/v1/products/relationships/artifacts_controller.rb
+++ app/controllers/api/v1/products/relationships/artifacts_controller.rb
@@ -4,6 +4,9 @@ module Api::V1::Products::Relationships
   class ArtifactsController < Api::V1::BaseController
     has_scope(:channel) { |c, s, v| s.for_channel(v) }
     has_scope(:status) { |c, s, v| s.with_status(v) }
+    has_scope(:filetype) { |c, s, v| s.for_filetype(v) }
+    has_scope(:platform) { |c, s, v| s.for_platform(v) }
+    has_scope(:arch) { |c, s, v| s.for_arch(v) }
 
     before_action :scope_to_current_account!
     before_action :require_active_subscription!
