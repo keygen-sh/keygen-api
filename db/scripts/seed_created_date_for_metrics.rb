74610c584e1169e742df5a71598378359ed01296
Zeke Gabrielse <ezekg@yahoo.com>
2022-06-07 14:29:06 -0500

    add sleep to reduce load

--- db/scripts/seed_created_date_for_metrics.rb
+++ db/scripts/seed_created_date_for_metrics.rb
@@ -1,4 +1,4 @@
-# heroku run -e BATCH_SIZE=100000 rails runner db/scripts/seed_created_date_for_metrics.rb
+# heroku run:detached -e BATCH_SIZE=100000 rails runner db/scripts/seed_created_date_for_metrics.rb --tail
 
 BATCH_SIZE = ENV.fetch('BATCH_SIZE') { 10_000 }.to_i
 batch = 0
@@ -28,6 +28,8 @@ loop do
   puts "[scripts.seed_created_date_for_metrics] Updated #{count} metric rows (batch ##{batch})"
 
   break if count == 0
+
+  sleep 0.1
 end
 
 puts "[scripts.seed_created_date_for_metrics] Done"
