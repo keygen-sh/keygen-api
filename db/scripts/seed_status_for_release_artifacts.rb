6c0a235248539df488af81103feb3dbdda22f477
Zeke Gabrielse <ezekg@yahoo.com>
2022-06-07 09:17:56 -0500

    remove old scripts

--- db/scripts/seed_status_for_release_artifacts.rb
+++
@@ -1,8 +0,0 @@
-# heroku run rails runner db/scripts/seed_status_for_release_artifacts.rb
-
-Keygen.logger.info { "[scripts.seed_status_for_release_artifacts] Starting" }
-
-count = ReleaseArtifact.update_all(status: 'UPLOADED')
-
-Keygen.logger.info { "[scripts.seed_status_for_release_artifacts] Seeded status for #{count} artifact rows" }
-Keygen.logger.info { "[scripts.seed_status_for_release_artifacts] Done" }
