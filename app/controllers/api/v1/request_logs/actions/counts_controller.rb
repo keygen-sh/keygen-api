e0565ff435abfb5452e8d3e6a5f8b9da4c3882ee
Zeke Gabrielse <ezekg@yahoo.com>
2022-06-08 11:45:25 -0500

    update request log count to use series

--- app/controllers/api/v1/request_logs/actions/counts_controller.rb
+++ app/controllers/api/v1/request_logs/actions/counts_controller.rb
@@ -13,32 +13,32 @@ module Api::V1::RequestLogs::Actions
         start_date = Date.current - 13.days
         end_date   = Date.current
 
-        rows = conn.exec_query(<<~SQL.squish)
+        rows = conn.execute(<<~SQL.squish)
           SELECT
-            "request_logs"."created_date" AS logs_date,
-            COUNT(*)                      AS logs_count
+            coalesce(logs.count, 0) AS count,
+            period
           FROM
-            "request_logs"
-          WHERE
-            "request_logs"."account_id" = #{conn.quote current_account.id} AND
+            generate_series(date #{conn.quote start_date}, date #{conn.quote end_date}, interval '1 day') AS period (date)
+          LEFT JOIN LATERAL
             (
-              "request_logs"."created_date" >= #{conn.quote start_date} AND
-              "request_logs"."created_date" <= #{conn.quote end_date}
-            )
-          GROUP BY
-            logs_date
-          ORDER BY
-            logs_count ASC
+              SELECT
+                count(*)     AS count,
+                created_date AS date,
+                account_id
+              FROM
+                request_logs
+              WHERE
+                account_id   = #{conn.quote current_account.id} AND
+                created_date = period.date
+              GROUP BY
+                account_id,
+                date
+            ) AS logs USING (date);
         SQL
 
-        # Create zeroed out hash of dates then merge real counts (so we include dates with no data)
-        dates = start_date.to_date..end_date.to_date
-
         {
-          meta: dates.map { |d| [d.strftime('%Y-%m-%d'), 0] }.to_h
-            .merge(
-              rows.map { |r| r.fetch_values('logs_date', 'logs_count') }.to_h
-            )
+          meta: rows.map { [_1['period'].strftime('%Y-%m-%d'), _1['count']] }
+                    .to_h,
         }
       end
 
