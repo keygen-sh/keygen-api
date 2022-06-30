9c805bed462395384f1ecfde9c94bb29ae9b5e22
Zeke Gabrielse <ezekg@yahoo.com>
2022-06-30 16:02:44 -0500

    fix rack attack notifications

--- config/initializers/notifications.rb
+++ config/initializers/notifications.rb
@@ -2,8 +2,10 @@
 
 ActiveSupport::Notifications.subscribe("rack.attack") do |name, start, finish, request_id, data|
   req = data[:request]
+  ip  = req.env['action_dispatch.remote_ip'] || req.ip
+  fwd = req.env['HTTP_X_FORWARDED_FOR']
 
   if req.env['rack.attack.match_type'] == :throttle
-    Keygen.logger.info "[rack_attack] Rate limited: request_id=#{request_id} ip=#{req.remote_ip}"
+    Keygen.logger.info "[rack_attack] Rate limited: request_id=#{request_id} ip=#{ip} fwd=#{fwd}"
   end
 end
