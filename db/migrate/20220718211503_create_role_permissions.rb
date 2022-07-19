c4f7db988eb47dd72bf2521c72c69d860d0300c2
Zeke Gabrielse <ezekg@yahoo.com>
2022-07-19 09:48:38 -0500

    add uniqueness indexes

--- db/migrate/20220718211503_create_role_permissions.rb
+++ db/migrate/20220718211503_create_role_permissions.rb
@@ -2,9 +2,11 @@ class CreateRolePermissions < ActiveRecord::Migration[7.0]
   def change
     create_table :role_permissions, id: :uuid, default: -> { 'uuid_generate_v4()' } do |t|
       t.uuid :permission_id, null: false, index: true
-      t.uuid :role_id,       null: false, index: true
+      t.uuid :role_id,       null: false
 
       t.timestamps
+
+      t.index %i[role_id permission_id], unique: true
     end
   end
 end
