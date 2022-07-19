bff6e6e62971f80b448af481d25422f96d7c5116
Zeke Gabrielse <ezekg@yahoo.com>
2022-07-19 09:48:38 -0500

    add uniqueness indexes

--- db/migrate/20220718211555_create_token_permissions.rb
+++ db/migrate/20220718211555_create_token_permissions.rb
@@ -2,9 +2,11 @@ class CreateTokenPermissions < ActiveRecord::Migration[7.0]
   def change
     create_table :token_permissions, id: :uuid, default: -> { 'uuid_generate_v4()' } do |t|
       t.uuid :permission_id, null: false, index: true
-      t.uuid :token_id,      null: false, index: true
+      t.uuid :token_id,      null: false
 
       t.timestamps
+
+      t.index %i[token_id permission_id], unique: true
     end
   end
 end
--- db/migrate/20220718211700_create_group_permissions.rb
+++ db/migrate/20220718211700_create_group_permissions.rb
@@ -2,9 +2,11 @@ class CreateGroupPermissions < ActiveRecord::Migration[7.0]
   def change
     create_table :group_permissions, id: :uuid, default: -> { 'uuid_generate_v4()' } do |t|
       t.uuid :permission_id, null: false, index: true
-      t.uuid :group_id,      null: false, index: true
+      t.uuid :group_id,      null: false
 
       t.timestamps
+
+      t.index %i[group_id permission_id], unique: true
     end
   end
 end
