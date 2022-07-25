6b0f12c122072ff86bb95c8450680ef6063ce693
Zeke Gabrielse <ezekg@yahoo.com>
2022-07-25 15:39:57 -0500

    add seed script for tokens

---
+++ db/scripts/seed_permissions_for_tokens.rb
@@ -0,0 +1,18 @@
+# heroku run:detached -e 'BATCH_SIZE=10000;SLEEP_DURATION=3' rails runner db/scripts/seed_permissions_for_tokens.rb --tail
+
+BATCH_SIZE     = ENV.fetch('BATCH_SIZE')     { 1_000 }.to_i
+SLEEP_DURATION = ENV.fetch('SLEEP_DURATION') { 1 }.to_f
+
+Rails.logger.info "[scripts.seed_permissions_for_tokens] Starting"
+
+Token.find_in_batches(batch_size: BATCH_SIZE) do |tokens|
+  tokens.each {
+    _1.send(:set_default_permissions!)
+
+    sleep SLEEP_DURATION / 10
+  }
+
+  sleep SLEEP_DURATION
+end
+
+Rails.logger.info "[scripts.seed_permissions_for_tokens] Done"
