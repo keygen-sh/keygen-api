0e35617968c708cf7a56c10ffe9c7848d25ad728
Zeke Gabrielse <ezekg@yahoo.com>
2022-07-27 10:07:41 -0500

    add pundit rspec matcher

--- spec/policies/product_policy_spec.rb
+++ spec/policies/product_policy_spec.rb
@@ -9,19 +9,68 @@ describe ProductPolicy, type: :policy do
   let(:account) { create(:account) }
   let(:product) { create(:product, account:) }
 
+  context 'for admin' do
+  end
+
+  context 'for product' do
+  end
+
+  context 'for license' do
+  end
+
+  context 'for user' do
+    let(:context) { authorization_context(account:, bearer:, token:) }
+
+    context 'without product license' do
+      let(:bearer)  { create(:user, account:) }
+      let(:token)   { create(:token, account:, bearer:) }
+
+      it 'denies index access' do
+        expect(subject).to_not permit(:index)
+      end
+
+      it 'denies show access' do
+        expect(subject).to_not permit(:show)
+      end
+
+      it 'denies create access' do
+        expect(subject).to_not permit(:create)
+      end
+    end
+
+    context 'with product license' do
+      let(:policy)   { create(:policy, account:, product:) }
+      let(:licenses) { [create(:license, account:, policy:)] }
+      let(:bearer)   { create(:user, account:, licenses:) }
+      let(:token)    { create(:token, account:, bearer:) }
+
+      it 'denies index access' do
+        expect(subject).to_not permit(:index)
+      end
+
+      it 'denies show access' do
+        expect(subject).to_not permit(:show)
+      end
+
+      it 'denies create access' do
+        expect(subject).to_not permit(:create)
+      end
+    end
+  end
+
   context 'for anonymous' do
     let(:context) { authorization_context(account:) }
 
     it 'denies index access' do
-      expect { subject.index? }.to raise_error Pundit::NotAuthorizedError
+      expect(subject).to_not permit(:index)
     end
 
     it 'denies show access' do
-      expect { subject.show? }.to raise_error Pundit::NotAuthorizedError
+      expect(subject).to_not permit(:show)
     end
 
     it 'denies create access' do
-      expect { subject.create? }.to raise_error Pundit::NotAuthorizedError
+      expect(subject).to_not permit(:create)
     end
   end
 end
