74544ea7aa25e38156e54e0cd21639c109576e7a
Zeke Gabrielse <ezekg@yahoo.com>
2022-07-27 11:06:13 -0500

    add tests for product policy

--- spec/policies/product_policy_spec.rb
+++ spec/policies/product_policy_spec.rb
@@ -10,20 +10,64 @@ describe ProductPolicy, type: :policy do
   let(:product) { create(:product, account:) }
 
   context 'for admin' do
+    let(:context) { authorization_context(account:, bearer:, token:) }
+    let(:token)  { create(:token, account:, bearer:) }
+
+    context 'without permission' do
+      let(:bearer) { create(:admin, account:, permissions: []) }
+
+      it 'permits index access' do
+        expect(subject).to permit(:index)
+      end
+
+      it 'permits show access' do
+        expect(subject).to permit(:show)
+      end
+
+      it 'permits create access' do
+        expect(subject).to permit(:create)
+      end
+
+      it 'permits update access' do
+        expect(subject).to permit(:update)
+      end
+
+      it 'permits destroy access' do
+        expect(subject).to permit(:destroy)
+      end
+    end
+
+    context 'with permission' do
+      let(:bearer) { create(:admin, account:) }
+
+      it 'permits index access' do
+        expect(subject).to permit(:index)
+      end
+
+      it 'permits show access' do
+        expect(subject).to permit(:show)
+      end
+
+      it 'permits create access' do
+        expect(subject).to permit(:create)
+      end
+
+      it 'permits update access' do
+        expect(subject).to permit(:update)
+      end
+
+      it 'permits destroy access' do
+        expect(subject).to permit(:destroy)
+      end
+    end
   end
 
   context 'for product' do
-  end
-
-  context 'for license' do
-  end
-
-  context 'for user' do
     let(:context) { authorization_context(account:, bearer:, token:) }
 
-    context 'without product license' do
-      let(:bearer)  { create(:user, account:) }
-      let(:token)   { create(:token, account:, bearer:) }
+    context 'other product' do
+      let(:bearer) { create(:product, account:) }
+      let(:token)  { create(:token, account:, bearer:) }
 
       it 'denies index access' do
         expect(subject).to_not permit(:index)
@@ -35,6 +79,177 @@ describe ProductPolicy, type: :policy do
 
       it 'denies create access' do
         expect(subject).to_not permit(:create)
+      end
+
+      it 'denies update access' do
+        expect(subject).to_not permit(:update)
+      end
+
+      it 'denies destroy access' do
+        expect(subject).to_not permit(:destroy)
+      end
+    end
+
+    context 'this product' do
+      let(:bearer) { product }
+      let(:token)  { create(:token, account:, bearer:) }
+
+      it 'permits index access' do
+        expect(subject).to_not permit(:index)
+      end
+
+      it 'permits show access' do
+        expect(subject).to permit(:show)
+      end
+
+      it 'permits create access' do
+        expect(subject).to_not permit(:create)
+      end
+
+      it 'denies update access' do
+        expect(subject).to permit(:update)
+      end
+
+      it 'denies destroy access' do
+        expect(subject).to_not permit(:destroy)
+      end
+    end
+  end
+
+  context 'for license' do
+    context 'for other product' do
+      let(:bearer) { create(:license, account:) }
+
+      context 'with token' do
+        let(:token)   { create(:token, account:, bearer:) }
+        let(:context) { authorization_context(account:, bearer:, token:) }
+
+        it 'denies index access' do
+          expect(subject).to_not permit(:index)
+        end
+
+        it 'denies show access' do
+          expect(subject).to_not permit(:show)
+        end
+
+        it 'denies create access' do
+          expect(subject).to_not permit(:create)
+        end
+
+        it 'denies update access' do
+          expect(subject).to_not permit(:update)
+        end
+
+        it 'denies destroy access' do
+          expect(subject).to_not permit(:destroy)
+        end
+      end
+
+      context 'with key' do
+        let(:context) { authorization_context(account:, bearer:) }
+
+        it 'denies index access' do
+          expect(subject).to_not permit(:index)
+        end
+
+        it 'denies show access' do
+          expect(subject).to_not permit(:show)
+        end
+
+        it 'denies create access' do
+          expect(subject).to_not permit(:create)
+        end
+
+        it 'denies update access' do
+          expect(subject).to_not permit(:update)
+        end
+
+        it 'denies destroy access' do
+          expect(subject).to_not permit(:destroy)
+        end
+      end
+    end
+
+    context 'for product' do
+      let(:policy) { create(:policy, account:, product:) }
+      let(:bearer) { create(:license, account:, policy:) }
+
+      context 'with token' do
+        let(:token)   { create(:token, account:, bearer:) }
+        let(:context) { authorization_context(account:, bearer:, token:) }
+
+        it 'denies index access' do
+          expect(subject).to_not permit(:index)
+        end
+
+        it 'denies show access' do
+          expect(subject).to_not permit(:show)
+        end
+
+        it 'denies create access' do
+          expect(subject).to_not permit(:create)
+        end
+
+        it 'denies update access' do
+          expect(subject).to_not permit(:update)
+        end
+
+        it 'denies destroy access' do
+          expect(subject).to_not permit(:destroy)
+        end
+      end
+
+      context 'with key' do
+        let(:context) { authorization_context(account:, bearer:) }
+
+        it 'denies index access' do
+          expect(subject).to_not permit(:index)
+        end
+
+        it 'denies show access' do
+          expect(subject).to_not permit(:show)
+        end
+
+        it 'denies create access' do
+          expect(subject).to_not permit(:create)
+        end
+
+        it 'denies update access' do
+          expect(subject).to_not permit(:update)
+        end
+
+        it 'denies destroy access' do
+          expect(subject).to_not permit(:destroy)
+        end
+      end
+    end
+  end
+
+  context 'for user' do
+    let(:context) { authorization_context(account:, bearer:, token:) }
+
+    context 'without product license' do
+      let(:bearer) { create(:user, account:) }
+      let(:token)  { create(:token, account:, bearer:) }
+
+      it 'denies index access' do
+        expect(subject).to_not permit(:index)
+      end
+
+      it 'denies show access' do
+        expect(subject).to_not permit(:show)
+      end
+
+      it 'denies create access' do
+        expect(subject).to_not permit(:create)
+      end
+
+      it 'denies update access' do
+        expect(subject).to_not permit(:update)
+      end
+
+      it 'denies destroy access' do
+        expect(subject).to_not permit(:destroy)
       end
     end
 
@@ -55,6 +270,14 @@ describe ProductPolicy, type: :policy do
       it 'denies create access' do
         expect(subject).to_not permit(:create)
       end
+
+      it 'denies update access' do
+        expect(subject).to_not permit(:update)
+      end
+
+      it 'denies destroy access' do
+        expect(subject).to_not permit(:destroy)
+      end
     end
   end
 
@@ -71,6 +294,14 @@ describe ProductPolicy, type: :policy do
 
     it 'denies create access' do
       expect(subject).to_not permit(:create)
+    end
+
+    it 'denies update access' do
+      expect(subject).to_not permit(:update)
+    end
+
+    it 'denies destroy access' do
+      expect(subject).to_not permit(:destroy)
     end
   end
 end
