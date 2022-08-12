8a656448142b7a9114608b3799184f3d3d1d3411
Zeke Gabrielse <ezekg@yahoo.com>
2022-08-12 11:03:39 -0500

    add action policy issue #s to monkey patches

--- config/initializers/action_policy.rb
+++ config/initializers/action_policy.rb
@@ -15,12 +15,16 @@ module ActionPolicy
     # Remove memoization of authorization context. This allows us to use
     # authorized_scope() before authorize!() in controllers for nested
     # resources, e.g. /v1/licenses/:id/group.
+    #
+    # See: https://github.com/palkan/action_policy/issues/217
     def authorization_context = self.class.authorization_targets.each_with_object({}) { |(k, m), o| o[k] = send(m) }
 
     ##
     # Allow nil :record in authorize!() calls. Sometimes we want to explicitly
     # authorize nil for certain policies, e.g. License::GroupPolicy#update?.
     # This also drops logic for implicit :record, which we don't use.
+    #
+    # See: https://github.com/palkan/action_policy/issues/218
     def lookup_authorization_policy(record, **options) = policy_for(record:, **options)
   end
 end
