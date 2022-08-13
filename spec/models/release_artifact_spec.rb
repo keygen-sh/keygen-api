a81f60ac730ba31864c729a1309a77f5f315acaa
Zeke Gabrielse <ezekg@yahoo.com>
2022-08-13 15:53:16 -0500

    add more versions to sort test

--- spec/models/release_artifact_spec.rb
+++ spec/models/release_artifact_spec.rb
@@ -29,17 +29,21 @@ describe ReleaseArtifact, type: :model do
         1.0.0-beta.2
         1.0.0-beta+exp.sha.6
         1.0.0-alpha.beta
+        99.99.99
         1.0.0+20130313144700
         1.0.0-alpha
         1.0.11
         1.0.0-beta.11
         1.0.0-alpha.1
         1.0.0
+        69.420.42
         1.11.0
         1.0.0+21AF26D3
+        22.0.1-beta.0
         1.0.0-beta+exp.sha.5114f85
         1.0.0-rc.1
         1.0.0-alpha+001
+        101.0.0
         1.0.2
         1.1.3
         11.0.0
@@ -47,6 +51,8 @@ describe ReleaseArtifact, type: :model do
         1.2.0
         1.0.1
         2.0.0
+        22.0.1
+        22.0.1-beta.1
       ]
 
       releases  = versions.map { create(:release, :published, version: _1, product:, account:) }
@@ -57,6 +63,12 @@ describe ReleaseArtifact, type: :model do
       versions = described_class.order_by_version.pluck(:version)
 
       expect(versions).to eq %w[
+        101.0.0
+        99.99.99
+        69.420.42
+        22.0.1
+        22.0.1-beta.1
+        22.0.1-beta.0
         11.0.0
         2.0.0
         1.11.0
