00c7d0efb78c2b5f790b3dde92aa4fc6ae6b2493
Zeke Gabrielse <ezekg@yahoo.com>
2022-08-15 16:28:33 -0500

    refactor machine heartbeats to separate policy

---
+++ app/policies/machines/heartbeat_policy.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module Machines
+  class HeartbeatPolicy < ApplicationPolicy
+    authorize :machine
+
+    def ping?
+      verify_permissions!('machine.heartbeat.ping')
+
+      case bearer
+      in role: { name: 'admin' | 'developer' }
+        allow!
+      in role: { name: 'product' } if machine.product == bearer
+        allow!
+      in role: { name: 'user' } if machine.user == bearer
+        !machine.license.protected?
+      in role: { name: 'license' } if machine.license == bearer
+        allow!
+      else
+        deny!
+      end
+    end
+
+    def reset?
+      verify_permissions!('machine.heartbeat.reset')
+
+      case bearer
+      in role: { name: 'admin' | 'developer' | 'sales_agent' }
+        allow!
+      in role: { name: 'product' } if machine.product == bearer
+        allow!
+      else
+        deny!
+      end
+    end
+  end
+end
