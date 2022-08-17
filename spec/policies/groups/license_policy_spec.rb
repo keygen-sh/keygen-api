48060c2e9fba04bd500c101d3740a0eff792072f
Zeke Gabrielse <ezekg@yahoo.com>
2022-08-17 12:15:28 -0500

    add tests for accessing other account

--- spec/policies/groups/license_policy_spec.rb
+++ spec/policies/groups/license_policy_spec.rb
@@ -54,6 +54,18 @@ describe Groups::LicensePolicy, type: :policy do
     with_scenarios %i[accessing_another_account accessing_a_group accessing_its_licenses] do
       with_token_authentication do
         with_permissions %w[group.licenses.read] do
+          denies :index
+        end
+
+        with_wildcard_permissions { denies :index }
+        with_default_permissions  { denies :index }
+        without_permissions       { denies :index }
+      end
+    end
+
+    with_scenarios %i[accessing_another_account accessing_a_group accessing_its_license] do
+      with_token_authentication do
+        with_permissions %w[group.licenses.read] do
           denies :show
         end
 
