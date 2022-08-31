d587e40b390613c2522c187d1c3fd787900b6084
Zeke Gabrielse <ezekg@yahoo.com>
2022-08-31 11:42:27 -0500

    update usage controller to use action policy

---
+++ app/policies/licenses/usage_policy.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+module Licenses
+  class UsagePolicy < ApplicationPolicy
+    authorize :license
+
+    def increment?
+      verify_permissions!('license.usage.increment')
+
+      case bearer
+      in role: { name: 'admin' | 'developer' | 'sales_agent' | 'support_agent' }
+        allow!
+      in role: { name: 'product' } if license.product == bearer
+        allow!
+      in role: { name: 'user' } if license.user == bearer
+        !license.policy.protected?
+      in role: { name: 'license' } if license == bearer
+        allow!
+      else
+        deny!
+      end
+    end
+
+    def decrement?
+      verify_permissions!('license.usage.decrement')
+
+      case bearer
+      in role: { name: 'admin' | 'developer' | 'sales_agent' | 'support_agent' }
+        allow!
+      in role: { name: 'product' } if license.product == bearer
+        allow!
+      else
+        deny!
+      end
+    end
+
+    def reset?
+      verify_permissions!('license.usage.reset')
+
+      case bearer
+      in role: { name: 'admin' | 'developer' | 'sales_agent' | 'support_agent' }
+        allow!
+      in role: { name: 'product' } if license.product == bearer
+        allow!
+      else
+        deny!
+      end
+    end
+  end
+end
