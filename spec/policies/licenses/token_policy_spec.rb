2cc74ce8e76547fc4a0ecd7769c8414cf58f6eb8
Zeke Gabrielse <ezekg@yahoo.com>
2022-08-31 14:18:31 -0500

    add tests for license token policy

---
+++ spec/policies/licenses/token_policy_spec.rb
@@ -0,0 +1,312 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'spec_helper'
+
+describe Licenses::TokenPolicy, type: :policy do
+  subject { described_class.new(record, account:, bearer:, token:, license:) }
+
+  with_role_authorization :admin do
+    with_scenarios %i[accessing_a_license accessing_its_tokens] do
+      with_token_authentication do
+        with_permissions %w[license.tokens.read] do
+          without_token_permissions { denies :index }
+
+          allows :index
+        end
+
+        with_wildcard_permissions { allows :index }
+        with_default_permissions  { allows :index }
+        without_permissions       { denies :index }
+      end
+    end
+
+    with_scenarios %i[accessing_a_license accessing_its_token] do
+      with_token_authentication do
+        with_permissions %w[license.tokens.read] do
+          without_token_permissions { denies :show }
+
+          allows :show
+        end
+
+        with_permissions %w[license.tokens.generate] do
+          without_token_permissions { denies :create }
+
+          allows :create
+        end
+
+        with_wildcard_permissions do
+          without_token_permissions do
+            denies :show, :create
+          end
+
+          allows :show, :create
+        end
+
+        with_default_permissions do
+          without_token_permissions do
+            denies :show, :create
+          end
+
+          allows :show, :create
+        end
+
+        without_permissions do
+          denies :show, :create
+        end
+      end
+    end
+
+    with_scenarios %i[accessing_another_account accessing_a_license accessing_its_token] do
+      with_token_authentication do
+        with_permissions %w[license.tokens.read] do
+          denies :show
+        end
+
+        with_permissions %w[license.tokens.generate] do
+          denies :create
+        end
+
+        with_wildcard_permissions do
+          denies :show, :create
+        end
+
+        with_default_permissions do
+          denies :show, :create
+        end
+
+        without_permissions do
+          denies :show, :create
+        end
+      end
+    end
+  end
+
+  with_role_authorization :product do
+    with_scenarios %i[accessing_its_license accessing_its_tokens] do
+      with_token_authentication do
+        with_permissions %w[license.tokens.read] do
+          without_token_permissions { denies :index }
+
+          allows :index
+        end
+
+        with_wildcard_permissions { allows :index }
+        with_default_permissions  { allows :index }
+        without_permissions       { denies :index }
+      end
+    end
+
+    with_scenarios %i[accessing_its_license accessing_its_token] do
+      with_token_authentication do
+        with_permissions %w[license.tokens.read] do
+          without_token_permissions { denies :show }
+
+          allows :show
+        end
+
+        with_permissions %w[license.tokens.generate] do
+          without_token_permissions { denies :create }
+
+          allows :create
+        end
+
+        with_wildcard_permissions do
+          allows :show, :create
+        end
+
+        with_default_permissions do
+          allows :show, :create
+        end
+
+        without_permissions do
+          denies :show, :create
+        end
+      end
+    end
+
+    with_scenarios %i[accessing_a_license accessing_its_tokens] do
+      with_token_authentication do
+        with_permissions %w[license.tokens.read] do
+          without_token_permissions { denies :index }
+
+          denies :index
+        end
+
+        with_wildcard_permissions { denies :index }
+        with_default_permissions  { denies :index }
+        without_permissions       { denies :index }
+      end
+    end
+
+    with_scenarios %i[accessing_a_license accessing_its_token] do
+      with_token_authentication do
+        with_permissions %w[license.tokens.read] do
+          without_token_permissions { denies :show }
+
+          denies :show
+        end
+
+        with_permissions %w[license.tokens.generate] do
+          without_token_permissions { denies :create }
+
+          denies :create
+        end
+
+        with_wildcard_permissions do
+          denies :show, :create
+        end
+
+        with_default_permissions do
+          denies :show, :create
+        end
+
+        without_permissions do
+          denies :show, :create
+        end
+      end
+    end
+  end
+
+  with_role_authorization :license do
+    with_scenarios %i[accessing_itself accessing_its_tokens] do
+      with_token_authentication do
+        with_wildcard_permissions { denies :index }
+        with_default_permissions  { denies :index }
+        without_permissions       { denies :index }
+      end
+    end
+
+    with_scenarios %i[accessing_itself accessing_its_token] do
+      with_license_authentication do
+        with_wildcard_permissions do
+          denies :show, :create
+        end
+
+        with_default_permissions do
+          denies :show, :create
+        end
+
+        without_permissions do
+          denies :show, :create
+        end
+      end
+
+      with_token_authentication do
+        with_wildcard_permissions do
+          denies :show, :create
+        end
+
+        with_default_permissions do
+          denies :show, :create
+        end
+
+        without_permissions do
+          denies :show, :create
+        end
+      end
+    end
+
+    with_scenarios %i[accessing_a_license accessing_its_tokens] do
+      with_token_authentication do
+        with_wildcard_permissions { denies :index }
+        with_default_permissions  { denies :index }
+        without_permissions       { denies :index }
+      end
+    end
+
+    with_scenarios %i[accessing_a_license accessing_its_token] do
+      with_license_authentication do
+        with_wildcard_permissions do
+          denies :show, :create
+        end
+
+        with_default_permissions do
+          denies :show, :create
+        end
+
+        without_permissions do
+          denies :show, :create
+        end
+      end
+
+      with_token_authentication do
+        with_wildcard_permissions do
+          denies :show, :create
+        end
+
+        with_default_permissions do
+          denies :show, :create
+        end
+
+        without_permissions do
+          denies :show, :create
+        end
+      end
+    end
+  end
+
+  with_role_authorization :user do
+    with_scenarios %i[is_licensed accessing_its_license accessing_its_tokens] do
+      with_token_authentication do
+        with_wildcard_permissions { denies :index }
+        with_default_permissions  { denies :index }
+        without_permissions       { denies :index }
+      end
+    end
+
+    with_scenarios %i[is_licensed accessing_its_license accessing_its_token] do
+      with_token_authentication do
+        with_wildcard_permissions do
+          denies :show, :create
+        end
+
+        with_default_permissions do
+          denies :show, :create
+        end
+
+        without_permissions do
+          denies :show, :create
+        end
+      end
+    end
+
+    with_scenarios %i[accessing_a_license accessing_its_tokens] do
+      with_token_authentication do
+        with_wildcard_permissions { denies :index }
+        with_default_permissions  { denies :index }
+        without_permissions       { denies :index }
+      end
+    end
+
+    with_scenarios %i[accessing_a_license accessing_its_token] do
+      with_token_authentication do
+        with_wildcard_permissions do
+          denies :show, :create
+        end
+
+        with_default_permissions do
+          denies :show, :create
+        end
+
+        without_permissions do
+          denies :show, :create
+        end
+      end
+    end
+  end
+
+  without_authorization do
+    with_scenarios %i[accessing_a_license accessing_its_tokens] do
+      without_authentication do
+        denies :index
+      end
+    end
+
+    with_scenarios %i[accessing_a_license accessing_its_token] do
+      without_authentication do
+        denies :show, :create
+      end
+    end
+  end
+end
