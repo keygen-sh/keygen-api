28f8f62f1c3bbe91fa647e3479c62ac5242f3427
Zeke Gabrielse <ezekg@yahoo.com>
2022-09-02 12:40:50 -0500

    update license user relationship to use action policy

---
+++ app/policies/licenses/user_policy.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module Licenses
+  class UserPolicy < ApplicationPolicy
+    authorize :license
+
+    def show?
+      verify_permissions!('license.user.read')
+
+      case bearer
+      in role: { name: 'admin' | 'developer' | 'sales_agent' | 'support_agent' | 'read_only' }
+        allow!
+      in role: { name: 'product' } if license.product == bearer
+        allow!
+      in role: { name: 'user' } if license.user == bearer
+        allow!
+      in role: { name: 'license' } if license == bearer
+        allow!
+      else
+        deny!
+      end
+    end
+
+    def update?
+      verify_permissions!('license.user.update')
+
+      case bearer
+      in role: { name: 'admin' | 'developer' | 'sales_agent' }
+        allow!
+      in role: { name: 'product' } if license.product == bearer
+        allow!
+      else
+        deny!
+      end
+    end
+  end
+end
