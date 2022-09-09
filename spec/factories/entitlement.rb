65e05a0503209d64a00ce6601fd8026b20a6ee79
Zeke Gabrielse <ezekg@yahoo.com>
2022-09-09 16:08:03 -0500

    add trait system to authz tests

--- spec/factories/entitlement.rb
+++ spec/factories/entitlement.rb
@@ -2,7 +2,15 @@
 
 FactoryBot.define do
   factory :entitlement do
-    code { "#{Faker::Company.buzzword.parameterize.underscore}_#{SecureRandom.hex(2)}".upcase }
-    name { Faker::Company.industry }
+    # Prevent duplicates due to cyclic entitlement codes below. Attempting
+    # to insert duplicate codes would fail, and this prevents that.
+    initialize_with { Entitlement.find_or_initialize_by(code:) }
+
+    # Our entitlement codes cycle in sets of 5, so we can do things like
+    # constrain a release with 5 entitlements via the :with_constraints
+    # trait, and subsequently entitle a license with the same 5
+    # entitlements via the :with_entitlements trait.
+    sequence :code, %w[ALPHA BRAVO CHARLIE DELTA ECHO].cycle
+    name { code.humanize }
   end
 end
