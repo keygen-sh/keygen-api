cf46a4c006f8f33a638e2ccf9ff79628b80f697c
Zeke Gabrielse <ezekg@yahoo.com>
2022-09-13 14:42:35 -0500

    replace pundit with action policy

--- app/policies/plan_policy.rb
+++ app/policies/plan_policy.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class PlanPolicy < ApplicationPolicy
+  skip_pre_check :verify_authenticated!
+
+  authorize :account, allow_nil: true
+
   def index? = true
   def show?  = true
 end
