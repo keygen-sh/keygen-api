60da6f625629c6f9549d2db7ad2de592bba6cc4e
Zeke Gabrielse <ezekg@yahoo.com>
2022-09-14 23:43:16 -0500

    fix broken second factor tests

--- spec/policies/users/second_factor_policy_spec.rb
+++ spec/policies/users/second_factor_policy_spec.rb
@@ -48,17 +48,13 @@ describe Users::SecondFactorPolicy, type: :policy do
         end
 
         with_wildcard_permissions do
-          without_token_permissions do
-            denies :show, :create, :update, :destroy
-          end
+          without_token_permissions { denies :show, :create, :update, :destroy }
 
           allows :show, :create, :update, :destroy
         end
 
         with_default_permissions do
-          without_token_permissions do
-            denies :show, :create, :update, :destroy
-          end
+          without_token_permissions { denies :show, :create, :update, :destroy }
 
           allows :show, :create, :update, :destroy
         end
@@ -72,11 +68,13 @@ describe Users::SecondFactorPolicy, type: :policy do
     with_scenarios %i[accessing_a_user accessing_its_second_factors] do
       with_token_authentication do
         with_permissions %w[user.second-factors.read] do
-          denies :index
+          without_token_permissions { denies :index }
+
+          allows :index
         end
 
-        with_wildcard_permissions { denies :index }
-        with_default_permissions  { denies :index }
+        with_wildcard_permissions { allows :index }
+        with_default_permissions  { allows :index }
         without_permissions       { denies :index }
       end
     end
@@ -84,27 +82,35 @@ describe Users::SecondFactorPolicy, type: :policy do
     with_scenarios %i[accessing_a_user accessing_its_second_factor] do
       with_token_authentication do
         with_permissions %w[user.second-factors.read] do
-          denies :show
+          without_token_permissions { denies :show }
+
+          allows :show
         end
 
         with_permissions %w[user.second-factors.create] do
-          denies :create
+          without_token_permissions { denies :create }
+
+          allows :create
         end
 
         with_permissions %w[user.second-factors.update] do
-          denies :update
+          without_token_permissions { denies :update }
+
+          allows :update
         end
 
         with_permissions %w[user.second-factors.delete] do
-          denies :destroy
+          without_token_permissions { denies :destroy }
+
+          allows :destroy
         end
 
         with_wildcard_permissions do
-          denies :show, :create, :update, :destroy
+          allows :show, :create, :update, :destroy
         end
 
         with_default_permissions do
-          denies :show, :create, :update, :destroy
+          allows :show, :create, :update, :destroy
         end
 
         without_permissions do
