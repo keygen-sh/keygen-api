a9c80e493d80ca6093f29c452bbba9d594aa6d61
Zeke Gabrielse <ezekg@yahoo.com>
2022-09-21 09:33:12 -0500

    update action policy and remove monkey patches

--- config/initializers/action_policy.rb
+++ config/initializers/action_policy.rb
@@ -9,25 +9,3 @@ end
 
 # Lookup chain should fallback to nil policy.
 ActionPolicy::LookupChain.chain << -> * { NilClassPolicy }
-
-##
-# Monkey patches for Action Policy.
-module ActionPolicy
-  module Behaviour
-    ##
-    # Remove memoization of authorization context. This allows us to use
-    # authorized_scope() before authorize!() in controllers for nested
-    # resources, e.g. /v1/licenses/:id/group.
-    #
-    # See: https://github.com/palkan/action_policy/issues/217
-    def authorization_context = self.class.authorization_targets.each_with_object({}) { |(k, m), o| o[k] = send(m) }
-
-    ##
-    # Allow nil :record in authorize!() calls. Sometimes we want to explicitly
-    # authorize nil for certain policies, e.g. License::GroupPolicy#update?.
-    # This also drops logic for implicit :record, which we don't use.
-    #
-    # See: https://github.com/palkan/action_policy/issues/218
-    def lookup_authorization_policy(record, **options) = policy_for(record:, **options)
-  end
-end
