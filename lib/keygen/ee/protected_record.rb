d63eae6014c00dc2bd47537682ec8713f5440269
Zeke Gabrielse <ezekg@yahoo.com>
2022-10-19 10:56:21 -0500

    update error message

--- lib/keygen/ee/protected_record.rb
+++ lib/keygen/ee/protected_record.rb
@@ -8,7 +8,7 @@ module Keygen
       module ClassMethods
         [:find_by_sql, :async_find_by_sql, :count_by_sql, :async_find_by_sql, :all, *ActiveRecord::Querying::QUERYING_METHODS].each do |method|
           define_method method do |*args, **kwargs|
-            raise ProtectedRecordError, "Querying model #{name} is not available in Keygen CE. Please upgrade to Keygen EE." if
+            raise ProtectedRecordError, "Querying with #{name}.#{method} is not available in Keygen CE. Please upgrade to Keygen EE." if
               Keygen.console? && Keygen.ce?
 
             super(*args, **kwargs)
