6f57b017924a3b0b55062d685c6e6e5f2fc0ce89
Zeke Gabrielse <ezekg@yahoo.com>
2022-10-21 10:05:01 -0500

    fix keygen module self refs

--- lib/keygen.rb
+++ lib/keygen.rb
@@ -9,17 +9,19 @@ require_relative 'keygen/middleware'
 module Keygen
   PUBLIC_KEY = %(\xB8\xF3\xEBL\xD2`\x13_g\xA5\tn\x8D\xC1\xC9\xB9\xDC\xB8\x1E\xE9\xFEP\xD1,\xDC\xD9A\xF6`z\x901).freeze
 
-  def self.console? = Rails.const_defined?(:Console)
+  class << self
+    def console? = Rails.const_defined?(:Console)
 
-  def self.ce? = !lic? && !key?
-  def self.ee? = !ce? && EE.license.valid?
+    def ce? = !lic? && !key?
+    def ee? = !ce? && EE.license.valid?
 
-  def self.ee(&)
-    yield EE.license if ee?
+    def ee(&)
+      yield EE.license if ee?
+    end
+
+    private
+
+    def lic? = ENV.key?('KEYGEN_LICENSE_FILE_PATH') || ENV.key?('KEYGEN_LICENSE_FILE')
+    def key? = ENV.key?('KEYGEN_LICENSE_KEY')
   end
-
-  private
-
-  def lic? = ENV.key?('KEYGEN_LICENSE_FILE_PATH') || ENV.key?('KEYGEN_LICENSE_FILE')
-  def key? = ENV.key?('KEYGEN_LICENSE_KEY')
 end
