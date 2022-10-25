2961aa7b10710811906fe246bffc18dee0262d54
Zeke Gabrielse <ezekg@yahoo.com>
2022-10-25 15:05:45 -0500

    refactor and add tests for protected records

--- lib/keygen/ee/protected_methods.rb
+++ lib/keygen/ee/protected_methods.rb
@@ -5,65 +5,59 @@ module Keygen
     module ProtectedMethods
       class MethodProxy
         def initialize(singleton_methods:, instance_methods:, entitlements:)
-          @singleton_methods = singleton_methods.reduce({}) { |h, v| h.merge(v => false) }
-          @instance_methods  = instance_methods.reduce({}) { |h, v| h.merge(v => false) }
+          @singleton_methods = singleton_methods.reduce({}) { |h, v| h.merge(v => nil) }
+          @instance_methods  = instance_methods.reduce({}) { |h, v| h.merge(v => nil) }
           @entitlements      = entitlements
         end
 
         def run_protected_singleton_method(method, ...)
-          raise ProtectedMethodError, "Calling #{method.owner.name}.#{method.name} is not available in Keygen CE. Please upgrade to Keygen EE." if
+          raise ProtectedMethodError, "Calling #{method.receiver.name}.#{method.name} is not available in Keygen CE. Please upgrade to Keygen EE." if
             Keygen.ce?
 
-          raise ProtectedMethodError, "Calling #{method.owner.name}.#{method.name} is not allowed. Please upgrade Keygen EE." unless
+          raise ProtectedMethodError, "Calling #{method.receiver.name}.#{method.name} is not allowed. Please upgrade Keygen EE." unless
             Keygen.ee { _1.entitled?(*entitlements) }
 
           method.call(...)
         end
 
         def run_protected_instance_method(method, ...)
-          raise ProtectedMethodError, "Calling #{method.owner.name}##{method.name} is not available in Keygen CE. Please upgrade to Keygen EE." if
+          raise ProtectedMethodError, "Calling #{method.receiver.class.name}##{method.name} is not available in Keygen CE. Please upgrade to Keygen EE." if
             Keygen.ce?
 
-          raise ProtectedMethodError, "Calling #{method.owner.name}##{method.name} is not allowed. Please upgrade Keygen EE." unless
+          raise ProtectedMethodError, "Calling #{method.receiver.class.name}##{method.name} is not allowed. Please upgrade Keygen EE." unless
             Keygen.ee { _1.entitled?(*entitlements) }
 
           method.call(...)
         end
 
         def protected_singleton_method?(method)
-          singleton_methods.key?(method)
+          singleton_methods.key?(method.name)
         end
 
         def protected_instance_method?(method)
-          instance_methods.key?(method)
+          instance_methods.key?(method.name)
         end
 
         def proxied_singleton_method?(method)
-          singleton_methods.fetch(method) { false }
+          singleton_methods[method.name] == method
         end
 
         def proxied_instance_method?(method)
-          instance_methods.fetch(method) { false }
+          instance_methods[method.name] == method
         end
 
-        def proxied_singleton_method!(method)
+        def proxy_singleton_method!(method)
           raise ArgumentError, "method #{method} is not a protected method" unless
             protected_singleton_method?(method)
 
-          raise ArgumentError, "method #{method} is already proxied" if
-            proxied_singleton_method?(method)
-
-          singleton_methods[method] = true
+          singleton_methods[method.name] = method
         end
 
-        def proxied_instance_method!(method)
+        def proxy_instance_method!(method)
           raise ArgumentError, "method #{method} is not a protected method" unless
             protected_instance_method?(method)
 
-          raise ArgumentError, "method #{method} is already proxied" if
-            proxied_instance_method?(method)
-
-          instance_methods[method] = true
+          instance_methods[method.name] = method
         end
 
         private
@@ -75,69 +69,169 @@ module Keygen
 
       module MethodBouncer
         def instrument_protected_methods!(singleton_methods:, instance_methods:, entitlements:)
-          @protected_method_proxy ||= MethodProxy.new(
+          @protected_singleton_methods = singleton_methods
+          @protected_instance_methods  = instance_methods
+          @protected_method_proxy    ||= MethodProxy.new(
             singleton_methods:,
             instance_methods:,
             entitlements:,
           )
+
+          protect_singleton_methods!
+          protect_instance_methods!
         end
 
         private
 
-        def singleton_method_added(method)
+        ##
+        # protect_singleton_methods! decorates singleton methods added before our module was included.
+        def protect_singleton_methods!
           proxy = @protected_method_proxy
 
+          @protected_singleton_methods.each do |method_name|
+            next unless
+              singleton_methods.include?(method_name)
+
+            method = method(method_name)
+            next unless
+              proxy.protected_singleton_method?(method)
+
+            redefine_singleton_method(method_name) do |*args, **kwargs|
+              proxy.run_protected_singleton_method(
+                method,
+                *args,
+                **kwargs,
+              )
+            end
+          end
+        end
+
+        ##
+        # protect_instance_methods! decorates instance methods added before our module was included.
+        def protect_instance_methods!
+          proxy = @protected_method_proxy
+
+          @protected_instance_methods.each do |method_name|
+            next unless
+              instance_methods.include?(method_name)
+
+            method = instance_method(method_name)
+            next unless
+              proxy.protected_instance_method?(method)
+
+            redefine_instance_method(method_name) do |*args, **kwargs|
+              bound_method = method.bind(self)
+
+              proxy.run_protected_instance_method(
+                bound_method,
+                *args,
+                **kwargs,
+              )
+            end
+          end
+        end
+
+        ##
+        # singleton_method_added decorates singleton methods added after our module has been included.
+        def singleton_method_added(method_name)
+          method = method(method_name)
+          return super unless
+            method.present?
+
+          proxy = @protected_method_proxy
           return super unless
             proxy.protected_singleton_method?(method)
 
           return super if
             proxy.proxied_singleton_method?(method)
 
-          original_method = method(method)
-
-          proxy.proxied_singleton_method!(method)
-
-          define_singleton_method method do |*args, **kwargs|
-            proxy.run_protected_singleton_method(
-              original_method,
-              *args,
-              **kwargs,
-            )
-          end
+          proxy.proxy_singleton_method!(
+            redefine_singleton_method(method_name) { |*args, **kwargs|
+              proxy.run_protected_singleton_method(
+                method,
+                *args,
+                **kwargs,
+              )
+            },
+          )
 
           super
         end
 
-        def method_added(method)
-          proxy = @protected_method_proxy
+        ##
+        # method_added decorates instance methods added after our module has been included.
+        def method_added(method_name)
+          method = instance_method(method_name)
+          return super unless
+            method.present?
 
+          proxy = @protected_method_proxy
           return super unless
             proxy.protected_instance_method?(method)
 
           return super if
             proxy.proxied_instance_method?(method)
 
-          original_method = instance_method(method)
+          proxy.proxy_instance_method!(
+            redefine_instance_method(method_name) { |*args, **kwargs|
+              bound_method = method.bind(self)
 
-          proxy.proxied_instance_method!(method)
-
-          define_method method do |*args, **kwargs|
-            bound_method = original_method.bind(self)
-
-            proxy.run_protected_instance_method(
-              bound_method,
-              *args,
-              **kwargs,
-            )
-          end
+              proxy.run_protected_instance_method(
+                bound_method,
+                *args,
+                **kwargs,
+              )
+            },
+          )
 
           super
+        end
+
+        ##
+        # redefine_singleton_method allows a non-cylic redefinition of a singleton method.
+        def redefine_singleton_method(method_name, &)
+          @_singleton_methods ||= {}
+
+          return method(method_name) if
+            @_singleton_methods[method_name]
+
+          begin
+            @_singleton_methods[method_name] = true
+
+            define_singleton_method(method_name, &)
+          ensure
+            @_singleton_methods[method_name] = false
+          end
+
+          method(method_name)
+        end
+
+        ##
+        # redefine_instance_method allows a non-cylic redefinition of an instance method.
+        def redefine_instance_method(method_name, &)
+          @_instance_methods ||= {}
+
+          return instance_method(method_name) if
+            @_instance_methods[method_name]
+
+          begin
+            @_instance_methods[method_name] = true
+
+            define_method(method_name, &)
+          ensure
+            @_instance_methods[method_name] = false
+          end
+
+          instance_method(method_name)
         end
       end
 
       def self.[](*methods, singleton_methods: methods, instance_methods: methods, entitlements: [])
         raise ArgumentError, 'cannot use both positional and keyword arguments for methods' if
           methods.any? && (singleton_methods != methods || instance_methods != methods)
+
+        raise ArgumentError, 'must provide at least 1 method' if
+          singleton_methods.empty? && instance_methods.empty?
 
         Module.new do
           next unless
@@ -153,6 +247,10 @@ module Keygen
             )
           end
         end
+      end
+
+      def self.included(klass)
+        raise NotImplementedError, 'must be included like ProtectedMethods[...]'
       end
     end
   end
--- spec/lib/keygen/ee/protected_record_spec.rb
+++ spec/lib/keygen/ee/protected_record_spec.rb
@@ -10,72 +10,89 @@ describe Keygen::EE::ProtectedRecord, type: :ee do
     Class.new do
       include Keygen::EE::ProtectedRecord
 
-      def self.ok? = nil
-      def ok?      = nil
+      Keygen::EE::ProtectedRecord::SINGLETON_METHODS.each do |method|
+        define_singleton_method method do |*args, **kwargs|
+          nil
+        end
+      end
+
+      Keygen::EE::ProtectedRecord::INSTANCE_METHODS.each do |method|
+        define_method method do |*args, **kwargs|
+          nil
+        end
+      end
     end
   end
 
-  it 'should not protect the singleton record' do
-    expect { subject.ok? }.to_not raise_error
+  Keygen::EE::ProtectedRecord::SINGLETON_METHODS.each do |method|
+    it "should allow querying the record with .#{method}" do
+      expect { subject.send(method) }.to_not raise_error
+    end
   end
 
-  it 'should not protect an instance record' do
-    expect { subject.new.ok? }.to_not raise_error
+  Keygen::EE::ProtectedRecord::INSTANCE_METHODS.each do |method|
+    it "should allow querying the record with ##{method}" do
+      expect { subject.new.send(method) }.to_not raise_error
+    end
   end
 
   within_console do
     within_ce do
-      it 'should protect the singleton record' do
-        expect { subject.ok? }.to raise_error Keygen::EE::ProtectedMethodError
+      Keygen::EE::ProtectedRecord::SINGLETON_METHODS.each do |method|
+        it "should block querying the record with .#{method}" do
+          expect { subject.send(method) }.to raise_error Keygen::EE::ProtectedMethodError
+        end
       end
 
-      it 'should protect an instance record' do
-        expect { subject.new.ok? }.to raise_error Keygen::EE::ProtectedMethodError
+      Keygen::EE::ProtectedRecord::INSTANCE_METHODS.each do |method|
+        it "should block querying the record with ##{method}" do
+          expect { subject.new.send(method) }.to raise_error Keygen::EE::ProtectedMethodError
+        end
       end
     end
 
     within_ee do
-      it 'should not protect the singleton record' do
-        expect { subject.ok? }.to_not raise_error
+      Keygen::EE::ProtectedRecord::SINGLETON_METHODS.each do |method|
+        it "should allow querying the record with .#{method}" do
+          expect { subject.send(method) }.to_not raise_error
+        end
       end
 
-      it 'should not protect an instance record' do
-        expect { subject.new.ok? }.to_not raise_error
+      Keygen::EE::ProtectedRecord::INSTANCE_METHODS.each do |method|
+        it "should allow querying the record with ##{method}" do
+          expect { subject.new.send(method) }.to_not raise_error
+        end
       end
     end
 
-    context 'with entitlements' do
-      within_ce do
-        it 'should block protected record' do
-          klass = Class.new do
-            include Keygen::EE::ProtectedRecord[entitlements: %i[test]]
+    context 'when record is protected with entitlements' do
+      subject do
+        Class.new do
+          include Keygen::EE::ProtectedRecord[entitlements: %i[test]]
 
-            def self.ok? = nil
-          end
-
-          expect { klass.ok? }.to raise_error Keygen::EE::ProtectedMethodError
+          def self.all = nil
+          def reload   = nil
         end
       end
 
-      within_ee do
-        it 'should allow protected record when entitled' do
-          klass = Class.new do
-            include Keygen::EE::ProtectedRecord[entitlements: %i[test]]
-
-            def ok? = nil
-          end
-
-          expect { subject.new.ok? }.to_not raise_error
+      within_ce do
+        it 'should block querying protected record' do
+          expect { subject.new.reload }.to raise_error Keygen::EE::ProtectedMethodError
+          expect { subject.all }.to raise_error Keygen::EE::ProtectedMethodError
         end
+      end
 
-        it 'should block protected record when not entitled' do
-          klass = Class.new do
-            include Keygen::EE::ProtectedRecord[entitlements: %i[other]]
+      within_ee entitlements: %i[test] do
+        it 'should allow querying protected record when entitled' do
+          expect { subject.new.reload }.to_not raise_error
+          expect { subject.all }.to_not raise_error
+        end
+      end
 
-            def self.ok? = nil
-          end
-
-          expect { subject.ok? }.to raise_error Keygen::EE::ProtectedMethodError
+      within_ee entitlements: [] do
+        it 'should block querying protected record when unentitled' do
+          expect { subject.new.reload }.to raise_error Keygen::EE::ProtectedMethodError
+          expect { subject.all }.to raise_error Keygen::EE::ProtectedMethodError
         end
       end
     end
