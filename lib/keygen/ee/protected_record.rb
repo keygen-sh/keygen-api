1bdb7b49713afcfb5f26d3c17af14a5f810e57a4
Zeke Gabrielse <ezekg@yahoo.com>
2022-10-25 15:05:45 -0500

    refactor and add tests for protected records

--- lib/keygen/ee/protected_record.rb
+++ lib/keygen/ee/protected_record.rb
@@ -2,48 +2,27 @@
 
 module Keygen
   module EE
-    class ProtectedRecordError < StandardError; end
-
     module ProtectedRecord
-      QUERYING_METHODS = [
-        *ActiveRecord::Querying::QUERYING_METHODS,
-        :find_by_sql,
-        :async_find_by_sql,
-        :count_by_sql,
-        :async_find_by_sql,
-        :all,
-      ].freeze
+      SINGLETON_METHODS = [:find_by_sql, :async_find_by_sql, :count_by_sql, :async_find_by_sql, :all, *ActiveRecord::Querying::QUERYING_METHODS].freeze
+      INSTANCE_METHODS  = %i[reload].freeze
 
-      module RecordBouncer
-        def instrument_protected_record!(entitlements:)
-          QUERYING_METHODS.each do |method|
-            define_method method do |*args, **kwargs|
-              raise ProtectedRecordError, "Querying #{name}.#{method} is not available in Keygen CE. Please upgrade to Keygen EE." if
-                Keygen.console? && Keygen.ce?
+      def self.[](entitlements: [])
+        Module.new do
+          next unless
+            Keygen.console?
 
-              raise ProtectedRecordError, "Querying #{name}.#{method} is not allowed. Please upgrade Keygen EE." if
-                Keygen.console? && Keygen.ee { !_1.entitled?(*entitlements) }
-
-              super(*args, **kwargs)
-            end
+          define_singleton_method :included do |klass|
+            klass.include ProtectedMethods[
+              singleton_methods: SINGLETON_METHODS,
+              instance_methods: INSTANCE_METHODS,
+              entitlements:,
+            ]
           end
         end
       end
 
-      def self.[](*methods, singleton_methods: methods, instance_methods: methods, entitlements: [])
-        raise ArgumentError, 'cannot use both positional and keyword arguments' if
-          methods.any? && singleton_methods != methods ||
-                          instance_methods != methods
-
-        Module.new do
-          define_singleton_method :included do |klass|
-            klass.extend RecordBouncer
-
-            klass.instrument_protected_record!(
-              entitlements:,
-            )
-          end
-        end
+      def self.included(klass)
+        klass.include ProtectedRecord[]
       end
     end
   end
--- spec/lib/keygen/ee/protected_methods_spec.rb
+++ spec/lib/keygen/ee/protected_methods_spec.rb
@@ -22,55 +22,63 @@ describe Keygen::EE::ProtectedMethods, type: :ee do
       .to raise_error ArgumentError
   end
 
-  it 'should not protect a predefined set of singleton methods' do
+  it 'should not include the module outside of a console environment' do
+    expect(subject.ancestors).to_not include Keygen::EE::ProtectedMethods::MethodBouncer
+  end
+
+  it 'should not block a predefined set of singleton methods' do
     expect { subject.foo }.to_not raise_error
   end
 
-  it 'should not protect other singleton methods' do
+  it 'should not block other singleton methods' do
     expect { subject.bar }.to_not raise_error
   end
 
-  it 'should not protect a predefined set of instance methods' do
+  it 'should not block a predefined set of instance methods' do
     expect { subject.new.baz }.to_not raise_error
   end
 
-  it 'should not protect other instance methods' do
+  it 'should not block other instance methods' do
     expect { subject.new.qux }.to_not raise_error
   end
 
   within_console do
+    it 'should include the module outside of a console environment' do
+      expect(subject.ancestors).to include Keygen::EE::ProtectedMethods::MethodBouncer
+    end
+
     within_ce do
-      it 'should protect a predefined set of singleton methods' do
+      it 'should block a predefined set of singleton methods' do
         expect { subject.foo }.to raise_error Keygen::EE::ProtectedMethodError
       end
 
-      it 'should not protect other singleton methods' do
+      it 'should allow other singleton methods' do
         expect { subject.bar }.to_not raise_error
       end
 
-      it 'should protect a predefined set of instance methods' do
+      it 'should block a predefined set of instance methods' do
         expect { subject.new.baz }.to raise_error Keygen::EE::ProtectedMethodError
       end
 
-      it 'should not protect other instance methods' do
+      it 'should allow other instance methods' do
         expect { subject.new.qux }.to_not raise_error
       end
     end
 
     within_ee do
-      it 'should not protect a predefined set of singleton methods' do
+      it 'should allow a predefined set of singleton methods' do
         expect { subject.foo }.to_not raise_error
       end
 
-      it 'should not protect other singleton methods' do
+      it 'should allow other singleton methods' do
         expect { subject.bar }.to_not raise_error
       end
 
-      it 'should not protect a predefined set of instance methods' do
+      it 'should allow a predefined set of instance methods' do
         expect { subject.new.baz }.to_not raise_error
       end
 
-      it 'should not protect other instance methods' do
+      it 'should allow other instance methods' do
         expect { subject.new.qux }.to_not raise_error
       end
     end
@@ -137,6 +145,30 @@ describe Keygen::EE::ProtectedMethods, type: :ee do
       end
     end
 
+    context 'when using inheritance' do
+      subject do
+        parent = Class.new do
+          def self.foo = nil
+        end
+
+        Class.new(parent) do
+          include Keygen::EE::ProtectedMethods[:foo, :baz]
+        end
+      end
+
+      within_ce do
+        it 'should block inherited method' do
+          expect { subject.foo }.to raise_error Keygen::EE::ProtectedMethodError
+        end
+      end
+
+      within_ee do
+        it 'should allow inherited method' do
+          expect { subject.foo }.to_not raise_error
+        end
+      end
+    end
+
     context 'when methods are protected with entitlements' do
       subject do
         Class.new do
@@ -159,8 +191,43 @@ describe Keygen::EE::ProtectedMethods, type: :ee do
       end
 
       within_ee entitlements: [] do
-        it 'should block protected method when not entitled' do
+        it 'should block protected method when unentitled' do
           expect { subject.new.foo }.to raise_error Keygen::EE::ProtectedMethodError
+        end
+      end
+    end
+  end
+
+  context 'bindings' do
+    subject do
+      Class.new do
+        include Keygen::EE::ProtectedMethods[:v]
+
+        def initialize(v) = @v = v
+        def v = @v
+      end
+    end
+
+    let(:t1) { subject.new(1) }
+    let(:t2) { subject.new(2) }
+
+    it 'should not fubar method bindings' do
+      expect(t1.v).to eq 1
+      expect(t2.v).to eq 2
+    end
+
+    within_console do
+      within_ce do
+        it 'should bind instance methods to the correct object' do
+          expect { t1.v }.to raise_error Keygen::EE::ProtectedMethodError
+          expect { t2.v }.to raise_error Keygen::EE::ProtectedMethodError
+        end
+      end
+
+      within_ee do
+        it 'should bind instance methods to the correct object' do
+          expect(t1.v).to eq 1
+          expect(t2.v).to eq 2
         end
       end
     end
