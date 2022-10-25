a896c40576f305e048a3c5101a7e1a2703f23f55
Zeke Gabrielse <ezekg@yahoo.com>
2022-10-25 16:26:51 -0500

    add tests for keygen lib

--- spec/lib/keygen/ee/router_spec.rb
+++ spec/lib/keygen/ee/router_spec.rb
@@ -6,4 +6,23 @@ require 'spec_helper'
 require_dependency Rails.root.join('lib', 'keygen')
 
 describe Keygen::EE::Router, type: :ee do
+  subject { Keygen::EE::Router::Constraint }
+
+  let(:req) { Rack::Request.new(Rack::MockRequest.env_for('/v1/licenses')) }
+
+  within_ce do
+    it 'should return false in a CE env' do
+      constraint = subject.new
+
+      expect(constraint.matches?(req)).to be false
+    end
+  end
+
+  within_ee do
+    it 'should return true in an EE env' do
+      constraint = subject.new
+
+      expect(constraint.matches?(req)).to be true
+    end
+  end
 end
--- spec/lib/keygen_spec.rb
+++ spec/lib/keygen_spec.rb
@@ -6,4 +6,57 @@ require 'spec_helper'
 require_dependency Rails.root.join('lib', 'keygen')
 
 describe Keygen, type: :ee do
+  describe '.console?' do
+    within_console do
+      it 'should return true in a console env' do
+        expect(Keygen.console?).to be true
+      end
+    end
+
+    it 'should return false in another env' do
+      expect(Keygen.console?).to be false
+    end
+  end
+
+  describe '.ce?' do
+    within_ce do
+      it 'should return true in a CE env' do
+        expect(Keygen.ce?).to be true
+      end
+    end
+
+    within_ee do
+      it 'should return false in an EE env' do
+        expect(Keygen.ce?).to be false
+      end
+    end
+  end
+
+  describe '.ee?' do
+    within_ce do
+      it 'should return false in a CE env' do
+        expect(Keygen.ee?).to be false
+      end
+    end
+
+    within_ee do
+      it 'should return true in an EE env' do
+        expect(Keygen.ee?).to be true
+      end
+    end
+  end
+
+  describe '.ee' do
+    within_ce do
+      it 'should not call the block in a CE env' do
+        expect(Keygen.ee { 1 }).to be nil
+      end
+    end
+
+    within_ee do
+      it 'should call the block in an EE env' do
+        expect(Keygen.ee { 1 }).to be 1
+      end
+    end
+  end
 end
