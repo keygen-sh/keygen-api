05c94107a7569a80fb5c0832ad3345ea40284e31
Zeke Gabrielse <ezekg@yahoo.com>
2022-11-04 09:10:56 -0500

    fix s3 region

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -10,6 +10,6 @@ AWS_REGION            = ENV.fetch('AWS_REGION')
 AWS_CLIENT_OPTIONS = {
   access_key_id: AWS_ACCESS_KEY_ID,
   secret_access_key: AWS_SECRET_ACCESS_KEY,
-  endpoint: "https://#{AWS_BUCKET}.s3.us-east-2.amazonaws.com",
+  endpoint: "https://#{AWS_BUCKET}.s3.#{AWS_REGION}.amazonaws.com",
   region: AWS_REGION,
 }.freeze
