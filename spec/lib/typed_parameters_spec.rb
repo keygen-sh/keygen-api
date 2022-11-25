df18c4ea5dc3ca42726878dce1b5f17d8c23d7f8
Zeke Gabrielse <ezekg@yahoo.com>
2022-11-25 16:48:55 -0600

    add tests for pipeline

--- spec/lib/typed_parameters_spec.rb
+++ spec/lib/typed_parameters_spec.rb
@@ -699,6 +699,21 @@ describe TypedParameters do
     end
   end
 
+  describe TypedParameters::Pipeline do
+    it 'should reduce the pipeline steps in order' do
+      pipeline = TypedParameters::Pipeline.new
+      input    = 1
+
+      pipeline << -> v { v += 1 }
+      pipeline << -> v { v -= 2 }
+      pipeline << -> v { v *= 3 }
+
+      output = pipeline.call(input)
+
+      expect(output).to eq 0
+    end
+  end
+
   # # Quick and dirty mock controller context for a request
   # def request(params = {}, action = :create)
   #   OpenStruct.new(
