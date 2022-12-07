198defa519eb743377f16fb46bd252d8a084f680
Zeke Gabrielse <ezekg@yahoo.com>
2022-12-07 15:03:52 -0600

    add formatter comments

--- lib/typed_parameters/formatters/rails.rb
+++ lib/typed_parameters/formatters/rails.rb
@@ -4,6 +4,18 @@ require_relative 'formatter'
 
 module TypedParameters
   module Formatters
+    ##
+    # The Rails formatter wraps the params in a key matching the current
+    # controller's name.
+    #
+    # For example, in a UsersController context, given the params:
+    #
+    #   { email: 'foo@bar.example' }
+    #
+    # The final params would become:
+    #
+    #   { user: { email: 'foo@bar.example' } }
+    #
     module Rails
       def self.call(key, value, controller:)
         key = controller.controller_name.singularize.to_sym
