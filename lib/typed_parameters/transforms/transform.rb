c69240a0297de94ffb1d047b05f742e1d141a710
Zeke Gabrielse <ezekg@yahoo.com>
2022-12-09 09:00:16 -0600

    remove transform calls with variable arity

--- lib/typed_parameters/transforms/transform.rb
+++ lib/typed_parameters/transforms/transform.rb
@@ -4,9 +4,6 @@ module TypedParameters
   module Transforms
     class Transform
       def call(key, value) = raise NotImplementedError
-
-      # FIXME(ezekg) Is there a cleaner way of delegating arity?
-      def arity = method(:call).arity
     end
   end
 end
