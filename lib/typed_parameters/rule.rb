5c2ce3c969fb2dbd468b9d3e51e0f17228b0bb8f
Zeke Gabrielse <ezekg@yahoo.com>
2022-12-09 09:21:18 -0600

    add comment on dfs

--- lib/typed_parameters/rule.rb
+++ lib/typed_parameters/rule.rb
@@ -14,12 +14,38 @@ module TypedParameters
     attr_reader :controller,
                 :schema
 
+    ##
+    # depth_first_map performs a postorder DFS-like traversal algorithm
+    # over the params. A postorder DFS starts at the leftmost leaf, and
+    # works its way through to the rightmost sibling, then it backtracks
+    # to the parent node and performs the same all the way up the tree
+    # until it reaches the root.
+    #
+    # The algorithm is used to perform bouncing, coercing, validations
+    # and transforms. For example, with transforms, this ensures that
+    # the node's children are transformed before the parent.
+    #
+    # Visualized, the traversal algorithm would look like this:
+    #
+    #                  ┌───┐
+    #                  │ 9 │
+    #                  └─┬─┘
+    #                    │
+    #                  ┌─▼─┐
+    #          ┌────┬──┤ 8 ├───────┐
+    #          │    │  └─┬─┘       │
+    #          │    │    │         │
+    #        ┌─▼─┐┌─▼─┐┌─▼─┐     ┌─▼─┐
+    #     ┌──┤ 3 ││ 4 ││ 6 │     │ 7 │
+    #     │  └─┬─┘└───┘└─┬─┘     └───┘
+    #     │    │         │
+    #   ┌─▼─┐┌─▼─┐     ┌─▼─┐
+    #   │ 1 ││ 2 │     │ 5 │
+    #   └───┘└───┘     └───┘
+    #
     def depth_first_map(param, &)
       return if param.nil?
 
-      # Traverse down the param tree until we hit the end of a "branch",
-      # then start validating in from there, moving from the outside in.
-      # This is kind of like a depth first search.
       if param.schema.children&.any?
         case param.schema.children
         when Array
