7fe0e947ba9e510a19fa9e7945d8ab9f89640285
Zeke Gabrielse <ezekg@yahoo.com>
2022-12-14 10:37:58 -0600

    fix issue with blank integer params being coerced to 0

--- lib/typed_parameters/types/integer.rb
+++ lib/typed_parameters/types/integer.rb
@@ -3,7 +3,7 @@
 module TypedParameters
   module Types
     register(:integer,
-      coerce: -> v { v.to_i },
+      coerce: -> v { v.blank? ? nil : v.to_i },
       match: -> v { v.is_a?(Integer) },
     )
   end
