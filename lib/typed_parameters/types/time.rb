7eb427f4ca084b2de5f2dc877a96cee82a4fab7d
Zeke Gabrielse <ezekg@yahoo.com>
2022-12-15 09:34:35 -0600

    fix date and time coercions

--- lib/typed_parameters/types/time.rb
+++ lib/typed_parameters/types/time.rb
@@ -3,8 +3,18 @@
 module TypedParameters
   module Types
     register(:time,
-      coerce: -> v { v.to_s.match?(/\A\d+\z/) ? Time.at(v.to_i) : v.to_time },
       match: -> v { v.is_a?(Time) },
+      coerce: -> v {
+        return nil if
+          v.blank?
+
+        case
+        when v.to_s.match?(/\A\d+\z/)
+          Time.at(v.to_i)
+        else
+          v.to_time
+        end
+      },
     )
   end
 end
