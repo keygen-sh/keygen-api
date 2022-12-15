b8dda3d76437936125e3791da1824595a26b5dc3
Zeke Gabrielse <ezekg@yahoo.com>
2022-12-15 17:08:24 -0600

    add config to ignore optional nils

--- lib/typed_parameters/configuration.rb
+++ lib/typed_parameters/configuration.rb
@@ -5,6 +5,12 @@ module TypedParameters
     include ActiveSupport::Configurable
 
     ##
+    # ignore_nil_optionals defines how nil optionals are handled.
+    # When enabled, optional params that are nil will be ignored if
+    # the schema does not allow_nil.
+    config_accessor(:ignore_nil_optionals) { false }
+
+    ##
     # path_transform defines the casing for parameter paths.
     config_accessor(:path_transform) { nil }
 
