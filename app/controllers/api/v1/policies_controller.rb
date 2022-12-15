db9f27b5826958d97f8b58937e3e5282441bda08
Zeke Gabrielse <ezekg@yahoo.com>
2022-12-15 08:45:38 -0600

    refactor policies

--- app/controllers/api/v1/policies_controller.rb
+++ app/controllers/api/v1/policies_controller.rb
@@ -22,6 +22,57 @@ module Api::V1
       render jsonapi: policy
     end
 
+    typed_params {
+      format :jsonapi
+
+      param :data, type: :hash do
+        param :type, type: :string, inclusion: { in: %w[policy policies] }
+        param :attributes, type: :hash do
+          param :scheme, type: :string, optional: true
+          param :encrypted, type: :boolean, optional: true
+          param :use_pool, type: :boolean, optional: true
+          param :name, type: :string, optional: true
+          param :duration, type: :integer, optional: true, allow_nil: true
+          param :strict, type: :boolean, optional: true
+          param :floating, type: :boolean, optional: true
+          param :protected, type: :boolean, optional: true
+          param :concurrent, type: :boolean, optional: true, if: -> { current_api_version == '1.0' || current_api_version == '1.1' }
+          param :max_machines, type: :integer, optional: true, allow_nil: true
+          param :max_processes, type: :integer, optional: true, allow_nil: true
+          param :max_cores, type: :integer, optional: true, allow_nil: true
+          param :max_uses, type: :integer, optional: true, allow_nil: true
+          param :fingerprint_uniqueness_strategy, type: :string, optional: true
+          param :fingerprint_matching_strategy, type: :string, optional: true
+          param :expiration_strategy, type: :string, optional: true
+          param :expiration_basis, type: :string, optional: true
+          param :transfer_strategy, type: :string, optional: true
+          param :authentication_strategy, type: :string, optional: true
+          param :leasing_strategy, type: :string, optional: true
+          param :overage_strategy, type: :string, optional: true
+          param :require_product_scope, type: :boolean, optional: true
+          param :require_policy_scope, type: :boolean, optional: true
+          param :require_machine_scope, type: :boolean, optional: true
+          param :require_fingerprint_scope, type: :boolean, optional: true
+          param :require_user_scope, type: :boolean, optional: true
+          param :require_check_in, type: :boolean, optional: true
+          param :check_in_interval, type: :string, optional: true, allow_nil: true
+          param :check_in_interval_count, type: :integer, optional: true, allow_nil: true
+          param :heartbeat_duration, type: :integer, optional: true, allow_nil: true
+          param :heartbeat_cull_strategy, type: :string, optional: true
+          param :heartbeat_resurrection_strategy, type: :string, optional: true
+          param :require_heartbeat, type: :boolean, optional: true
+          param :metadata, type: :metadata, allow_blank: true, optional: true
+        end
+        param :relationships, type: :hash do
+          param :product, type: :hash do
+            param :data, type: :hash do
+              param :type, type: :string, inclusion: { in: %w[product products] }
+              param :id, type: :string
+            end
+          end
+        end
+      end
+    }
     def create
       policy = current_account.policies.new(api_version: current_api_version, **policy_params)
       authorize! policy
@@ -39,6 +90,47 @@ module Api::V1
       end
     end
 
+    typed_params {
+      format :jsonapi
+
+      param :data, type: :hash do
+        param :type, type: :string, inclusion: { in: %w[policy policies] }
+        param :id, type: :string, optional: true, noop: true
+        param :attributes, type: :hash do
+          param :name, type: :string, optional: true
+          param :duration, type: :integer, optional: true, allow_nil: true
+          param :strict, type: :boolean, optional: true
+          param :floating, type: :boolean, optional: true
+          param :protected, type: :boolean, optional: true
+          param :concurrent, type: :boolean, optional: true, if: -> { current_api_version == '1.0' || current_api_version == '1.1' }
+          param :max_machines, type: :integer, optional: true, allow_nil: true
+          param :max_processes, type: :integer, optional: true, allow_nil: true
+          param :max_cores, type: :integer, optional: true, allow_nil: true
+          param :max_uses, type: :integer, optional: true, allow_nil: true
+          param :fingerprint_uniqueness_strategy, type: :string, optional: true
+          param :fingerprint_matching_strategy, type: :string, optional: true
+          param :expiration_strategy, type: :string, optional: true
+          param :expiration_basis, type: :string, optional: true
+          param :transfer_strategy, type: :string, optional: true
+          param :authentication_strategy, type: :string, optional: true
+          param :leasing_strategy, type: :string, optional: true
+          param :overage_strategy, type: :string, optional: true
+          param :require_product_scope, type: :boolean, optional: true
+          param :require_policy_scope, type: :boolean, optional: true
+          param :require_machine_scope, type: :boolean, optional: true
+          param :require_fingerprint_scope, type: :boolean, optional: true
+          param :require_user_scope, type: :boolean, optional: true
+          param :require_check_in, type: :boolean, optional: true
+          param :check_in_interval, type: :string, optional: true, allow_nil: true
+          param :check_in_interval_count, type: :integer, optional: true, allow_nil: true
+          param :heartbeat_duration, type: :integer, optional: true, allow_nil: true
+          param :heartbeat_cull_strategy, type: :string, optional: true
+          param :require_heartbeat, type: :boolean, optional: true
+          param :heartbeat_resurrection_strategy, type: :string, optional: true
+          param :metadata, type: :metadata, allow_blank: true, optional: true
+        end
+      end
+    }
     def update
       authorize! policy
 
@@ -77,104 +169,6 @@ module Api::V1
       @policy = scoped_policies.find(params[:id])
 
       Current.resource = policy
-    end
-
-    typed_parameters format: :jsonapi do
-      options strict: true
-
-      on :create do
-        param :data, type: :hash do
-          param :type, type: :string, inclusion: %w[policy policies]
-          param :attributes, type: :hash do
-            param :scheme, type: :string, optional: true
-            param :encrypted, type: :boolean, optional: true
-            param :use_pool, type: :boolean, optional: true
-            param :name, type: :string, optional: true
-            param :duration, type: :integer, optional: true, allow_nil: true
-            param :strict, type: :boolean, optional: true
-            param :floating, type: :boolean, optional: true
-            param :protected, type: :boolean, optional: true
-            if current_api_version == '1.0' || current_api_version == '1.1'
-              param :concurrent, type: :boolean, optional: true
-            end
-            param :max_machines, type: :integer, optional: true, allow_nil: true
-            param :max_processes, type: :integer, optional: true, allow_nil: true
-            param :max_cores, type: :integer, optional: true, allow_nil: true
-            param :max_uses, type: :integer, optional: true, allow_nil: true
-            param :fingerprint_uniqueness_strategy, type: :string, optional: true
-            param :fingerprint_matching_strategy, type: :string, optional: true
-            param :expiration_strategy, type: :string, optional: true
-            param :expiration_basis, type: :string, optional: true
-            param :transfer_strategy, type: :string, optional: true
-            param :authentication_strategy, type: :string, optional: true
-            param :leasing_strategy, type: :string, optional: true
-            param :overage_strategy, type: :string, optional: true
-            param :require_product_scope, type: :boolean, optional: true
-            param :require_policy_scope, type: :boolean, optional: true
-            param :require_machine_scope, type: :boolean, optional: true
-            param :require_fingerprint_scope, type: :boolean, optional: true
-            param :require_user_scope, type: :boolean, optional: true
-            param :require_check_in, type: :boolean, optional: true
-            param :check_in_interval, type: :string, optional: true, allow_nil: true
-            param :check_in_interval_count, type: :integer, optional: true, allow_nil: true
-            param :heartbeat_duration, type: :integer, optional: true, allow_nil: true
-            param :heartbeat_cull_strategy, type: :string, optional: true
-            param :heartbeat_resurrection_strategy, type: :string, optional: true
-            param :require_heartbeat, type: :boolean, optional: true
-            param :metadata, type: :hash, allow_non_scalars: true, optional: true
-          end
-          param :relationships, type: :hash do
-            param :product, type: :hash do
-              param :data, type: :hash do
-                param :type, type: :string, inclusion: %w[product products]
-                param :id, type: :string
-              end
-            end
-          end
-        end
-      end
-
-      on :update do
-        param :data, type: :hash do
-          param :type, type: :string, inclusion: %w[policy policies]
-          param :id, type: :string, inclusion: [controller.params[:id]], optional: true, transform: -> (k, v) { [] }
-          param :attributes, type: :hash do
-            param :name, type: :string, optional: true
-            param :duration, type: :integer, optional: true, allow_nil: true
-            param :strict, type: :boolean, optional: true
-            param :floating, type: :boolean, optional: true
-            param :protected, type: :boolean, optional: true
-            if current_api_version == '1.0' || current_api_version == '1.1'
-              param :concurrent, type: :boolean, optional: true
-            end
-            param :max_machines, type: :integer, optional: true, allow_nil: true
-            param :max_processes, type: :integer, optional: true, allow_nil: true
-            param :max_cores, type: :integer, optional: true, allow_nil: true
-            param :max_uses, type: :integer, optional: true, allow_nil: true
-            param :fingerprint_uniqueness_strategy, type: :string, optional: true
-            param :fingerprint_matching_strategy, type: :string, optional: true
-            param :expiration_strategy, type: :string, optional: true
-            param :expiration_basis, type: :string, optional: true
-            param :transfer_strategy, type: :string, optional: true
-            param :authentication_strategy, type: :string, optional: true
-            param :leasing_strategy, type: :string, optional: true
-            param :overage_strategy, type: :string, optional: true
-            param :require_product_scope, type: :boolean, optional: true
-            param :require_policy_scope, type: :boolean, optional: true
-            param :require_machine_scope, type: :boolean, optional: true
-            param :require_fingerprint_scope, type: :boolean, optional: true
-            param :require_user_scope, type: :boolean, optional: true
-            param :require_check_in, type: :boolean, optional: true
-            param :check_in_interval, type: :string, optional: true, allow_nil: true
-            param :check_in_interval_count, type: :integer, optional: true, allow_nil: true
-            param :heartbeat_duration, type: :integer, optional: true, allow_nil: true
-            param :heartbeat_cull_strategy, type: :string, optional: true
-            param :require_heartbeat, type: :boolean, optional: true
-            param :heartbeat_resurrection_strategy, type: :string, optional: true
-            param :metadata, type: :hash, allow_non_scalars: true, optional: true
-          end
-        end
-      end
     end
   end
 end
