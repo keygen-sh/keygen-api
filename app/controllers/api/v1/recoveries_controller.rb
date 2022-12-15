79cf8da691632e76907ec4f8981af11049ceddb0
Zeke Gabrielse <ezekg@yahoo.com>
2022-12-15 09:01:04 -0600

    refactor recoveries

--- app/controllers/api/v1/recoveries_controller.rb
+++ app/controllers/api/v1/recoveries_controller.rb
@@ -4,24 +4,19 @@ module Api::V1
   class RecoveriesController < Api::V1::BaseController
     skip_verify_authorized
 
+    typed_params {
+      format :jsonapi
+
+      param :meta, type: :hash do
+        param :type, type: :string, inclusion: { in: %w[account] }
+        param :email, type: :string
+      end
+    }
     def recover
       case recovery_params
       in meta: { type: 'account', email: }
         mailer = RecoveryMailer.recover_accounts_for_email(email:)
         mailer.deliver_later
-      end
-    end
-
-    private
-
-    typed_parameters do
-      options strict: true
-
-      on :recover do
-        param :meta, type: :hash do
-          param :type, type: :string, inclusion: %w[account]
-          param :email, type: :string
-        end
       end
     end
   end
