a70fdc227249688ff45bd368590bbd050edf89f9
Zeke Gabrielse <ezekg@yahoo.com>
2022-12-16 13:57:22 -0600

    refactor handler set

--- lib/typed_parameters/handler_set.rb
+++ lib/typed_parameters/handler_set.rb
@@ -4,13 +4,16 @@ require_relative 'namespaced_set'
 
 module TypedParameters
   class HandlerSet
-    attr_reader :deferred
+    attr_reader :deferred,
+                :params,
+                :query
 
-    def initialize = @deferred = []
+    def initialize
+      @deferred = []
+      @params   = NamespacedSet.new
+      @query    = NamespacedSet.new
+    end
 
     def deferred? = @deferred.any?
-
-    def params = @params ||= NamespacedSet.new
-    def query  = @query  ||= NamespacedSet.new
   end
 end
