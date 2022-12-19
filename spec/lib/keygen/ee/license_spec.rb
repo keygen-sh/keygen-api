e64756b45bd3d2b64468bb3843fcbbecf843f4b6
Zeke Gabrielse <ezekg@yahoo.com>
2022-12-19 14:10:03 -0600

    fix duplicate const warnings

--- spec/lib/keygen/ee/license_spec.rb
+++ spec/lib/keygen/ee/license_spec.rb
@@ -6,18 +6,18 @@ require 'spec_helper'
 require_dependency Rails.root.join('lib', 'keygen')
 
 describe Keygen::EE::License, type: :ee do
-  TEST_PUBLIC_KEY   = ['775e65407f3d86de55efbac47d1bbeab79768a21a406e39976606a704984e7d1'].pack('H*')
-  TEST_LICENSE_KEY  = 'TEST-116A58-3F79F9-9F1982-9D63B1-V3'
-  TEST_LICENSE_FILE = Base64.strict_encode64(
+  public_key   = ['775e65407f3d86de55efbac47d1bbeab79768a21a406e39976606a704984e7d1'].pack('H*')
+  license_key  = 'TEST-116A58-3F79F9-9F1982-9D63B1-V3'
+  license_file = Base64.strict_encode64(
     file_fixture('valid.lic').read,
   )
 
   before do
-    stub_const('Keygen::PUBLIC_KEY', TEST_PUBLIC_KEY)
+    stub_const('Keygen::PUBLIC_KEY', public_key)
   end
 
   context 'when using a valid license file' do
-    with_env KEYGEN_LICENSE_FILE: TEST_LICENSE_FILE, KEYGEN_LICENSE_KEY: TEST_LICENSE_KEY do
+    with_env KEYGEN_LICENSE_FILE: license_file, KEYGEN_LICENSE_KEY: license_key do
       it 'should be a valid license' do
         license = described_class.current
 
@@ -54,7 +54,7 @@ describe Keygen::EE::License, type: :ee do
 
   context 'when using an expired license file' do
     with_file path: Keygen::EE::LicenseFile::DEFAULT_PATH, fixture: 'expired.lic' do
-      with_env KEYGEN_LICENSE_KEY: TEST_LICENSE_KEY do
+      with_env KEYGEN_LICENSE_KEY: license_key do
         it 'should be an invalid license' do
           license = described_class.current
 
@@ -91,7 +91,7 @@ describe Keygen::EE::License, type: :ee do
   end
 
   context 'when using an invalid license file' do
-    with_env KEYGEN_LICENSE_FILE_PATH: '/dev/null', KEYGEN_LICENSE_KEY: TEST_LICENSE_KEY do
+    with_env KEYGEN_LICENSE_FILE_PATH: '/dev/null', KEYGEN_LICENSE_KEY: license_key do
       it 'should fail to load license' do
         license = described_class.current
 
@@ -101,7 +101,7 @@ describe Keygen::EE::License, type: :ee do
   end
 
   context 'when using an invalid license key' do
-    with_env KEYGEN_LICENSE_FILE: TEST_LICENSE_FILE, KEYGEN_LICENSE_KEY: 'invalid' do
+    with_env KEYGEN_LICENSE_FILE: license_file, KEYGEN_LICENSE_KEY: 'invalid' do
       it 'should fail to load license' do
         license = described_class.current
 
