bc53524b188e15369ada2e0bb2081d0bedc56ba8
Zeke Gabrielse <ezekg@yahoo.com>
2022-12-22 11:04:28 -0600

    add script for encrypting license keys

---
+++ db/scripts/encrypt_license_keys_script.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# heroku run:detached -e SLEEP_DURATION=0.5 --tail \
+#   rails runner db/scripts/encrypt_license_keys.rb
+
+SLEEP_DURATION = ENV.fetch('SLEEP_DURATION') { 1 }.to_f
+
+License.find_each do |license|
+  next if
+    license.encrypted_attribute?(:key)
+
+  license.encrypt
+
+  sleep SLEEP_DURATION
+end
