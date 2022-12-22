300034ebc3dd9168b4f3eddce6dad019b11bfec2
Zeke Gabrielse <ezekg@yahoo.com>
2022-12-22 11:56:26 -0600

    update default sleep duration

--- db/scripts/encrypt_license_keys_script.rb
+++ db/scripts/encrypt_license_keys_script.rb
@@ -3,7 +3,7 @@
 # heroku run:detached -e SLEEP_DURATION=0.5 --tail \
 #   rails runner db/scripts/encrypt_license_keys_script.rb
 
-SLEEP_DURATION = ENV.fetch('SLEEP_DURATION') { 1 }.to_f
+SLEEP_DURATION = ENV.fetch('SLEEP_DURATION') { 0.1 }.to_f
 
 License.find_each do |license|
   next if
