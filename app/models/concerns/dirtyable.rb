9c68f6c554c9fa8522e16e372f6ec92f26686710
Zeke Gabrielse <ezekg@yahoo.com>
2023-02-02 17:07:05 -0600

    add support for tracking attribute assignments

--- app/models/concerns/dirtyable.rb
+++ app/models/concerns/dirtyable.rb
@@ -3,7 +3,59 @@
 module Dirtyable
   extend ActiveSupport::Concern
 
+  included do
+    ##
+    # Override for AR's default assign_attributes method, used when we need
+    # to check is an attribute was provided during class instantiation.
+    # This is especially when we want to check if a given attribute
+    # was explicitly assigned nil.
+    #
+    # For example,
+    #
+    #   Current.environment = #<Environment id=1>
+    #
+    #   license = License.new
+    #   license.environment # => #<Environment id=1>
+    #   license.environment_assigned? # => false
+    #
+    #   license = License.new environment: nil
+    #   license.environment # => nil
+    #   license.environment_assigned? # => true
+    #
+    #   license = License.new environment: #<Environment id=2>
+    #   license.environment # => #<Environment id=2>
+    #   license.environment_assigned? # => true
+    #
+    # As you can see, our environment is nil for the second license. Without
+    # this override, we wouldn't be able to determine whether or not :environment
+    # was initialized as nil vs nil being explicitly provided. This allows us
+    # to conditionally apply a default via the current environment based on
+    # whether or not an environment was explicitly passed.
+    #
+    after_save -> { remove_instance_variable(:@assigned_attributes) },
+      if: :assigned_attributes?
+
+    alias :__assign_attributes :assign_attributes
+
+    def assigned_attributes? = instance_variable_defined?(:@assigned_attributes)
+    def assign_attributes(attributes)
+      @assigned_attributes = (@assigned_attributes || {}).merge(attributes)
+
+      __assign_attributes(attributes)
+    end
+
+    def method_missing(method_name, ...)
+      case /(?<key>.*?)_assigned\?/.match(method_name)
+      in { key: } if has_attribute?(key) || self.class.reflections.key?(key)
+        @assigned_attributes&.key?(key.to_sym)
+      else
+        super
+      end
+    end
+  end
+
   class_methods do
+    ##
     # FIXME(ezekg) Can't find a way to determine whether or not nested attributes
     #              have been provided. This adds a flag we can check.
     #
@@ -35,14 +87,14 @@ module Dirtyable
         after_save -> { remove_instance_variable(:@#{relation}_attributes) },
           if: :#{relation}_attributes_changed?
 
-        alias :_#{relation}_attributes= :#{relation}_attributes=
+        alias :__#{relation}_attributes= :#{relation}_attributes=
 
         def #{relation}_attributes_changed? = instance_variable_defined?(:@#{relation}_attributes)
         def #{relation}_attributes          = @#{relation}_attributes
         def #{relation}_attributes=(attributes)
           @#{relation}_attributes = attributes
 
-          self._#{relation}_attributes = attributes
+          self.__#{relation}_attributes = attributes
         end
       RUBY
     end
