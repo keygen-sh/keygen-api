2e0b4da7be903dd8efd1b988ff59fc87c7f9d560
Zeke Gabrielse <ezekg@yahoo.com>
2023-02-09 22:07:30 -0600

    fix cache clear before/after specs

--- spec/support/clear_cache.rb
+++ spec/support/clear_cache.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.configure do |config|
-  config.before(:all) do
-    Rails.cache.clear(namespace: "test_#{ENV['TEST_ENV_NUMBER']}")
-  end
-
-  config.after(:each) do
-    Rails.cache.clear(namespace: "test_#{ENV['TEST_ENV_NUMBER']}")
+  config.before(:each) do
+    Rails.cache.clear
   end
 end
