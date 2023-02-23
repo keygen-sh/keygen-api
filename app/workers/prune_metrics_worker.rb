7b9507a32e40c26e496edcf5e58d142d32df3c3e
Zeke Gabrielse <ezekg@yahoo.com>
2023-02-23 08:40:29 -0600

    add env var for pruning jobs backlog size

--- app/workers/prune_metrics_worker.rb
+++ app/workers/prune_metrics_worker.rb
@@ -1,28 +1,41 @@
 class PruneMetricsWorker < BaseWorker
-  BATCH_SIZE     = ENV.fetch('PRUNE_BATCH_SIZE')     { 1_000 }.to_i
-  SLEEP_DURATION = ENV.fetch('PRUNE_SLEEP_DURATION') { 1 }.to_f
+  BACKLOG_DAYS   = ENV.fetch('PRUNE_METRIC_BACKLOG_DAYS') { 30 }.to_i
+  BATCH_SIZE     = ENV.fetch('PRUNE_BATCH_SIZE')          { 1_000 }.to_i
+  SLEEP_DURATION = ENV.fetch('PRUNE_SLEEP_DURATION')      { 1 }.to_f
 
   sidekiq_options queue: :cron,
                   lock: :until_executed,
                   cronitor_disabled: false
 
   def perform
-    accounts = Account.joins(:metrics)
-                      .where('metrics.created_at < ?', 60.days.ago)
-                      .group('accounts.id')
-                      .having('count(metrics.id) > 0')
+    return if
+      BACKLOG_DAYS <= 0
+
+    accounts = Account.where(<<~SQL.squish, BACKLOG_DAYS.days.ago.beginning_of_day)
+      EXISTS (
+        SELECT
+          1
+        FROM
+          "metrics"
+        WHERE
+          "metrics"."account_id" = "accounts"."id" AND
+          "metrics"."created_at" < ?
+        LIMIT
+          1
+      )
+    SQL
 
     Keygen.logger.info "[workers.prune-metrics] Starting: accounts=#{accounts.count}"
 
     accounts.find_each do |account|
       account_id = account.id
-      batch = 0
+      batch      = 0
 
       Keygen.logger.info "[workers.prune-metrics] Pruning rows: account_id=#{account_id}"
 
       loop do
         metrics = account.metrics
-                         .where('created_at < ?', 60.days.ago.beginning_of_day)
+                         .where('created_at < ?', BACKLOG_DAYS.days.ago.beginning_of_day)
 
         batch += 1
         count = metrics.limit(BATCH_SIZE)
