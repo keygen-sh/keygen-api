566c4bdd01e416b39e16333e7c542e42419ccb56
Zeke Gabrielse <ezekg@yahoo.com>
2023-02-27 13:39:37 -0600

    rename shared examples directory

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,7 +33,7 @@ DatabaseCleaner.strategy = :transaction
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require(f) }
 
 # Requires shared examples.
-Dir[Rails.root.join('spec/shared_examples/**/*.rb')].each { |f| require(f) }
+Dir[Rails.root.join('spec/shared/**/*.rb')].each { |f| require(f) }
 
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
