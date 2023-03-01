50c881c2b18c7078017a2e3fb372b09272c54d4a
Zeke Gabrielse <ezekg@yahoo.com>
2023-03-01 10:35:42 -0600

    add token caching

--- app/services/token_lookup_service.rb
+++ app/services/token_lookup_service.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class TokenLookupService < BaseService
-  TOKEN_LEGACY_RE = /\A(?<account_id>[^\.]+)\.(?<token_id>[^\.]+)\.(?<bits>.+?)(?<version>v\d+)\z/.freeze
-  TOKEN_RE        = /\A(?<bits>.+?)\.(?<version>v\d+)\z/.freeze
-  TOKEN_CACHE_TTL = 15.minutes
+  TOKEN_VERSION_RE = /\A.+?(?<token_version>v\d+)\z/.freeze
+  TOKEN_LEGACY_RE  = /\A(?<account_id>[^\.]+)\.(?<token_id>[^\.]+)\.(?<token_bits>.+?)(?<token_version>v\d+)\z/.freeze
+  TOKEN_RE         = /\A(?<token_prefix>.+?)-(?<token_bits>.+?)(?<token_version>v\d+)\z/.freeze
+  TOKEN_CACHE_TTL  = 15.minutes
 
   def initialize(account:, token:, environment: nil)
     @account     = account
@@ -15,30 +16,38 @@ class TokenLookupService < BaseService
     return nil unless
       account.present? && token.present?
 
-    case version
+    matches = TOKEN_VERSION_RE.match(token)
+    return nil unless
+      matches.present?
+
+    case matches[:token_version]
     when 'v1'
-      matches = TOKEN_LEGACY_RE.match(token)
+      m = TOKEN_LEGACY_RE.match(token)
       return nil unless
-        matches.present? && account.id.remove('-') == matches[:account_id]
+        m.present? && account.id.remove('-') == m[:account_id]
 
       # FIXME(ezekg) Can't figure out a clean way to cache v1 tokens since we don't
       #              store the raw token value anywhere, which makes invalidation
       #              harder on revocation.
-      tok = tokens.find_by(id: matches[:token_id])
+      instance = tokens.find_by(id: m[:token_id])
 
-      if tok&.compare_hashed_token(:digest, token, version: 'v1')
-        tok
+      if instance&.compare_hashed_token(:digest, token, version: 'v1')
+        instance
       else
         nil
       end
     when 'v2'
       digest = OpenSSL::HMAC.hexdigest('SHA512', account.private_key, token)
 
-      tokens.find_by(digest:)
+      with_cache digest do
+        tokens.find_by(digest:)
+      end
     when 'v3'
       digest = OpenSSL::HMAC.hexdigest('SHA256', account.secret_key, token)
 
-      tokens.find_by(digest:)
+      with_cache digest do
+        tokens.find_by(digest:)
+      end
     end
   end
 
@@ -51,6 +60,14 @@ class TokenLookupService < BaseService
   # NOTE(ezekg) When we're in the global (nil) environment, we want to enable strict mode
   #             so that we can't authenticate with a token from another environment,
   #             even if we have access to that environment's tokens.
-  def tokens  = account.tokens.for_environment(environment, strict: environment.nil?)
-  def version = token[-2..-1]&.downcase
+  def tokens = account.tokens.for_environment(environment, strict: environment.nil?)
+  def cache  = Rails.cache
+
+  def with_cache(digest)
+    key = Token.cache_key(digest, account:, environment:)
+
+    cache.fetch(key, skip_nil: true, expires_in: TOKEN_CACHE_TTL) do
+      yield
+    end
+  end
 end
