13ade48379bcf05e05f106f0e2b100b4889d6145
Zeke Gabrielse <ezekg@yahoo.com>
2023-03-02 14:29:43 -0600

    remove redundant environment scopes

--- app/controllers/api/v1/keys_controller.rb
+++ app/controllers/api/v1/keys_controller.rb
@@ -2,7 +2,6 @@
 
 module Api::V1
   class KeysController < Api::V1::BaseController
-    has_scope(:environment, allow_blank: true) { |c, s, v| s.for_environment(v.presence, strict: true) }
     has_scope(:product) { |c, s, v| s.for_product(v) }
     has_scope(:policy) { |c, s, v| s.for_policy(v) }
 
