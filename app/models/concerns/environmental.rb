dda6f33d84771c84056a96d8665ea0d6c26daef2
Zeke Gabrielse <ezekg@yahoo.com>
2023-03-24 16:52:24 -0500

    fix protected env setters

--- app/models/concerns/environmental.rb
+++ app/models/concerns/environmental.rb
@@ -4,7 +4,7 @@ module Environmental
   extend ActiveSupport::Concern
 
   included do
-    include Keygen::EE::ProtectedMethods[:environment=, entitlements: %i[environments]]
+    include Keygen::EE::ProtectedMethods[:environment_id=, :environment=, entitlements: %i[environments]]
     include Dirtyable
 
     ##
