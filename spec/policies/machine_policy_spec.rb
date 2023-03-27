434408caee62a10f6a5e85809f0b879645c0cd93
Zeke Gabrielse <ezekg@yahoo.com>
2023-03-27 15:22:40 -0500

    add machine authz tests

--- spec/policies/machine_policy_spec.rb
+++ spec/policies/machine_policy_spec.rb
@@ -196,6 +196,138 @@ describe MachinePolicy, type: :policy do
     end
   end
 
+  with_role_authorization :environment do
+    within_environment do
+      with_scenarios %i[accessing_machines] do
+        with_token_authentication do
+          with_permissions %w[machine.read] do
+            without_token_permissions { denies :index }
+
+            allows :index
+          end
+
+          with_wildcard_permissions { allows :index }
+          with_default_permissions  { allows :index }
+          without_permissions       { denies :index }
+
+          within_environment :isolated do
+            with_bearer_and_token_trait :isolated do
+              allows :index
+            end
+
+            with_bearer_and_token_trait :shared do
+              denies :index
+            end
+          end
+
+          within_environment :shared do
+            with_bearer_and_token_trait :isolated do
+              denies :index
+            end
+
+            with_bearer_and_token_trait :shared do
+              allows :index
+            end
+          end
+
+          within_environment nil do
+            with_bearer_and_token_trait :isolated do
+              denies :index
+            end
+
+            with_bearer_and_token_trait :shared do
+              denies :index
+            end
+          end
+        end
+      end
+
+      with_scenarios %i[accessing_a_machine] do
+        with_token_authentication do
+          with_permissions %w[machine.read] do
+            without_token_permissions { denies :show }
+
+            allows :show
+          end
+
+          with_permissions %w[machine.create] do
+            without_token_permissions { denies :create }
+
+            allows :create
+          end
+
+          with_permissions %w[machine.update] do
+            without_token_permissions { denies :update }
+
+            allows :update
+          end
+
+          with_permissions %w[machine.delete] do
+            without_token_permissions { denies :destroy }
+
+            allows :destroy
+          end
+
+          with_permissions %w[machine.check-out] do
+            without_token_permissions { denies :check_out }
+
+            allows :check_out
+          end
+
+          with_wildcard_permissions do
+            without_token_permissions do
+              denies :show, :create, :update, :destroy, :check_out
+            end
+
+            allows :show, :create, :update, :destroy, :check_out
+          end
+
+          with_default_permissions do
+            without_token_permissions do
+              denies :show, :create, :update, :destroy, :check_out
+            end
+
+            allows :show, :create, :update, :destroy, :check_out
+          end
+
+          without_permissions do
+            denies :show, :create, :update, :destroy, :check_out
+          end
+
+          within_environment :isolated do
+            with_bearer_and_token_trait :isolated do
+              allows :show, :create, :update, :destroy, :check_out
+            end
+
+            with_bearer_and_token_trait :shared do
+              denies :show, :create, :update, :destroy, :check_out
+            end
+          end
+
+          within_environment :shared do
+            with_bearer_and_token_trait :isolated do
+              denies :show, :create, :update, :destroy, :check_out
+            end
+
+            with_bearer_and_token_trait :shared do
+              allows :show, :create, :update, :destroy, :check_out
+            end
+          end
+
+          within_environment nil do
+            with_bearer_and_token_trait :isolated do
+              denies :show, :create, :update, :destroy, :check_out
+            end
+
+            with_bearer_and_token_trait :shared do
+              denies :show, :create, :update, :destroy, :check_out
+            end
+          end
+        end
+      end
+    end
+  end
+
   with_role_authorization :product do
     with_scenarios %i[accessing_its_machines] do
       with_token_authentication do
