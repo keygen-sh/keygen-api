f0c7169fbe5fdd90db6b3b94cd0417c6bba1ceaa
Zeke Gabrielse <ezekg@yahoo.com>
2023-03-27 14:56:08 -0500

    add key authz tests

--- spec/policies/key_policy_spec.rb
+++ spec/policies/key_policy_spec.rb
@@ -182,6 +182,128 @@ describe KeyPolicy, type: :policy do
     end
   end
 
+  with_role_authorization :environment do
+    within_environment do
+      with_scenarios %i[accessing_keys] do
+        with_token_authentication do
+          with_permissions %w[key.read] do
+            without_token_permissions { denies :index }
+
+            allows :index
+          end
+
+          with_wildcard_permissions { allows :index }
+          with_default_permissions  { allows :index }
+          without_permissions       { denies :index }
+
+           within_environment :isolated do
+            with_bearer_and_token_trait :isolated do
+              allows :index
+            end
+
+            with_bearer_and_token_trait :shared do
+              denies :index
+            end
+          end
+
+          within_environment :shared do
+            with_bearer_and_token_trait :isolated do
+              denies :index
+            end
+
+            with_bearer_and_token_trait :shared do
+              allows :index
+            end
+          end
+
+          within_environment nil do
+            with_bearer_and_token_trait :isolated do
+              denies :index
+            end
+
+            with_bearer_and_token_trait :shared do
+              denies :index
+            end
+          end
+        end
+      end
+
+      with_scenarios %i[accessing_a_key] do
+        with_token_authentication do
+          with_permissions %w[key.read] do
+            without_token_permissions { denies :show }
+
+            allows :show
+          end
+
+          with_permissions %w[key.create] do
+            without_token_permissions { denies :create }
+
+            allows :create
+          end
+
+          with_permissions %w[key.update] do
+            without_token_permissions { denies :update }
+
+            allows :update
+          end
+
+          with_permissions %w[key.delete] do
+            without_token_permissions { denies :destroy }
+
+            allows :destroy
+          end
+
+          with_wildcard_permissions do
+            without_token_permissions { denies :show, :create, :update, :destroy }
+
+            allows :show, :create, :update, :destroy
+          end
+
+          with_default_permissions do
+            without_token_permissions { denies :show, :create, :update, :destroy }
+
+            allows :show, :create, :update, :destroy
+          end
+
+          without_permissions do
+            denies :show, :create, :update, :destroy
+          end
+
+          within_environment :isolated do
+            with_bearer_and_token_trait :isolated do
+              allows :show, :create, :update, :destroy
+            end
+
+            with_bearer_and_token_trait :shared do
+              denies :show, :create, :update, :destroy
+            end
+          end
+
+          within_environment :shared do
+            with_bearer_and_token_trait :isolated do
+              denies :show, :create, :update, :destroy
+            end
+
+            with_bearer_and_token_trait :shared do
+              allows :show, :create, :update, :destroy
+            end
+          end
+
+          within_environment nil do
+            with_bearer_and_token_trait :isolated do
+              denies :show, :create, :update, :destroy
+            end
+
+            with_bearer_and_token_trait :shared do
+              denies :show, :create, :update, :destroy
+            end
+          end
+        end
+      end
+    end
+  end
+
   with_role_authorization :product do
     with_scenarios %i[accessing_its_keys] do
       with_token_authentication do
