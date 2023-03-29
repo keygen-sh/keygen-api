1df1a67ad35b7abf5c9564e267bb4117f1673b02
Zeke Gabrielse <ezekg@yahoo.com>
2023-03-29 14:14:48 -0500

    add license machine authz tests

--- app/policies/licenses/machine_policy.rb
+++ app/policies/licenses/machine_policy.rb
@@ -11,7 +11,7 @@ module Licenses
       )
 
       case bearer
-      in role: { name: 'admin' | 'developer' | 'sales_agent' | 'support_agent' | 'read_only' }
+      in role: { name: 'admin' | 'developer' | 'sales_agent' | 'support_agent' | 'read_only' | 'environment' }
         allow!
       in role: { name: 'product' } if license.product == bearer
         allow!
@@ -31,7 +31,7 @@ module Licenses
       )
 
       case bearer
-      in role: { name: 'admin' | 'developer' | 'sales_agent' | 'support_agent' | 'read_only' }
+      in role: { name: 'admin' | 'developer' | 'sales_agent' | 'support_agent' | 'read_only' | 'environment' }
         allow!
       in role: { name: 'product' } if license.product == bearer
         allow!
