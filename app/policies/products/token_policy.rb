e13093a1012cb5c5a9a0f477868b68d9d1bd77b5
Zeke Gabrielse <ezekg@yahoo.com>
2023-03-29 14:58:13 -0500

    add product relationship authz tests

--- app/policies/products/token_policy.rb
+++ app/policies/products/token_policy.rb
@@ -11,7 +11,7 @@ module Products
       )
 
       case bearer
-      in role: { name: 'admin' | 'developer' | 'sales_agent' | 'support_agent' | 'read_only' }
+      in role: { name: 'admin' | 'developer' | 'sales_agent' | 'support_agent' | 'read_only' | 'environment' }
         allow!
       in role: { name: 'product' } if product == bearer
         record.all? { _1 in bearer_type: ^(Product.name), bearer_id: ^(bearer.id) }
@@ -27,7 +27,7 @@ module Products
       )
 
       case bearer
-      in role: { name: 'admin' | 'developer' | 'sales_agent' | 'support_agent' | 'read_only' }
+      in role: { name: 'admin' | 'developer' | 'sales_agent' | 'support_agent' | 'read_only' | 'environment' }
         allow!
       in role: { name: 'product' } if product == bearer && record.bearer == bearer
         allow!
--- spec/policies/products/license_policy_spec.rb
+++ spec/policies/products/license_policy_spec.rb
@@ -132,6 +132,78 @@ describe Products::LicensePolicy, type: :policy do
     end
   end
 
+  with_role_authorization :environment do
+    within_environment :current do
+      with_scenarios %i[accessing_a_product accessing_its_licenses] do
+        with_token_authentication do
+          with_permissions %w[license.read] do
+            without_token_permissions { denies :index }
+
+            allows :index
+          end
+
+          with_wildcard_permissions { allows :index }
+          with_default_permissions  { allows :index }
+          without_permissions       { denies :index }
+        end
+      end
+
+      with_scenarios %i[accessing_a_product accessing_its_license] do
+        with_token_authentication do
+          with_permissions %w[license.read] do
+            without_token_permissions { denies :show }
+
+            allows :show
+          end
+
+          with_wildcard_permissions do
+            without_token_permissions { denies :show }
+
+            allows :show
+          end
+
+          with_default_permissions do
+            without_token_permissions { denies :show }
+
+            allows :show
+          end
+
+          without_permissions do
+            denies :show
+          end
+        end
+      end
+    end
+
+    with_scenarios %i[accessing_a_product accessing_its_licenses] do
+      with_token_authentication do
+        with_permissions %w[license.read] do
+          without_token_permissions { denies :index }
+
+          denies :index
+        end
+
+        with_wildcard_permissions { denies :index }
+        with_default_permissions  { denies :index }
+        without_permissions       { denies :index }
+      end
+    end
+
+    with_scenarios %i[accessing_a_product accessing_its_license] do
+      with_token_authentication do
+        with_permissions %w[license.read] do
+          without_token_permissions { denies :show }
+
+          denies :show
+        end
+
+        with_wildcard_permissions { denies :show }
+        with_default_permissions  { denies :show }
+        without_permissions       { denies :show }
+      end
+    end
+  end
+
   with_role_authorization :product do
     with_scenarios %i[accessing_itself accessing_its_licenses] do
       with_token_authentication do
