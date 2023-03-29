5cdd753865a0fe46747127349bec30ede1879a99
Zeke Gabrielse <ezekg@yahoo.com>
2023-03-29 14:22:50 -0500

    add license user authz tests

--- app/policies/licenses/user_policy.rb
+++ app/policies/licenses/user_policy.rb
@@ -11,7 +11,7 @@ module Licenses
       )
 
       case bearer
-      in role: { name: 'admin' | 'developer' | 'sales_agent' | 'support_agent' | 'read_only' }
+      in role: { name: 'admin' | 'developer' | 'sales_agent' | 'support_agent' | 'read_only' | 'environment' }
         allow!
       in role: { name: 'product' } if license.product == bearer
         allow!
@@ -29,7 +29,7 @@ module Licenses
       verify_environment!
 
       case bearer
-      in role: { name: 'admin' | 'developer' | 'sales_agent' }
+      in role: { name: 'admin' | 'developer' | 'sales_agent' | 'environment' }
         allow!
       in role: { name: 'product' } if license.product == bearer
         allow!
