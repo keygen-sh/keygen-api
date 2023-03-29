da492ef50823f2c41e51297667be5ba0f3f0a1ae
Zeke Gabrielse <ezekg@yahoo.com>
2023-03-29 14:58:13 -0500

    add product relationship authz tests

--- spec/policies/products/release_arch_policy_spec.rb
+++ spec/policies/products/release_arch_policy_spec.rb
@@ -120,6 +120,82 @@ describe Products::ReleaseArchPolicy, type: :policy do
     end
   end
 
+  with_role_authorization :environment do
+    within_environment :current do
+      with_scenarios %i[accessing_a_product accessing_its_arches] do
+        with_token_authentication do
+          with_permissions %w[arch.read] do
+            without_token_permissions { denies :show }
+
+            allows :show
+          end
+
+          with_wildcard_permissions { allows :index }
+          with_default_permissions  { allows :index }
+          without_permissions       { denies :index }
+        end
+      end
+
+      with_scenarios %i[accessing_a_product accessing_its_arch] do
+        with_token_authentication do
+          with_permissions %w[arch.read] do
+            without_token_permissions { denies :show }
+
+            allows :show
+          end
+
+          with_wildcard_permissions do
+            without_token_permissions { denies :show }
+
+            allows :show
+          end
+
+          with_default_permissions do
+            without_token_permissions { denies :show }
+
+            allows :show
+          end
+
+          without_permissions do
+            denies :show
+          end
+        end
+      end
+    end
+
+    with_scenarios %i[accessing_a_product accessing_its_arches] do
+      with_token_authentication do
+        with_permissions %w[arch.read] do
+          denies :show
+        end
+
+        with_wildcard_permissions { denies :index }
+        with_default_permissions  { denies :index }
+        without_permissions       { denies :index }
+      end
+    end
+
+    with_scenarios %i[accessing_a_product accessing_its_arch] do
+      with_token_authentication do
+        with_permissions %w[arch.read] do
+          denies :show
+        end
+
+        with_wildcard_permissions do
+          denies :show
+        end
+
+        with_default_permissions do
+          denies :show
+        end
+
+        without_permissions do
+          denies :show
+        end
+      end
+    end
+  end
+
   with_role_authorization :product do
     with_scenarios %i[accessing_itself accessing_its_arches] do
       with_token_authentication do
