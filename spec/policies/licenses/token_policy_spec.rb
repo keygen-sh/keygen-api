54387f23beda101a6de59faa616e759042af2511
Zeke Gabrielse <ezekg@yahoo.com>
2023-03-29 14:20:49 -0500

    add license token authz tests

--- spec/policies/licenses/token_policy_spec.rb
+++ spec/policies/licenses/token_policy_spec.rb
@@ -154,6 +154,94 @@ describe Licenses::TokenPolicy, type: :policy do
     end
   end
 
+  with_role_authorization :environment do
+    within_environment do
+      with_scenarios %i[accessing_a_license accessing_its_tokens] do
+        with_token_authentication do
+          with_permissions %w[token.read] do
+            without_token_permissions { denies :index }
+
+            allows :index
+          end
+
+          with_wildcard_permissions { allows :index }
+          with_default_permissions  { allows :index }
+          without_permissions       { denies :index }
+        end
+      end
+
+      with_scenarios %i[accessing_a_license accessing_its_token] do
+        with_token_authentication do
+          with_permissions %w[token.read] do
+            without_token_permissions { denies :show }
+
+            allows :show
+          end
+
+          with_permissions %w[license.tokens.generate] do
+            without_token_permissions { denies :create }
+
+            allows :create
+          end
+
+          with_wildcard_permissions do
+            allows :show, :create
+          end
+
+          with_default_permissions do
+            allows :show, :create
+          end
+
+          without_permissions do
+            denies :show, :create
+          end
+        end
+      end
+    end
+
+    with_scenarios %i[accessing_a_license accessing_its_tokens] do
+      with_token_authentication do
+        with_permissions %w[token.read] do
+          without_token_permissions { denies :index }
+
+          denies :index
+        end
+
+        with_wildcard_permissions { denies :index }
+        with_default_permissions  { denies :index }
+        without_permissions       { denies :index }
+      end
+    end
+
+    with_scenarios %i[accessing_a_license accessing_its_token] do
+      with_token_authentication do
+        with_permissions %w[token.read] do
+          without_token_permissions { denies :show }
+
+          denies :show
+        end
+
+        with_permissions %w[license.tokens.generate] do
+          without_token_permissions { denies :create }
+
+          denies :create
+        end
+
+        with_wildcard_permissions do
+          denies :show, :create
+        end
+
+        with_default_permissions do
+          denies :show, :create
+        end
+
+        without_permissions do
+          denies :show, :create
+        end
+      end
+    end
+  end
+
   with_role_authorization :product do
     with_scenarios %i[accessing_its_license accessing_its_tokens] do
       with_token_authentication do
