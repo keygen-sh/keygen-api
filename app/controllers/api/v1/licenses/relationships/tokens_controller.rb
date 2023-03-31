99e8cd21fed9b4ee7d365d68f7b3c6d797eaf2e6
Zeke Gabrielse <ezekg@yahoo.com>
2023-03-31 16:37:13 -0500

    fix env param authz

--- app/controllers/api/v1/licenses/relationships/tokens_controller.rb
+++ app/controllers/api/v1/licenses/relationships/tokens_controller.rb
@@ -40,7 +40,7 @@ module Api::V1::Licenses::Relationships
             next unless
               license.entitled?(:permissions)
 
-            param :permissions, type: :array, optional: true, if: -> { current_account.ent? && current_bearer&.has_role?(:admin, :product) } do
+            param :permissions, type: :array, optional: true, if: -> { current_account.ent? && current_bearer&.has_role?(:admin, :product, :environment) } do
               items type: :string
             end
           end
