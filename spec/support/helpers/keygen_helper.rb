210e08d0ad1a49a509166e7215546e0e780951c2
Zeke Gabrielse <ezekg@yahoo.com>
2023-04-20 16:34:29 -0500

    refactor ee/ce helpers

--- spec/support/helpers/keygen_helper.rb
+++ spec/support/helpers/keygen_helper.rb
@@ -3,31 +3,28 @@
 module KeygenHelper
   module ScenarioMethods
     def within_ee(expiry: 1.year.from_now.iso8601, issued: 1.day.ago.iso8601, entitlements: %i[request_logs event_logs permissions environments multiplayer], &block)
-      context 'when in an EE context' do
-        before do
-          allow(Keygen).to receive(:ce?).and_return false
-          allow(Keygen).to receive(:ee?).and_return true
-          allow(Keygen::EE::LicenseFile).to receive(:current).and_return(
-            Keygen::EE::LicenseFile.new(
-              included: entitlements.map {{ type: 'entitlements', attributes: { code: _1.to_s.upcase } }},
-              data: { type: 'licenses', attributes: { expiry: } },
-              meta: { issued:, expiry: },
-            ),
-          )
-        end
+      context 'when in the EE edition' do
+        with_env KEYGEN_EDITION: 'EE' do
+          before do
+            allow(Keygen::EE::LicenseFile).to receive(:current).and_return(
+              Keygen::EE::LicenseFile.new(
+                included: entitlements.map {{ type: 'entitlements', attributes: { code: _1.to_s.upcase } }},
+                data: { type: 'licenses', attributes: { expiry: } },
+                meta: { issued:, expiry: },
+              ),
+            )
+          end
 
-        instance_exec(&block)
+          instance_exec(&block)
+        end
       end
     end
 
     def within_ce(&)
-      context 'when in a CE context' do
-        before do
-          allow(Keygen).to receive(:ce?).and_return true
-          allow(Keygen).to receive(:ee?).and_return false
+      context 'when in the CE edition' do
+        with_env KEYGEN_EDITION: 'CE' do
+          instance_exec(&)
         end
-
-        instance_exec(&)
       end
     end
   end
