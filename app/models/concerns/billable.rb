a9d375fd76968ab83927929ef6de63ca19eeb915
Zeke Gabrielse <ezekg@yahoo.com>
2023-04-20 14:13:35 -0500

    add account creation to keygen:setup

--- app/models/concerns/billable.rb
+++ app/models/concerns/billable.rb
@@ -7,16 +7,16 @@ module Billable
     # Virtual attribute for tracking referrals
     attr_accessor :referral_id
 
-    after_commit :initialize_billing, on: :create
+    after_commit :initialize_billing, on: :create, unless: -> { Keygen.singleplayer? }
 
     Billing::AVAILABLE_EVENTS.each do |event|
       delegate "#{event}!", to: :billing, allow_nil: true
-      delegate "#{event}", to: :billing, allow_nil: true
+      delegate "#{event}",  to: :billing, allow_nil: true
     end
 
     Billing::AVAILABLE_STATES.each do |state|
       delegate "#{state}?", to: :billing, allow_nil: true
-      delegate "#{state}", to: :billing, allow_nil: true
+      delegate "#{state}",  to: :billing, allow_nil: true
     end
 
     def active?
