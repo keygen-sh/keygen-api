16a216e5d4a261b4a07791af009ff97b72823f18
Zeke Gabrielse <ezekg@yahoo.com>
2023-04-20 10:43:00 -0500

    add base for env asserts

--- config/initializers/keygen.rb
+++ config/initializers/keygen.rb
@@ -1,3 +1,15 @@
 # frozen_string_literal: true
 
 require_dependency Rails.root / 'lib' / 'keygen'
+
+Rails.application.config.to_prepare do
+  if Keygen.singleplayer?
+    account_id = ENV['KEYGEN_ACCOUNT_ID']
+    abort 'environment variable KEYGEN_ACCOUNT_ID is required when running in singleplayer mode' unless
+      account_id.present?
+
+    unless Account.exists?(id: account_id)
+      abort "account #{account_id} does not exist (run `rake keygen:setup` to create it)"
+    end
+  end
+end
