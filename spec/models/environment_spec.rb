113658dd4588cc0ee1b11b9b8527e96bcd802514
Zeke Gabrielse <ezekg@yahoo.com>
2023-04-21 14:39:16 -0500

    fix test titles

--- spec/models/environment_spec.rb
+++ spec/models/environment_spec.rb
@@ -7,7 +7,7 @@ describe Environment, type: :model do
   let(:account) { create(:account) }
 
   %i[isolated shared].each do |isolation|
-    it 'should promote nested users to admins on create' do
+    it "should promote nested #{isolation} users to admins on create" do
       users_attributes = [
         attributes_for(:user),
         attributes_for(:user),
@@ -27,7 +27,7 @@ describe Environment, type: :model do
       expect { environment.save }.to change { account.admins.count }
     end
 
-    it 'should not promote users to admins on create' do
+    it "should not promote #{isolation} users to admins on create" do
       environment = build(:environment, isolation, account:)
       users       = build_list(:user, 3, account:, environment:)
 
@@ -38,7 +38,7 @@ describe Environment, type: :model do
       expect { environment.save }.to_not change { account.admins.count }
     end
 
-    it 'should not promote users to admins on update' do
+    it "should not promote #{isolation} users to admins on update" do
       environment = create(:environment, isolation, account:)
       users       = create_list(:user, 3, account:, environment:)
 
