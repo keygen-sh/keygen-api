f499f9c688a6b028b7924f581db8f5303e3cecb0
Zeke Gabrielse <ezekg@yahoo.com>
2023-04-21 11:55:39 -0500

    add tests for admin promotions

---
+++ spec/models/account_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'spec_helper'
+
+describe Account, type: :model do
+  it 'should promote nested users to admins on create' do
+    users_attributes = [
+      attributes_for(:user),
+      attributes_for(:user),
+      attributes_for(:user),
+    ]
+
+    account = build(:account,
+      users_attributes:,
+    )
+
+    expect { account.save }.to(
+      # Since the new account will have 1 admin automatically, we're expecting >= 3.
+      change { account.admins.count }.by_at_least(3),
+    )
+  end
+
+  it 'should not promote users to admins on create' do
+    account = build(:account)
+    users   = build_list(:user, 3, account:)
+
+    expect { account.save }.to(
+      # Since the new account will have 1 admin automatically, we're expecting <= 1.
+      change { account.admins.count }.by_at_most(1),
+    )
+  end
+
+  it 'should not promote users to admins on update' do
+    account = create(:account)
+    users   = create_list(:user, 3, account:)
+
+    expect { account.touch }.to_not change { account.admins.count }
+  end
+end
