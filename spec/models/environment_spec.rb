174c2cc449445f2efbce37e5e422e7fd7b16c567
Zeke Gabrielse <ezekg@yahoo.com>
2023-04-21 14:34:19 -0500

    add tests for users in nil env

--- spec/models/environment_spec.rb
+++ spec/models/environment_spec.rb
@@ -6,32 +6,47 @@ require 'spec_helper'
 describe Environment, type: :model do
   let(:account) { create(:account) }
 
-  it 'should promote nested users to admins on create' do
-    users_attributes = [
-      attributes_for(:user),
-      attributes_for(:user),
-      attributes_for(:user),
-    ]
+  %i[isolated shared].each do |isolation|
+    it 'should promote nested users to admins on create' do
+      users_attributes = [
+        attributes_for(:user),
+        attributes_for(:user),
+        attributes_for(:user),
+      ]
 
-    environment = build(:environment,
-      users_attributes:,
-      account:,
-    )
+      # We also want to make sure existing users in the nil environment are not promoted
+      create_list(:user, 3,
+        account:,
+      )
 
-    expect { environment.save }.to change { account.admins.count }
-  end
+      environment = build(:environment, isolation,
+        users_attributes:,
+        account:,
+      )
 
-  it 'should not promote users to admins on create' do
-    environment = build(:environment, account:)
-    users       = build_list(:user, 3, account:, environment:)
+      expect { environment.save }.to change { account.admins.count }
+    end
 
-    expect { environment.save }.to_not change { account.admins.count }
-  end
+    it 'should not promote users to admins on create' do
+      environment = build(:environment, isolation, account:)
+      users       = build_list(:user, 3, account:, environment:)
 
-  it 'should not promote users to admins on update' do
-    environment = create(:environment, account:)
-    users       = create_list(:user, 3, account:, environment:)
+      create_list(:user, 3,
+        account:,
+      )
 
-    expect { environment.touch }.to_not change { account.admins.count }
+      expect { environment.save }.to_not change { account.admins.count }
+    end
+
+    it 'should not promote users to admins on update' do
+      environment = create(:environment, isolation, account:)
+      users       = create_list(:user, 3, account:, environment:)
+
+      create_list(:user, 3,
+        account:,
+      )
+
+      expect { environment.touch }.to_not change { account.admins.count }
+    end
   end
 end
