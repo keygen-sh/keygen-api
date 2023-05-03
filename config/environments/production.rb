a56c34cb3bcef7d0c7f39c6172d126deaf24a32f
Zeke Gabrielse <ezekg@yahoo.com>
2023-05-03 16:46:56 -0500

    add secret to setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.hosts.concat(
-    [ENV.fetch('KEYGEN_HOST'), *ENV['KEYGEN_HOSTS'].split(',')].then { |host|
+    [ENV.fetch('KEYGEN_HOST'), *ENV.fetch('KEYGEN_HOSTS', '').split(',')].then { |host|
       host.uniq.compact_blank.map { _1.downcase.strip }
     },
   )
