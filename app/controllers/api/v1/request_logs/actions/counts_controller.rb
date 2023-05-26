d793665353fb55e8f32191347cfc9f0cd8680591
Zeke Gabrielse <ezekg@yahoo.com>
2023-05-26 16:25:36 -0500

    update count sql to use sanitization

--- app/controllers/api/v1/request_logs/actions/counts_controller.rb
+++ app/controllers/api/v1/request_logs/actions/counts_controller.rb
@@ -10,34 +10,36 @@ module Api::V1::RequestLogs::Actions
       authorize! with: RequestLogPolicy
 
       json = Rails.cache.fetch(cache_key, expires_in: 10.minutes, race_condition_ttl: 1.minute) do
-        conn       = ActiveRecord::Base.connection
         start_date = Date.current - 13.days
         end_date   = Date.current
 
         # FIXME(ezekg) Counts should take into account the current environment. Need to update
         #              indexes and all that jazz to also include the environment_id.
-        rows = conn.execute(<<~SQL.squish)
-          SELECT
-            coalesce(logs.count, 0) AS count,
-            series.date             AS period
-          FROM
-            generate_series(date #{conn.quote start_date}, date #{conn.quote end_date}, interval '1 day') AS series (date)
-          LEFT JOIN LATERAL
-            (
-              SELECT
-                count(*)     AS count,
-                created_date AS date,
-                account_id
-              FROM
-                request_logs
-              WHERE
-                account_id   = #{conn.quote current_account.id} AND
-                created_date = series.date
-              GROUP BY
-                account_id,
-                date
-            ) AS logs USING (date);
-        SQL
+        conn = ActiveRecord::Base.connection
+        rows = conn.execute(
+          RequestLog.sanitize_sql([<<~SQL.squish, account_id: current_account.id, start_date:, end_date:])
+            SELECT
+              coalesce(logs.count, 0) AS count,
+              series.date             AS period
+            FROM
+              generate_series(date :start_date, date :end_date, interval '1 day') AS series (date)
+            LEFT JOIN LATERAL
+              (
+                SELECT
+                  count(*)     AS count,
+                  created_date AS date,
+                  account_id
+                FROM
+                  request_logs
+                WHERE
+                  account_id   = :account_id AND
+                  created_date = series.date
+                GROUP BY
+                  account_id,
+                  date
+              ) AS logs USING (date);
+          SQL
+        )
 
         {
           meta: rows.map { [_1['period'].strftime('%Y-%m-%d'), _1['count'].to_i] }
