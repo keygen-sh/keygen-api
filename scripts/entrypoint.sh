55944ec9e49019039ec36cfc9cbc3bda8a678d86
Zeke Gabrielse <ezekg@yahoo.com>
2023-06-06 14:30:36 -0500

    fix issue where signals were not passed through entrypoint

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -10,19 +10,19 @@ case "$@"
 in
 setup)
   echo "Running command: bundle exec rails keygen:setup"
-  bundle exec rails keygen:setup
+  exec bundle exec rails keygen:setup
   ;;
 release)
   echo "Running command: bundle exec rails db:migrate"
-  bundle exec rails db:migrate
+  exec bundle exec rails db:migrate
   ;;
 web)
   echo "Running command: exec rails server -b $BIND -p $PORT"
-  bundle exec rails server -b "$BIND" -p "$PORT"
+  exec bundle exec rails server -b "$BIND" -p "$PORT"
   ;;
 worker)
   echo "Running command: bundle exec sidekiq"
-  bundle exec sidekiq
+  exec bundle exec sidekiq
   ;;
 *)
   echo "Running command: $@"
