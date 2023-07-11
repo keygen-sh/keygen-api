396fae656fa0f997c2cac8231d9339da53fa75e2
Zeke Gabrielse <ezekg@yahoo.com>
2023-07-11 16:33:01 -0500

    add support for openly distributed packages

--- app/policies/product_policy.rb
+++ app/policies/product_policy.rb
@@ -1,6 +1,26 @@
 # frozen_string_literal: true
 
 class ProductPolicy < ApplicationPolicy
+  scope_for :active_record_relation do |relation|
+    relation = relation.for_environment(environment, strict: environment.nil?) if
+      relation.respond_to?(:for_environment)
+
+    case bearer
+    in role: Role(:admin | :developer | :read_only | :sales_agent | :support_agent)
+      relation.all
+    in role: Role(:environment) if relation.respond_to?(:for_environment)
+      relation.for_environment(bearer.id)
+    in role: Role(:product) if relation.respond_to?(:for_product)
+      relation.for_product(bearer.id)
+    in role: Role(:user) if relation.respond_to?(:for_user)
+      relation.for_user(bearer.id)
+    in role: Role(:license) if relation.respond_to?(:for_license)
+      relation.for_license(bearer.id)
+    else
+      relation.open
+    end
+  end
+
   def index?
     verify_permissions!('product.read')
     verify_environment!(
