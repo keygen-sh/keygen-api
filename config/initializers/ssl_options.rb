79416401c6c4b38a10fc017f22a9c22d2fe7aff9
Zeke Gabrielse <ezekg@yahoo.com>
2023-08-04 09:08:34 -0500

    update force tls to exclude health checks

--- config/initializers/ssl_options.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-# Be sure to restart your server when you modify this file.
-
-# Configure SSL options to enable HSTS with subdomains.
-Rails.application.config.ssl_options = { hsts: { subdomains: true } }
