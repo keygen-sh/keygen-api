29f5971399c1140c8a75961ffa86e36448f5b469
Zeke Gabrielse <ezekg@yahoo.com>
2023-08-15 10:30:51 -0500

    add tauri engine

---
+++ app/controllers/api/v1/release_engines/tauri/upgrades_controller.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+module Api::V1::ReleaseEngines
+  class Tauri::UpgradesController < Api::V1::BaseController
+    before_action :scope_to_current_account!
+    before_action :require_active_subscription!
+    before_action :authenticate_with_token
+    before_action :set_package, only: %i[show]
+
+    typed_query {
+      param :constraint, type: :string, optional: true
+      param :channel, type: :string, optional: true
+    }
+    def show
+      authorize! package
+
+      releases = authorized_scope(package.releases)
+      release  = releases.find_by!(version: params[:current_version])
+      authorize! release,
+        to: :upgrade?
+
+      kwargs  = upgrade_query.slice(:constraint, :channel)
+      upgrade = release.upgrade!(**kwargs)
+      authorize! upgrade
+
+      artifact = upgrade.artifacts.joins(:platform, :arch, :filetype)
+                                  .reorder(filename: :desc) # so that NSIS takes precedence over MSI on conflict
+                                  .find_by!(
+                                    platform: { key: params[:target] },
+                                    arch: { key: params[:arch] },
+                                    filetype: { key: %w[gz zip] },
+                                  )
+      authorize! artifact
+
+      # See: https://tauri.app/v1/guides/distribution/updater
+      render json: {
+        version: upgrade.version,
+        pub_date: upgrade.created_at,
+        url: vanity_v1_account_release_artifact_url(artifact.account, artifact, filename: artifact.filename),
+        signature: artifact.signature,
+        notes: upgrade.description,
+      }
+    rescue ActiveRecord::RecordNotFound
+      render_no_content
+    end
+
+    private
+
+    attr_reader :package
+
+    def set_package
+      Current.resource = @package = FindByAliasService.call(
+        authorized_scope(current_account.release_packages.tauri),
+        id: params[:package],
+        aliases: :key,
+      )
+    end
+  end
+end
--- config/initializers/uuid.rb
+++ config/initializers/uuid.rb
@@ -3,6 +3,9 @@
 # This is used throughout the app to check if a value is a UUID
 UUID_RE = /\A[0-9A-F]{8}-?[0-9A-F]{4}-?[4][0-9A-F]{3}-?[89AB][0-9A-F]{3}-?[0-9A-F]{12}\z/i
 
+# Above pattern without anchors for URLs
+UUID_URL_RE = /[0-9A-F]{8}-?[0-9A-F]{4}-?[4][0-9A-F]{3}-?[89AB][0-9A-F]{3}-?[0-9A-F]{12}/i
+
 # This is used throughout the app for checking if a value is a partial UUID
 UUID_CHAR_RE = /\A[-0-9A-F]+\z/i
 
