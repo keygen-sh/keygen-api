bdaa6c6468876d64be2525dcf25a681cbfd886dd
Zeke Gabrielse <ezekg@yahoo.com>
2023-08-18 10:37:18 -0500

    add tauri to allowed package engines

--- app/controllers/api/v1/release_packages_controller.rb
+++ app/controllers/api/v1/release_packages_controller.rb
@@ -32,7 +32,7 @@ module Api::V1
         param :attributes, type: :hash do
           param :name, type: :string
           param :key, type: :string
-          param :engine, type: :string, inclusion: { in: %w[pypi] }, optional: true, allow_nil: true,
+          param :engine, type: :string, inclusion: { in: %w[pypi tauri] }, optional: true, allow_nil: true,
             transform: -> (_, key) {
               [:engine_attributes, { key: }]
             }
