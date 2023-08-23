10159f593a5cd6e0c83fe038495aca4598d836f9
Zeke Gabrielse <ezekg@yahoo.com>
2023-08-23 14:37:42 -0500

    add tests for components

---
+++ spec/factories/machine_component.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :machine_component, aliases: %i[component] do
+    initialize_with { new(**attributes.reject { NIL_ENVIRONMENT == _2 }) }
+
+    sequence :name, %w[HWID HDDID SSD CPU MOBO IP MAC].cycle
+    fingerprint { SecureRandom.hex(16) }
+
+    account     { nil }
+    environment { NIL_ENVIRONMENT }
+    machine     { build(:machine, account:, environment:) }
+
+    trait :in_isolated_environment do
+      environment { build(:environment, :isolated, account:) }
+    end
+
+    trait :isolated do
+      in_isolated_environment
+    end
+
+    trait :in_shared_environment do
+      environment { build(:environment, :shared, account:) }
+    end
+
+    trait :shared do
+      in_shared_environment
+    end
+
+    trait :in_nil_environment do
+      environment { nil }
+    end
+
+    trait :global do
+      in_nil_environment
+    end
+  end
+end
