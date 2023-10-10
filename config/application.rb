3752c3117dbfed578e3c5f4a8addb4697589a2da
Zeke Gabrielse <ezekg@yahoo.com>
2023-10-09 21:55:07 -0500

    upgrade to rails 7.1

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ Bundler.require *Rails.groups
 
 module Keygen
   class Application < Rails::Application
-    config.load_defaults 7.0
+    config.load_defaults 7.1
 
     config.generators do |generator|
       # Use UUIDs for table primary keys
