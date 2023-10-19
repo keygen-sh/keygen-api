878f59925a1dace0b7b208eef5466b852355ccb0
Zeke Gabrielse <ezekg@yahoo.com>
2023-10-19 09:44:57 -0500

    add config env vars to other pruning workers
    - this also improves performance by limiting the date range of the pruning op

--- app/workers/prune_request_logs_worker.rb
+++ app/workers/prune_request_logs_worker.rb
@@ -1,5 +1,6 @@
 class PruneRequestLogsWorker < BaseWorker
   BACKLOG_DAYS = ENV.fetch('KEYGEN_PRUNE_REQUEST_BACKLOG_DAYS') { 30 }.to_i
+  TARGET_DAYS  = ENV.fetch('KEYGEN_PRUNE_REQUEST_TARGET_DAYS')  { 1 }.to_i
   BATCH_SIZE   = ENV.fetch('KEYGEN_PRUNE_BATCH_SIZE')           { 1_000 }.to_i
   BATCH_WAIT   = ENV.fetch('KEYGEN_PRUNE_BATCH_WAIT')           { 1 }.to_f
 
@@ -11,15 +12,19 @@ class PruneRequestLogsWorker < BaseWorker
     return if
       BACKLOG_DAYS <= 0
 
-    accounts = Account.where(<<~SQL.squish, BACKLOG_DAYS.days.ago.beginning_of_day)
+    end_date   = BACKLOG_DAYS.days.ago.beginning_of_day
+    start_date = (end_date - TARGET_DAYS.day).beginning_of_day
+
+    accounts = Account.where(<<~SQL.squish, start_date:, end_date:)
       EXISTS (
         SELECT
           1
         FROM
           "request_logs"
         WHERE
-          "request_logs"."account_id" = "accounts"."id" AND
-          "request_logs"."created_at" < ?
+          "request_logs"."account_id"  = "accounts"."id" AND
+          "request_logs"."created_at" >= :start_date     AND
+          "request_logs"."created_at" <  :end_date
         LIMIT
           1
       )
@@ -34,8 +39,8 @@ class PruneRequestLogsWorker < BaseWorker
       Keygen.logger.info "[workers.prune-request-logs] Pruning rows: account_id=#{account_id}"
 
       loop do
-        logs = account.request_logs
-                      .where('created_at < ?', BACKLOG_DAYS.days.ago.beginning_of_day)
+        logs = account.request_logs.where('created_at >= ?', start_date)
+                                   .where('created_at < ?', end_date)
 
         batch += 1
         count = logs.limit(BATCH_SIZE)
