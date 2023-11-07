da4ed07974952fff5e625d7ea1ff16d653b8d905
Zeke Gabrielse <ezekg@yahoo.com>
2023-11-07 12:37:40 -0600

    update license user association from has one to has many

---
+++ db/migrate/20231106203122_create_license_users.rb
@@ -0,0 +1,18 @@
+class CreateLicenseUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :license_users, id: :uuid, default: -> { 'uuid_generate_v4()' } do |t|
+      t.uuid :account_id,     null: false
+      t.uuid :environment_id, null: true
+      t.uuid :license_id,     null: false
+      t.uuid :user_id,        null: false
+
+      t.timestamps
+
+      t.index %i[account_id created_at], order: { created_at: :desc }
+      t.index %i[account_id license_id user_id], unique: true
+      t.index %i[environment_id]
+      t.index %i[license_id]
+      t.index %i[user_id]
+    end
+  end
+end
