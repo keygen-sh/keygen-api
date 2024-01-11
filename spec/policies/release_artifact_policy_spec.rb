d9cc55758ef18be6bfe1bf1f959eb5b3893461d3
Zeke Gabrielse <ezekg@yahoo.com>
2024-01-11 14:35:05 -0600

    update policies to allow licensees a subset of actions

--- spec/policies/release_artifact_policy_spec.rb
+++ spec/policies/release_artifact_policy_spec.rb
@@ -613,7 +613,109 @@ describe ReleaseArtifactPolicy, type: :policy do
   end
 
   with_role_authorization :user do
-    with_bearer_trait :with_licenses do
+    with_bearer_trait :with_owned_licenses do
+      with_scenarios %i[accessing_its_artifacts] do
+        with_token_authentication do
+          with_permissions %w[artifact.read release.read] do
+            allows :index
+          end
+
+          with_wildcard_permissions { allows :index }
+          with_default_permissions  { allows :index }
+          without_permissions       { denies :index }
+        end
+      end
+
+      with_scenarios %i[accessing_its_artifact] do
+        with_token_authentication do
+          with_permissions %w[artifact.read] do
+            allows :show
+          end
+
+          with_wildcard_permissions do
+            denies :create, :update, :destroy
+            allows :show
+          end
+
+          with_default_permissions do
+            denies :create, :update, :destroy
+            allows :show
+          end
+
+          without_permissions do
+            denies :show, :create, :update, :destroy
+          end
+        end
+      end
+
+      with_scenarios %i[accessing_artifacts] do
+        with_artifact_traits %i[open] do
+          with_token_authentication do
+            with_permissions %w[artifact.read] do
+              allows :index
+            end
+
+            with_wildcard_permissions { allows :index }
+            with_default_permissions  { allows :index }
+            without_permissions       { denies :index }
+          end
+        end
+
+        with_token_authentication do
+          with_permissions %w[artifact.read release.read] do
+            denies :index
+          end
+
+          with_wildcard_permissions { denies :index }
+          with_default_permissions  { denies :index }
+          without_permissions       { denies :index }
+        end
+      end
+
+      with_scenarios %i[accessing_an_artifact] do
+        with_artifact_traits %i[open] do
+          with_token_authentication do
+            with_permissions %w[artifact.read] do
+              allows :show
+            end
+
+            with_wildcard_permissions do
+              denies :create, :update, :destroy
+              allows :show
+            end
+
+            with_default_permissions do
+              denies :create, :update, :destroy
+              allows :show
+            end
+
+            without_permissions do
+              denies :show, :create, :update, :destroy
+            end
+          end
+        end
+
+        with_token_authentication do
+          with_permissions %w[artifact.read] do
+            denies :show
+          end
+
+          with_wildcard_permissions do
+            denies :show, :create, :update, :destroy
+          end
+
+          with_default_permissions do
+            denies :show, :create, :update, :destroy
+          end
+
+          without_permissions do
+            denies :show, :create, :update, :destroy
+          end
+        end
+      end
+    end
+
+    with_bearer_trait :with_user_licenses do
       with_scenarios %i[accessing_its_artifacts] do
         with_token_authentication do
           with_permissions %w[artifact.read release.read] do
