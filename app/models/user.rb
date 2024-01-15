a5545259c284f0da553160315ea91a1dd4bbb7d5
Zeke Gabrielse <ezekg@yahoo.com>
2024-01-15 12:07:10 -0600

    remove superfluous license_ids method

--- app/models/user.rb
+++ app/models/user.rb
@@ -349,12 +349,12 @@ class User < ApplicationRecord
     end
   }
 
+  # FIXME(ezekg) Selecting on ID isn't supported by our association scopes.
+  def product_ids = products.reorder(nil).ids
+  def policy_ids  = policies.reorder(nil).ids
+
   def entitlement_codes = entitlements.reorder(nil).codes
   def entitlement_ids   = entitlements.reorder(nil).ids
-  def product_ids       = products.reorder(nil).ids
-  def policy_ids        = policies.reorder(nil).ids
-  def license_ids       = licenses.reorder(nil).ids
-
   def entitlements
     entl = Entitlement.where(account_id: account_id).distinct
 
