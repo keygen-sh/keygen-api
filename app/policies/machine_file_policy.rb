57a8b37ad99788369758df519c44a135bfcc132b
Zeke Gabrielse <ezekg@yahoo.com>
2024-01-15 11:50:28 -0600

    add specs for license file policies

--- app/policies/machine_file_policy.rb
+++ app/policies/machine_file_policy.rb
@@ -12,7 +12,7 @@ class MachineFilePolicy < ApplicationPolicy
       allow!
     in role: Role(:product) if record.product == bearer
       allow!
-    in role: Role(:user) if record.owner == bearer || bearer.licenses.exists?(record.id)
+    in role: Role(:user) if record.owner == bearer || bearer.licenses.exists?(record.license_id)
       allow!
     in role: Role(:license) if record.license == bearer
       allow!
