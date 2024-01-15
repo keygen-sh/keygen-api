973cca35832fb07fc02c77519efe3d914eb30db8
Zeke Gabrielse <ezekg@yahoo.com>
2024-01-15 11:50:28 -0600

    add specs for license file policies

---
+++ spec/factories/machine_file.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :machine_file do
+    initialize_with { new(**attributes.reject { _2 in NIL_ACCOUNT | NIL_ENVIRONMENT }) }
+
+    account     { NIL_ACCOUNT }
+    environment { NIL_ENVIRONMENT }
+    license     { build(:license, account:, environment:) }
+    machine     { build(:machine, account:, environment:, license:) }
+
+    trait :in_isolated_environment do
+      environment { build(:environment, :isolated, account:) }
+    end
+
+    trait :isolated do
+      in_isolated_environment
+    end
+
+    trait :in_shared_environment do
+      environment { build(:environment, :shared, account:) }
+    end
+
+    trait :shared do
+      in_shared_environment
+    end
+
+    trait :in_nil_environment do
+      environment { nil }
+    end
+
+    trait :global do
+      in_nil_environment
+    end
+  end
+end
---
+++ spec/policies/machine_file_policy_spec.rb
@@ -0,0 +1,352 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'spec_helper'
+
+describe MachineFilePolicy, type: :policy do
+  subject { described_class.new(record, account:, environment:, bearer:, token:) }
+
+  with_role_authorization :admin do
+    with_scenarios %i[accessing_a_machine accessing_its_machine_file] do
+      with_token_authentication do
+        with_permissions %w[machine.read] do
+          without_token_permissions { denies :show }
+
+          allows :show
+        end
+
+        with_wildcard_permissions do
+          without_token_permissions do
+            denies :show
+          end
+
+          allows :show
+        end
+
+        with_default_permissions do
+          without_token_permissions do
+            denies :show
+          end
+
+          allows :show
+        end
+
+        without_permissions do
+          denies :show
+        end
+
+        within_environment :isolated do
+          with_bearer_and_token_trait :in_shared_environment do
+            denies :show
+          end
+
+          with_bearer_and_token_trait :in_nil_environment do
+            denies :show
+          end
+
+          allows :show
+        end
+
+        within_environment :shared do
+          with_bearer_and_token_trait :in_isolated_environment do
+            denies :show
+          end
+
+          with_bearer_and_token_trait :in_nil_environment do
+            allows :show
+          end
+
+          allows :show
+        end
+
+        within_environment nil do
+          with_bearer_and_token_trait :in_isolated_environment do
+            denies :show
+          end
+
+          with_bearer_and_token_trait :in_shared_environment do
+            denies :show
+          end
+
+          allows :show
+        end
+      end
+    end
+
+    with_scenarios %i[accessing_another_account accessing_a_machine accessing_its_machine_file] do
+      with_token_authentication do
+        with_permissions %w[machine.read] do
+          denies :show
+        end
+
+        with_wildcard_permissions do
+          denies :show
+        end
+
+        with_default_permissions do
+          denies :show
+        end
+
+        without_permissions do
+          denies :show
+        end
+      end
+    end
+  end
+
+  with_role_authorization :environment do
+    within_environment :self do
+      with_scenarios %i[accessing_a_machine accessing_its_machine_file] do
+        with_token_authentication do
+          with_permissions %w[machine.read] do
+            without_token_permissions { denies :show }
+
+            allows :show
+          end
+
+          with_wildcard_permissions do
+            allows :show
+          end
+
+          with_default_permissions do
+            allows :show
+          end
+
+          without_permissions do
+            denies :show
+          end
+        end
+      end
+    end
+
+    with_scenarios %i[accessing_a_machine accessing_its_machine_file] do
+      with_token_authentication do
+        with_permissions %w[machine.read] do
+          without_token_permissions { denies :show }
+
+          denies :show
+        end
+
+        with_wildcard_permissions do
+          denies :show
+        end
+
+        with_default_permissions do
+          denies :show
+        end
+
+        without_permissions do
+          denies :show
+        end
+      end
+    end
+  end
+
+  with_role_authorization :product do
+    with_scenarios %i[accessing_its_machine accessing_its_machine_file] do
+      with_token_authentication do
+        with_permissions %w[machine.read] do
+          without_token_permissions { denies :show }
+
+          allows :show
+        end
+
+        with_wildcard_permissions do
+          allows :show
+        end
+
+        with_default_permissions do
+          allows :show
+        end
+
+        without_permissions do
+          denies :show
+        end
+      end
+    end
+
+    with_scenarios %i[accessing_a_machine accessing_its_machine_file] do
+      with_token_authentication do
+        with_permissions %w[machine.read] do
+          without_token_permissions { denies :show }
+
+          denies :show
+        end
+
+        with_wildcard_permissions do
+          denies :show
+        end
+
+        with_default_permissions do
+          denies :show
+        end
+
+        without_permissions do
+          denies :show
+        end
+      end
+    end
+  end
+
+  with_role_authorization :license do
+    with_scenarios %i[accessing_its_machine accessing_its_machine_file] do
+      with_license_authentication do
+        with_permissions %w[machine.read] do
+          allows :show
+        end
+
+        with_wildcard_permissions do
+          allows :show
+        end
+
+        with_default_permissions do
+          allows :show
+        end
+
+        without_permissions do
+          denies :show
+        end
+      end
+
+      with_token_authentication do
+        with_permissions %w[machine.read] do
+          without_token_permissions { denies :show }
+
+          allows :show
+        end
+
+        with_wildcard_permissions do
+          allows :show
+        end
+
+        with_default_permissions do
+          allows :show
+        end
+
+        without_permissions do
+          denies :show
+        end
+      end
+    end
+
+    with_scenarios %i[accessing_a_machine accessing_its_machine_file] do
+      with_license_authentication do
+        with_permissions %w[machine.read] do
+          denies :show
+        end
+
+        with_wildcard_permissions do
+          denies :show
+        end
+
+        with_default_permissions do
+          denies :show
+        end
+
+        without_permissions do
+          denies :show
+        end
+      end
+
+      with_token_authentication do
+        with_permissions %w[machine.read] do
+          without_token_permissions { denies :show }
+
+          denies :show
+        end
+
+        with_wildcard_permissions do
+          denies :show
+        end
+
+        with_default_permissions do
+          denies :show
+        end
+
+        without_permissions do
+          denies :show
+        end
+      end
+    end
+  end
+
+  with_role_authorization :user do
+    with_bearer_trait :with_owned_licenses do
+      with_scenarios %i[accessing_its_machine accessing_its_machine_file] do
+        with_token_authentication do
+          with_permissions %w[machine.read] do
+            without_token_permissions { denies :show }
+
+            allows :show
+          end
+
+          with_wildcard_permissions do
+            allows :show
+          end
+
+          with_default_permissions do
+            allows :show
+          end
+
+          without_permissions do
+            denies :show
+          end
+        end
+      end
+    end
+
+    with_bearer_trait :with_user_licenses do
+      with_scenarios %i[accessing_its_machine accessing_its_machine_file] do
+        with_token_authentication do
+          with_permissions %w[machine.read] do
+            without_token_permissions { denies :show }
+
+            allows :show
+          end
+
+          with_wildcard_permissions do
+            allows :show
+          end
+
+          with_default_permissions do
+            allows :show
+          end
+
+          without_permissions do
+            denies :show
+          end
+        end
+      end
+    end
+
+    with_scenarios %i[accessing_a_machine accessing_its_machine_file] do
+      with_token_authentication do
+        with_permissions %w[machine.read] do
+          without_token_permissions { denies :show }
+
+          denies :show
+        end
+
+        with_wildcard_permissions do
+          denies :show
+        end
+
+        with_default_permissions do
+          denies :show
+        end
+
+        without_permissions do
+          denies :show
+        end
+      end
+    end
+  end
+
+  without_authorization do
+    with_scenarios %i[accessing_a_machine accessing_its_machine_file] do
+      without_authentication do
+        denies :show
+      end
+    end
+  end
+end
