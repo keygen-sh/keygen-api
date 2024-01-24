195457bbfd196870a4cad43f2d5bc25f2ea06345
Zeke Gabrielse <ezekg@yahoo.com>
2024-01-24 15:22:58 -0600

    update cache key version format to include minor version

--- config/initializers/cache_key_version.rb
+++ config/initializers/cache_key_version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-# NOTE(ezekg) This saves us the headache of busting caches when
-#             upgrading Rails (which caused downtime before)
-rails_major_version = Rails.version.split('.').first
+# NOTE(ezekg) This saves us the headache of busting caches when upgrading
+#             Rails (which has caused downtime before).
+rails_major_version, rails_minor_version, * = Rails.version.split('.')
 
-CACHE_KEY_VERSION = "v#{rails_major_version}"
+CACHE_KEY_VERSION = "v#{rails_major_version}.#{rails_minor_version}"
