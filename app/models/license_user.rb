c523e8f50655b35055d1a87a1ce7503472ec4f15
Zeke Gabrielse <ezekg@yahoo.com>
2024-01-26 10:42:01 -0600

    fix issue where user machines where not nilified on detach

--- app/models/license_user.rb
+++ app/models/license_user.rb
@@ -13,6 +13,8 @@ class LicenseUser < ApplicationRecord
   has_environment default: -> { license&.environment_id }
   has_account default: -> { license&.account_id }
 
+  after_destroy :nullify_machines_for_user
+
   validates :user,
     uniqueness: { message: 'already exists', scope: %i[account_id license_id user_id] },
     scope: { by: :account_id }
@@ -25,5 +27,16 @@ class LicenseUser < ApplicationRecord
       user == license.owner
 
     errors.add :user, :conflict, message: 'already exists (user is attached through owner)'
+  end
+
+  private
+
+  def nullify_machines_for_user
+    # TODO(ezekg) Should add a policy configuration to allow you to destroy
+    #             these machines instead of nilifying. (Do async.)
+    license.machines.where(owner: user)
+                    .update_all(
+                      owner_id: nil,
+                    )
   end
 end
