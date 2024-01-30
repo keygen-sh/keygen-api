51abca167de3ad24e933c46b847cc1e1279c143c
Zeke Gabrielse <ezekg@yahoo.com>
2024-01-30 15:43:47 -0600

    update machine-related authz tests to cover teammates

--- spec/policies/machine_component_policy_spec.rb
+++ spec/policies/machine_component_policy_spec.rb
@@ -723,6 +723,66 @@ describe MachineComponentPolicy, type: :policy do
           end
 
           with_permissions %w[component.create] do
+            without_token_permissions { denies :create }
+
+            allows :create
+          end
+
+          with_permissions %w[component.update] do
+            without_token_permissions { denies :update }
+
+            allows :update
+          end
+
+          with_permissions %w[component.delete] do
+            without_token_permissions { denies :destroy }
+
+            allows :destroy
+          end
+
+          with_wildcard_permissions do
+            without_token_permissions do
+              denies :show, :create, :update, :destroy
+            end
+
+            allows :show, :create, :update, :destroy
+          end
+
+          with_default_permissions do
+            without_token_permissions do
+              denies :show, :create, :update, :destroy
+            end
+
+            allows :show, :create, :update, :destroy
+          end
+
+          without_permissions do
+            denies :show, :create, :update, :destroy
+          end
+        end
+      end
+
+      with_scenarios %i[accessing_our_machine_components] do
+        with_token_authentication do
+          with_permissions %w[component.read] do
+            allows :index
+          end
+
+          with_wildcard_permissions { allows :index }
+          with_default_permissions  { allows :index }
+          without_permissions       { denies :index }
+        end
+      end
+
+      with_scenarios %i[accessing_our_machine_component] do
+        with_token_authentication do
+          with_permissions %w[component.read] do
+            without_token_permissions { denies :show }
+
+            allows :show
+          end
+
+          with_permissions %w[component.create] do
             denies :create
           end
 
@@ -750,38 +810,6 @@ describe MachineComponentPolicy, type: :policy do
 
             denies :create, :update, :destroy
             allows :show
-          end
-
-          without_permissions do
-            denies :show, :create, :update, :destroy
-          end
-        end
-      end
-
-      with_scenarios %i[accessing_machine_components] do
-        with_token_authentication do
-          with_permissions %w[component.read] do
-            denies :index
-          end
-
-          with_wildcard_permissions { denies :index }
-          with_default_permissions  { denies :index }
-          without_permissions       { denies :index }
-        end
-      end
-
-      with_scenarios %i[accessing_a_machine_component] do
-        with_token_authentication do
-          with_permissions %w[component.read] do
-            denies :show
-          end
-
-          with_wildcard_permissions do
-            denies :show, :create, :update, :destroy
-          end
-
-          with_default_permissions do
-            denies :show, :create, :update, :destroy
           end
 
           without_permissions do
