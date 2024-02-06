089443992f9b94db7a8c86b6423a47f41b3a8ba3
Zeke Gabrielse <ezekg@yahoo.com>
2024-02-06 08:30:41 -0600

    add profiling to sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,8 +2,9 @@
 
 unless ENV.key?('NO_SENTRY')
   Sentry.init do |config|
-    config.dsn                = ENV.fetch('SENTRY_DSN')         { nil }
-    config.traces_sample_rate = ENV.fetch('SENTRY_SAMPLE_RATE') { 0.0 }.to_f
-    config.environment        = Rails.env
+    config.dsn                  = ENV.fetch('SENTRY_DSN')                  { nil }
+    config.traces_sample_rate   = ENV.fetch('SENTRY_TRACES_SAMPLE_RATE')   { 0.0 }.to_f
+    config.profiles_sample_rate = ENV.fetch('SENTRY_PROFILES_SAMPLE_RATE') { 0.0 }.to_f
+    config.environment          = Rails.env
   end
 end
