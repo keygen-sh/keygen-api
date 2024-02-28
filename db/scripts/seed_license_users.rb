fc6cc8cf2035ebf9a9575d9ca97a37482a2b8de8
Zeke Gabrielse <ezekg@yahoo.com>
2024-02-28 09:45:26 -0600

    Revert "refactor license user seed script to use batches"
    This reverts commit 250c431724188d2f76a762996904697053af713b.

--- db/scripts/seed_license_users.rb
+++ db/scripts/seed_license_users.rb
@@ -3,26 +3,18 @@
 # heroku run:detached -e SLEEP_DURATION=0.5 --tail \
 #   rails runner db/scripts/seed_license_users.rb
 
-BATCH_SIZE     = ENV.fetch('BATCH_SIZE')     { 1_000 }.to_i
 SLEEP_DURATION = ENV.fetch('SLEEP_DURATION') { 1 }.to_f
 
-licenses_with_users = License.where_assoc_not_exists(:license_users)
-                             .where_assoc_exists(:user)
-                             .eager_load(
-                               :account,
-                               :user,
-                             )
+licenses = License.where_assoc_not_exists(:license_users)
+                  .where_assoc_exists(:user)
+                  .includes(:user)
 
-Rails.logger.info "Seeding license_users for #{licenses_with_users.count} total licenses"
+Rails.logger.info "Seeding #{licenses.count} license_users"
 
-licenses_with_users.find_in_batches(batch_size: BATCH_SIZE) do |licenses|
-  Rails.logger.info "Seeding batch of #{licenses.count} licenses"
+licenses.find_each do |license|
+  Rails.logger.info "Seeding license_user for license=#{license.id} <> user=#{license.user_id}"
 
-  licenses.each do |license|
-    Rails.logger.info "Seeding license_user for license=#{license.id} <> user=#{license.user_id}"
-
-    license.users << license.user
-  end
+  license.users << license.user
 
   sleep SLEEP_DURATION
 end
