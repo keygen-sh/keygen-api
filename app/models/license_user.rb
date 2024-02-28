d05f43066f3b882721c01368f2e20ecc336aba91
Zeke Gabrielse <ezekg@yahoo.com>
2024-02-28 09:45:26 -0600

    Revert "update condition to be clearer"
    This reverts commit c67af1dd9d28ed8911e2e4b8f9c3cd5964ea9e07.

--- app/models/license_user.rb
+++ app/models/license_user.rb
@@ -23,8 +23,8 @@ class LicenseUser < ApplicationRecord
     next unless
       license.present? && user.present?
 
-    next if
-      user != license.owner
+    next unless
+      user == license.owner
 
     errors.add :user, :conflict, message: 'already exists (user is attached through owner)'
   end
