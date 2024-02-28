5104a088198a83e38b1ac7f485c1b7ccc2887749
Zeke Gabrielse <ezekg@yahoo.com>
2024-02-28 09:45:26 -0600

    Revert "remove superfluous nil"
    This reverts commit 2802140b902ccabc8ae8e08583b4cde1e8dc5805.

--- lib/union_of.rb
+++ lib/union_of.rb
@@ -22,8 +22,7 @@ module UnionOf
 
   class Association < ReadonlyAssociation
     def association_scope
-      return if
-        klass.nil?
+      return if klass.nil?
 
       @association_scope ||= Scope.create.scope(self)
     end
@@ -45,7 +44,7 @@ module UnionOf
         primary_key = reflection.active_record_primary_key
 
         # FIXME(ezekg) Find an alternative to this private API.
-        next if
+        next nil if
           reflection.belongs_to? && !association.send(:foreign_key_present?)
 
         association.scope.select(primary_key)
