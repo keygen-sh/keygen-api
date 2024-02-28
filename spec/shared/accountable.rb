cc938acbe4219ce33667548259af730b51dd8d4a
Zeke Gabrielse <ezekg@yahoo.com>
2024-02-28 09:45:26 -0600

    Revert "fix useless accountable test"
    This reverts commit 29adee1e511330203bda783c9792366266609836.

--- spec/shared/accountable.rb
+++ spec/shared/accountable.rb
@@ -28,7 +28,7 @@ shared_examples :accountable do
       end
 
       context 'with current account' do
-        before { Current.account = create(:account) }
+        before { Current.account = account }
         after  { Current.account = nil }
 
         it 'should raise when account is nil' do
