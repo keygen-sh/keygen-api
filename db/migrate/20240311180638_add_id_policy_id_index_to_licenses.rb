989b93761010b5e8e025bb48631a6c65aa20a124
Zeke Gabrielse <ezekg@yahoo.com>
2024-03-12 15:20:29 -0500

    update indexes

--- db/migrate/20240311180638_add_id_policy_id_index_to_licenses.rb
+++
@@ -1,7 +0,0 @@
-class AddIdPolicyIdIndexToLicenses < ActiveRecord::Migration[7.1]
-  disable_ddl_transaction!
-
-  def change
-    add_index :licenses, %i[id policy_id account_id], algorithm: :concurrently
-  end
-end
--- db/migrate/20240312183410_add_license_id_account_id_index_to_license_users.rb
+++
@@ -1,5 +0,0 @@
-class AddLicenseIdAccountIdIndexToLicenseUsers < ActiveRecord::Migration[7.1]
-  def change
-    add_index :license_users, %i[license_id account_id]
-  end
-end
