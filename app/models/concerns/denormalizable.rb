d68708ac58b5b3969678a11eed0b43d4f8445a3a
Zeke Gabrielse <ezekg@yahoo.com>
2024-03-26 17:06:41 -0500

    refactor denormalizable to be dry

--- app/models/concerns/denormalizable.rb
+++ app/models/concerns/denormalizable.rb
@@ -42,11 +42,10 @@ module Denormalizable
                                   end
 
         unless reflection.collection?
-          # FIXME(ezekg) Dedupe all of this mess.
-          after_initialize  -> { write_attribute(prefixed_attribute_name, send(association_name)&.send(attribute_name)) }, if: :"#{reflection.foreign_key}_changed?"
-          before_validation -> { write_attribute(prefixed_attribute_name, send(association_name)&.send(attribute_name)) }, if: :"#{reflection.foreign_key}_changed?", on: :create
-          before_create     -> { write_attribute(prefixed_attribute_name, send(association_name)&.send(attribute_name)) }, if: :"#{reflection.foreign_key}_changed?"
-          before_update     -> { write_attribute(prefixed_attribute_name, send(association_name)&.send(attribute_name)) }, if: :"#{reflection.foreign_key}_changed?"
+          after_initialize  -> { write_denormalized_attribute_from_unpersisted_record(association_name, attribute_name, prefixed_attribute_name) }, if: :"#{reflection.foreign_key}_changed?"
+          before_validation -> { write_denormalized_attribute_from_unpersisted_record(association_name, attribute_name, prefixed_attribute_name) }, if: :"#{reflection.foreign_key}_changed?", on: :create
+          before_create     -> { write_denormalized_attribute_from_persisted_record(association_name, attribute_name, prefixed_attribute_name) },   if: :"#{reflection.foreign_key}_changed?"
+          before_update     -> { write_denormalized_attribute_from_persisted_record(association_name, attribute_name, prefixed_attribute_name) },   if: :"#{reflection.foreign_key}_changed?"
         else
           raise ArgumentError, "must be a singular association: #{association_name.inspect}"
         end
@@ -69,21 +68,63 @@ module Denormalizable
                                   end
 
         # FIXME(ezekg) Set to nil on destroy unless the association is dependent?
-        # FIXME(ezekg) Dedupe all of this mess.
         if reflection.collection?
-          after_initialize  -> { send(association_name).each { _1.write_attribute(prefixed_attribute_name, read_attribute(attribute_name)) } }, if: :"#{attribute_name}_changed?"
-          before_validation -> { send(association_name).each { _1.write_attribute(prefixed_attribute_name, read_attribute(attribute_name)) } }, if: :"#{attribute_name}_changed?", on: :create
-          before_create     -> { send(association_name).update_all(prefixed_attribute_name => read_attribute(attribute_name)) }, if: :"#{attribute_name}_changed?"
-          before_update     -> { send(association_name).update_all(prefixed_attribute_name => read_attribute(attribute_name)) }, if: :"#{attribute_name}_changed?"
+          after_initialize  -> { write_denormalized_attribute_to_unpersisted_relation(association_name, prefixed_attribute_name, attribute_name) }, if: :"#{attribute_name}_changed?"
+          before_validation -> { write_denormalized_attribute_to_unpersisted_relation(association_name, prefixed_attribute_name, attribute_name) }, if: :"#{attribute_name}_changed?", on: :create
+          before_create     -> { write_denormalized_attribute_to_persisted_relation(association_name, prefixed_attribute_name, attribute_name) },   if: :"#{attribute_name}_changed?"
+          before_update     -> { write_denormalized_attribute_to_persisted_relation(association_name, prefixed_attribute_name, attribute_name) },   if: :"#{attribute_name}_changed?"
         else
-          after_initialize  -> { send(association_name).write_attribute(prefixed_attribute_name, read_attribute(attribute_name)) }, if: :"#{attribute_name}_changed?"
-          before_validation -> { send(association_name).write_attribute(prefixed_attribute_name, read_attribute(attribute_name)) }, if: :"#{attribute_name}_changed?", on: :create
-          before_create     -> { send(association_name).update(prefixed_attribute_name => read_attribute(attribute_name)) }, if: :"#{attribute_name}_changed?"
-          before_update     -> { send(association_name).update(prefixed_attribute_name => read_attribute(attribute_name)) }, if: :"#{attribute_name}_changed?"
+          after_initialize  -> { write_denormalized_attribute_to_unpersisted_record(association_name, prefixed_attribute_name, attribute_name) }, if: :"#{attribute_name}_changed?"
+          before_validation -> { write_denormalized_attribute_to_unpersisted_record(association_name, prefixed_attribute_name, attribute_name) }, if: :"#{attribute_name}_changed?", on: :create
+          before_create     -> { write_denormalized_attribute_to_persisted_record(association_name, prefixed_attribute_name, attribute_name) },   if: :"#{attribute_name}_changed?"
+          before_update     -> { write_denormalized_attribute_to_persisted_record(association_name, prefixed_attribute_name, attribute_name) },   if: :"#{attribute_name}_changed?"
         end
       else
         raise ArgumentError, "invalid :to association: #{to.inspect}"
       end
+    end
+  end
+
+  # FIXME(ezekg) Move this out into a separate module so that we don't pollute the model.
+  included do
+    private
+
+    def write_denormalized_attribute_to_unpersisted_relation(target_association_name, target_attribute_name, source_attribute_name)
+      relation = send(target_association_name)
+
+      relation.each do |record|
+        record.write_attribute(target_attribute_name, read_attribute(source_attribute_name))
+      end
+    end
+
+    def write_denormalized_attribute_to_persisted_relation(target_association_name, target_attribute_name, source_attribute_name)
+      relation = send(target_association_name)
+
+      relation.update_all(target_attribute_name => read_attribute(source_attribute_name))
+    end
+
+    def write_denormalized_attribute_to_unpersisted_record(target_association_name, target_attribute_name, source_attribute_name)
+      record = send(target_association_name)
+
+      record.write_attribute(target_attribute_name, read_attribute(source_attribute_name))
+    end
+
+    def write_denormalized_attribute_to_persisted_record(target_association_name, target_attribute_name, source_attribute_name)
+      record = send(target_association_name)
+
+      record.update(target_attribute_name => read_attribute(source_attribute_name))
+    end
+
+    def write_denormalized_attribute_from_unpersisted_record(source_association_name, source_attribute_name, target_attribute_name)
+      record = send(source_association_name)
+
+      write_attribute(target_attribute_name, record&.send(source_attribute_name))
+    end
+
+    def write_denormalized_attribute_from_persisted_record(source_association_name, source_attribute_name, target_attribute_name)
+      record = send(source_association_name)
+
+      write_attribute(target_attribute_name, record&.send(source_attribute_name))
     end
   end
 end
