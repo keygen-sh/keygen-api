6f0e0588cffa2fb42e711cb0681af9bf0130f235
Zeke Gabrielse <ezekg@yahoo.com>
2024-04-03 16:40:04 -0500

    refactor data migration to use optimized sql

--- db/migrate/20240403173726_denormalize_product_for_licenses.rb
+++ db/migrate/20240403173726_denormalize_product_for_licenses.rb
@@ -1,15 +1,72 @@
 class DenormalizeProductForLicenses < ActiveRecord::Migration[7.1]
   disable_ddl_transaction!
 
+  BATCH_SIZE = 10_000
+
   def up
-    Policy.find_each do |policy|
-      policy.licenses.in_batches.update_all(product_id: policy.product_id)
+    update_count = nil
+    batch_count  = 0
+
+    until update_count == 0
+      batch_count  += 1
+      update_count  = execute(<<~SQL, batch_count:, batch_size: BATCH_SIZE)
+        WITH batch AS (
+          SELECT
+            licenses.id AS license_id,
+            policies.product_id
+          FROM
+            licenses INNER JOIN
+            policies ON policies.id = licenses.policy_id
+          WHERE
+            licenses.product_id IS NULL
+          LIMIT
+            :batch_size
+        )
+        UPDATE
+          licenses
+        SET
+          product_id = batch.product_id
+        FROM
+          batch
+        WHERE
+          licenses.id = batch.license_id
+        /* batch=:batch_count */
+      SQL
     end
   end
 
   def down
-    Policy.find_each do |policy|
-      policy.licenses.in_batches.update_all(product_id: nil)
+    update_count = nil
+    batch_count  = 0
+
+    until update_count == 0
+      batch_count  += 1
+      update_count  = execute(<<~SQL, batch_count:, batch_size: BATCH_SIZE)
+        UPDATE
+          licenses
+        SET
+          product_id = NULL
+        WHERE
+          licenses.id IN (
+            SELECT
+              licenses.id
+            FROM
+              licenses
+            WHERE
+              licenses.product_id IS NOT NULL
+            LIMIT
+              :batch_size
+          )
+        /* batch=:batch_count */
+      SQL
     end
+  end
+
+  private
+
+  def execute(sql, **binds)
+    ActiveRecord::Base.connection.exec_update(
+      ActiveRecord::Base.sanitize_sql([sql, **binds]),
+    )
   end
 end
