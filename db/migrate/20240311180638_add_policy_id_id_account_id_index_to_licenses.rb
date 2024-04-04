ea571d66a4d276b34f665f0f3485d19f9876da08
Zeke Gabrielse <ezekg@yahoo.com>
2024-04-04 10:55:12 -0500

    remove unused indexes

--- db/migrate/20240311180638_add_policy_id_id_account_id_index_to_licenses.rb
+++
@@ -1,7 +0,0 @@
-class AddPolicyIdIdAccountIdIndexToLicenses < ActiveRecord::Migration[7.1]
-  disable_ddl_transaction!
-
-  def change
-    add_index :licenses, %i[policy_id id account_id], unique: true, algorithm: :concurrently
-  end
-end
