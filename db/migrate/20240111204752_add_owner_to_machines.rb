019d59a301bdfa7cf77e41e1b09d5b0f8a633642
Zeke Gabrielse <ezekg@yahoo.com>
2024-04-10 12:21:11 -0500

    update new machine owner index to be concurrent

--- db/migrate/20240111204752_add_owner_to_machines.rb
+++ db/migrate/20240111204752_add_owner_to_machines.rb
@@ -1,7 +1,9 @@
 class AddOwnerToMachines < ActiveRecord::Migration[7.0]
+  disable_ddl_transaction!
+
   def change
     add_column :machines, :owner_id, :uuid, null: true, if_not_exists: true
 
-    add_index :machines, :owner_id, if_not_exists: true
+    add_index :machines, :owner_id, algorithm: :concurrently, if_not_exists: true
   end
 end
