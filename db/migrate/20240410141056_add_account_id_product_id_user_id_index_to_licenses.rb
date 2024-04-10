c0fbbc0aac5701276638ced251e64617e801ec11
Zeke Gabrielse <ezekg@yahoo.com>
2024-04-10 09:26:20 -0500

    update users for product scope to use exists

---
+++ db/migrate/20240410141056_add_account_id_product_id_user_id_index_to_licenses.rb
@@ -0,0 +1,7 @@
+class AddAccountIdProductIdUserIdIndexToLicenses < ActiveRecord::Migration[7.1]
+  disable_ddl_transaction!
+
+  def change
+    add_index :licenses, %i[account_id product_id user_id], algorithm: :concurrently
+  end
+end
