8e3bfd9fec51b7338a94a09053e984f07630adea
Zeke Gabrielse <ezekg@yahoo.com>
2024-04-11 14:55:47 -0500

    temp: remove product_id not null migration

--- db/migrate/20240403215802_remove_nullable_product_id_from_licenses.rb
+++
@@ -1,9 +0,0 @@
-class RemoveNullableProductIdFromLicenses < ActiveRecord::Migration[7.1]
-  def up
-    change_column_null :licenses, :product_id, false
-  end
-
-  def down
-    change_column_null :licenses, :product_id, true
-  end
-end
