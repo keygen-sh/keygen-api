59c78fc5ca8cfedaf8932ae3f61239f1c9d04768
Zeke Gabrielse <ezekg@yahoo.com>
2024-04-12 15:05:39 -0500

    fix issue where from record could be persisted on create

--- app/models/concerns/denormalizable.rb
+++ app/models/concerns/denormalizable.rb
@@ -47,8 +47,8 @@ module Denormalizable
           raise ArgumentError, "must be a singular association: #{association_name.inspect}"
         end
 
-        after_initialize  -> { write_denormalized_attribute_from_unpersisted_record(association_name, attribute_name, prefixed_attribute_name) }, if: -> { send(:"#{reflection.foreign_key}_changed?") || send(:"#{reflection.name}_changed?") }, unless: :persisted?
-        before_validation -> { write_denormalized_attribute_from_unpersisted_record(association_name, attribute_name, prefixed_attribute_name) }, if: -> { send(:"#{reflection.foreign_key}_changed?") || send(:"#{reflection.name}_changed?") }, on: :create
+        after_initialize  -> { write_denormalized_attribute_from_schrodingers_record(association_name, attribute_name, prefixed_attribute_name) }, if: -> { send(:"#{reflection.foreign_key}_changed?") || send(:"#{reflection.name}_changed?") }, unless: :persisted?
+        before_validation -> { write_denormalized_attribute_from_schrodingers_record(association_name, attribute_name, prefixed_attribute_name) }, if: -> { send(:"#{reflection.foreign_key}_changed?") || send(:"#{reflection.name}_changed?") }, on: :create
         before_update     -> { write_denormalized_attribute_from_persisted_record(association_name, attribute_name, prefixed_attribute_name) },   if: -> { send(:"#{reflection.foreign_key}_changed?") || send(:"#{reflection.name}_changed?") }
 
         # Make sure validation fails if our denormalized column is modified directly
@@ -88,7 +88,6 @@ module Denormalizable
   end
 
   # FIXME(ezekg) Move this out into a separate module so that we don't pollute the model.
-  # FIXME(ezekg) These could probably figure out persistence automatically.
   included do
     private
 
@@ -149,6 +148,16 @@ module Denormalizable
       record = send(source_association_name)
 
       write_attribute(target_attribute_name, record&.read_attribute(source_attribute_name))
+    end
+
+    def write_denormalized_attribute_from_schrodingers_record(source_association_name, ...)
+      record = send(source_association_name)
+
+      if record&.persisted?
+        write_denormalized_attribute_from_persisted_record(source_association_name, ...)
+      else
+        write_denormalized_attribute_from_unpersisted_record(source_association_name, ...)
+      end
     end
 
     def validate_denormalized_attribute_from_persisted_record(source_association_name, source_attribute_name, target_attribute_name)
