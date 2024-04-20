f89ec95afc1ab015d3bdb53ce6de932961a494fa
Zeke Gabrielse <ezekg@yahoo.com>
2024-04-20 01:32:21 -0500

    add verbose_migrations

--- db/migrate/20240420053552_validate_account_id_not_null_constraint_for_licenses.rb
+++ db/migrate/20240420053552_validate_account_id_not_null_constraint_for_licenses.rb
@@ -1,9 +1,7 @@
 class ValidateAccountIdNotNullConstraintForLicenses < ActiveRecord::Migration[7.1]
-  def up
-    log_level_was, ActiveRecord::Base.logger.level = ActiveRecord::Base.logger.level, Logger::DEBUG
+  verbose!
 
+  def up
     validate_check_constraint :licenses, name: 'licenses_account_id_not_null'
-  ensure
-    ActiveRecord::Base.logger.level = log_level_was
   end
 end
