60b9202830d21a209c689bedfe302af095c5e7b1
Zeke Gabrielse <ezekg@yahoo.com>
2024-04-20 00:49:06 -0500

    add start after to strong migrations

--- config/initializers/strong_migrations.rb
+++ config/initializers/strong_migrations.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 StrongMigrations.tap do |config|
+  # migrations created prior to installing the gem are safe
+  config.start_after = 20240403173726
+
+  # timeouts
   config.lock_timeout      = 10.seconds
   config.statement_timeout = 1.hour
 end
