0fe76e4539ee5c25591843c037691ca8571907f3
Zeke Gabrielse <ezekg@yahoo.com>
2024-04-19 23:38:38 -0500

    update licenses.product_id to be non-null

---
+++ db/migrate/20240420043710_change_product_id_not_null_for_licenses.rb
@@ -0,0 +1,10 @@
+class ChangeProductIdNotNullForLicenses < ActiveRecord::Migration[7.1]
+  def up
+    log_level_was, ActiveRecord::Base.logger.level = ActiveRecord::Base.logger.level, Logger::DEBUG
+
+    change_column_null :licenses, :product_id, false
+    remove_check_constraint :licenses, name: 'licenses_product_id_not_null'
+  ensure
+    ActiveRecord::Base.logger.level = log_level_was
+  end
+end
