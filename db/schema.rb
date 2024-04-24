289d3d4bc6470384e244ab29b6842e14fe63cb60
Zeke Gabrielse <ezekg@yahoo.com>
2024-04-23 21:21:06 -0500

    update licenses.policy_id to be non-null

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_20_053607) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_24_021336) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
   enable_extension "pg_stat_statements"
@@ -201,7 +201,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_20_053607) do
     t.datetime "updated_at", precision: nil, null: false
     t.jsonb "metadata"
     t.uuid "user_id"
-    t.uuid "policy_id"
+    t.uuid "policy_id", null: false
     t.uuid "account_id", null: false
     t.boolean "suspended", default: false
     t.datetime "last_check_in_at", precision: nil
