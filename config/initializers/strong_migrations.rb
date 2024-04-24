d64eed03a859fb9361d30cadd9fc6f4ad7f081c6
Zeke Gabrielse <ezekg@yahoo.com>
2024-04-23 21:21:14 -0500

    fix strong migrations timestamp format

--- config/initializers/strong_migrations.rb
+++ config/initializers/strong_migrations.rb
@@ -2,7 +2,7 @@
 
 StrongMigrations.tap do |config|
   # migrations created prior to installing the gem are safe
-  config.start_after = 20240403173726
+  config.start_after = 2024_04_03_173726
 
   # timeouts
   config.lock_timeout      = 10.seconds
