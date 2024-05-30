7bd4d2e219ea9f3f7515b18344ac0044d60dde95
Zeke Gabrielse <ezekg@yahoo.com>
2024-05-30 11:50:29 -0500

    update statement_timeout to not use a transaction

--- lib/statement_timeout.rb
+++ lib/statement_timeout.rb
@@ -1,22 +1,41 @@
 # frozen_string_literal: true
 
 module StatementTimeout
+  module AbstractAdapterExtension
+    def supports_statement_timeout? = false
+    def statement_timeout           = raise NotImplementedError
+    def statement_timeout=(timeout)
+      raise NotImplementedError
+    end
+  end
+
+  module PostgreSQLAdapterExtension
+    def supports_statement_timeout? = true
+    def statement_timeout           = @statement_timeout ||= query_value("SHOW statement_timeout")
+    def statement_timeout=(timeout)
+      @statement_timeout = nil
+
+      internal_exec_query("SET statement_timeout = #{quote(timeout)}")
+    end
+  end
+
   module QueryMethodsExtension
     def statement_timeout(timeout)
-      t = if timeout in ActiveSupport::Duration
-            timeout.in_milliseconds
-          else
-            timeout
-          end
+      timeout = if timeout in ActiveSupport::Duration
+                  timeout.in_milliseconds
+                else
+                  timeout
+                end
 
-      spawn.transaction do
-        conn = respond_to?(:lease_connection) ? lease_connection : connection
+      connection_pool.with_connection do |connection|
+        raise ActiveRecord::AdapterError, "statement_timeout is not supported for the #{connection.class.inspect} adapter" unless
+          connection.supports_statement_timeout?
 
-        conn.execute(
-          sanitize_sql(['SET LOCAL statement_timeout = :t', t:]),
-        )
+        statement_timeout_was, connection.statement_timeout = connection.statement_timeout, timeout
 
-        yield conn
+        yield connection
+      ensure
+        connection.statement_timeout = statement_timeout_was
       end
     end
   end
@@ -26,6 +45,8 @@ module StatementTimeout
   end
 
   ActiveSupport.on_load :active_record do
+    ActiveRecord::ConnectionAdapters::AbstractAdapter.prepend(AbstractAdapterExtension)
+    ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.prepend(PostgreSQLAdapterExtension)
     ActiveRecord::QueryMethods.prepend(QueryMethodsExtension)
     ActiveRecord::Querying.prepend(QueryingExtension)
   end
