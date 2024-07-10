09c94264c7f4706194fe7538fa444188786fefaa
Zeke Gabrielse <ezekg@yahoo.com>
2024-07-10 12:11:31 -0500

    fix connection errors on test setup

--- config/initializers/permissions.rb
+++ config/initializers/permissions.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
-Rails.configuration.after_initialize do
+Rails.configuration.to_prepare do
   ActiveRecord::Base.logger.silence do
     PERMISSIONS           = Permission.all
     PERMISSIONS_BY_ID     = PERMISSIONS.index_by(&:id)
     PERMISSIONS_BY_ACTION = PERMISSIONS.index_by(&:action)
+  rescue ActiveRecord::NoDatabaseError
+    # database may not be created yet, so we swallow this to allow
+    # it to be created, e.g. during test setup.
   end
 end
