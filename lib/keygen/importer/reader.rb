a662e883dea00f5567ba96218a2c804c083cc912
Zeke Gabrielse <ezekg@yahoo.com>
2024-07-10 22:06:23 -0500

    refactor import readers

--- lib/keygen/importer/reader.rb
+++ lib/keygen/importer/reader.rb
@@ -5,8 +5,6 @@ module Keygen
     class Reader
       def initialize(io) = @io = io
       def read(n)        = @io.read(n)
-      def read_version   = read(1).unpack1('C') # first byte is the version
-      def read_chunk     = raise NotImplementedError
     end
   end
 end
