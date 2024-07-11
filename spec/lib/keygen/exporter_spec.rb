eb02ee72946f0491d24d586f540b09cc4f0eef6f
Zeke Gabrielse <ezekg@yahoo.com>
2024-07-11 11:58:56 -0500

    add tests for export/import errs

--- spec/lib/keygen/exporter_spec.rb
+++ spec/lib/keygen/exporter_spec.rb
@@ -8,14 +8,24 @@ require_dependency Rails.root / 'lib' / 'keygen'
 describe Keygen::Exporter do
   let(:account) { create(:account) }
 
-  before do
-    create_list(:user, 5, account:)
-    create_list(:license, 5, account:)
-    create_list(:machine, 5, account:)
+  it 'should not raise for valid version 1' do
+    expect { Keygen::Exporter.export(account, to: StringIO.new, version: 1) }
+      .to_not raise_error
+  end
+
+  it 'should raise for invalid version' do
+    expect { Keygen::Exporter.export(account, to: StringIO.new, version: 42) }
+      .to raise_error Keygen::Exporter::UnsupportedVersionError
   end
 
   context 'with encryption' do
     let(:secret_key) { SecureRandom.hex }
+
+    before do
+      create_list(:user, 5, account:)
+      create_list(:license, 5, account:)
+      create_list(:machine, 5, account:)
+    end
 
     it 'should export' do
       export = Keygen::Exporter.export(account, to: StringIO.new, secret_key:)
@@ -38,6 +48,12 @@ describe Keygen::Exporter do
   end
 
   context 'without encryption' do
+    before do
+      create_list(:user, 5, account:)
+      create_list(:license, 5, account:)
+      create_list(:machine, 5, account:)
+    end
+
     it 'should export' do
       export = Keygen::Exporter.export(account, to: StringIO.new)
 
