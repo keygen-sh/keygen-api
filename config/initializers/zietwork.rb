29a9cb8afdb0bd6f1d16a82fd8f0af00654f32c9
Zeke Gabrielse <ezekg@yahoo.com>
2024-07-12 10:31:50 -0500

    fix autoloading for digest io

--- config/initializers/zietwork.rb
+++ config/initializers/zietwork.rb
@@ -3,6 +3,7 @@
 Rails.autoloaders.each do |autoloader|
   # FIXME(ezekg) Should we rename these to follow conventions?
   autoloader.inflector.inflect(
+    "digest_io" => "DigestIO",
     "jsonapi" => "JSONAPI",
     "ee" => "EE",
   )
