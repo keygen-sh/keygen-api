e07290a96a252ad67d1896c80248f590f6e3dc59
Zeke Gabrielse <ezekg@yahoo.com>
2024-07-29 15:49:56 -0500

    add limits to null plan

--- app/models/null_plan.rb
+++ app/models/null_plan.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
 class NullPlan < NullObject
-  def name  = 'Null'
-  def free? = false
-  def paid? = true
-  def ent?  = true
+  def name         = 'Null'
+  def price        = 0
+  def max_products = nil
+  def max_policies = nil
+  def max_licenses = nil
+  def max_users    = nil
+  def max_admins   = nil
+  def max_reqs     = nil
+  def free?        = false
+  def paid?        = true
+  def ent?         = true
 end
