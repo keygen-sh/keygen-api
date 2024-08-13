02ef19eeafbc3a171c1e3ca4e8039a61d582af02
Zeke Gabrielse <ezekg@yahoo.com>
2024-08-12 20:40:28 -0500

    add excluded exceptions env var for sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -5,6 +5,7 @@ unless ENV.key?('NO_SENTRY')
     config.dsn                  = ENV.fetch('SENTRY_DSN')                  { nil }
     config.traces_sample_rate   = ENV.fetch('SENTRY_TRACES_SAMPLE_RATE')   { 0.0 }.to_f
     config.profiles_sample_rate = ENV.fetch('SENTRY_PROFILES_SAMPLE_RATE') { 0.0 }.to_f
+    config.excluded_exceptions += ENV.fetch('SENTRY_EXCLUDED_EXCEPTIONS')  { ''  }.split(',')
     config.environment          = Rails.env
   end
 end
