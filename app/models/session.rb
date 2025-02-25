e7758bb5b28e29b5e988ac0db1cb5d5376080a97
Zeke Gabrielse <ezekg@yahoo.com>
2025-02-25 16:47:49 -0600

    add validations to session

--- app/models/session.rb
+++ app/models/session.rb
@@ -17,5 +17,23 @@ class Session < ApplicationRecord
   denormalizes :bearer_type, :bearer_id,
     from: :token
 
+  validates :account, presence: true
+  validates :token,
+    presence: { message: 'must exist' },
+    scope: { by: :account_id }
+  validates :bearer,
+    presence: { message: 'must exist' },
+    scope: { by: :account_id }
+
+  # assert that bearer matches the token's bearer
+  validate on: %i[create update] do
+    next unless
+      token_id_changed? || bearer_id_changed?
+
+    unless token.nil? || bearer_type == token.bearer_type && bearer_id == token.bearer_id
+      errors.add :bearer, :not_allowed, message: 'bearer must match token bearer'
+    end
+  end
+
   def expired? = expiry < Time.current || created_at < MAX_AGE.ago
 end
