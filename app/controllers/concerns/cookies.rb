4d8f33a329f20a5ff7e8c89783461f0df4fd3809
Zeke Gabrielse <ezekg@yahoo.com>
2025-02-28 10:53:00 -0600

    update cookie origin to use whitelist

--- app/controllers/concerns/cookies.rb
+++ app/controllers/concerns/cookies.rb
@@ -8,7 +8,7 @@ module Cookies
   def set_session_id_cookie(session)
     return unless session in Session # lol nice
     return unless
-      request.origin.nil? || request.origin.ends_with?(Keygen::DOMAIN)
+      request.origin.nil? || request.origin.ends_with?(Keygen::Portal::HOST)
 
     cookies.encrypted[:session_id] = {
       value: session.id,
@@ -23,7 +23,7 @@ module Cookies
 
   def reset_session_id_cookie
     return unless
-      request.origin.nil? || request.origin.ends_with?(Keygen::DOMAIN)
+      request.origin.nil? || request.origin.ends_with?(Keygen::Portal::HOST)
 
     cookies.delete(:session_id,
       domain: Keygen::DOMAIN,
