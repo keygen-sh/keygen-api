d5c89726a812144e822e647b6f2a211160112dfc
Zeke Gabrielse <ezekg@yahoo.com>
2025-05-05 16:07:08 -0500

    add tests for invalid settings

--- app/models/account_setting.rb
+++ app/models/account_setting.rb
@@ -16,17 +16,13 @@ class AccountSetting < ApplicationRecord
   validates :key, presence: true, uniqueness: { scope: :account_id }, inclusion: { in: VALID_KEYS, message: "must be one of: #{VALID_KEYS.join(', ')}" }
   validate on: %i[create update] do
     case self
-    in key: :default_license_permissions | 'default_license_permissions', value: [*]
-      permissions = value.uniq
-
+    in key: :default_license_permissions | 'default_license_permissions', value: [*] => permissions
       unless (permissions & License.allowed_permissions).size == permissions.size
-        errors.add :value, :invalid, message: 'must be an array of valid license permissions'
+        errors.add :value, :not_allowed, message: 'must be an array of valid license permissions'
       end
-    in key: :default_user_permissions | 'default_user_permissions', value: [*]
-      permissions = value.uniq
-
+    in key: :default_user_permissions | 'default_user_permissions', value: [*] => permissions
       unless (permissions & User.allowed_permissions).size == permissions.size
-        errors.add :value, :invalid, message: 'must be an array of valid user permissions'
+        errors.add :value, :not_allowed, message: 'must be an array of valid user permissions'
       end
     else
       errors.add :value, :not_allowed, message: 'must be a valid setting'
