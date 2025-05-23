ddd1471a468530b0e28b29ed376973b0b2747456
Zeke Gabrielse <ezekg@yahoo.com>
2025-05-23 07:00:35 -0500

    update slack invites to send during work hours

--- app/models/concerns/slackable.rb
+++ app/models/concerns/slackable.rb
@@ -3,6 +3,8 @@
 module Slackable
   extend ActiveSupport::Concern
 
+  SLACK_INVITE_TZ = ENV.fetch('SLACK_INVITE_TZ') { 'Central Time (US & Canada)' }
+
   included do
     attr_accessor :skip_slack_invite
 
@@ -13,9 +15,13 @@ module Slackable
 
   def skip_slack_invite? = !!skip_slack_invite
   def send_slack_invite
-    SlackInviteWorker.perform_in(
-      rand(1..4).hours, # random delay
-      id,
-    )
+    now = Time.now.in_time_zone(SLACK_INVITE_TZ)
+
+    t  = now.beginning_of_day
+    t  = t.next_weekday if t.on_weekend? # only weekdays
+    t += rand(9..17.0).hours             # only 9am-5pm
+    t  = t.next_weekday if t.past?       # only future
+
+    SlackInviteWorker.perform_at(t, id)
   end
 end
