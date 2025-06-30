afece53b3a0f6ce3525ce69d0d52b3506e977f06
Zeke Gabrielse <ezekg@yahoo.com>
2025-06-30 08:26:34 -0500

    remove deprecated log filter

--- app/controllers/api/v1/request_logs_controller.rb
+++ app/controllers/api/v1/request_logs_controller.rb
@@ -5,7 +5,6 @@ module Api::V1
     has_scope(:date, type: :hash, using: [:start, :end], only: :index)
     has_scope(:requestor, type: :any) { |c, s, v| s.search_requestor(v) }
     has_scope(:resource, type: :any) { |c, s, v| s.search_resource(v) }
-    has_scope(:request) { |c, s, v| s.search_request_id(v) }
     has_scope(:ip) { |c, s, v| s.search_ip(v) }
     has_scope(:method) { |c, s, v| s.search_method(v) }
     has_scope(:url) { |c, s, v| s.search_url(v) }
