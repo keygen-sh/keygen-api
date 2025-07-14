92081e51597fda9c5c50271d1138dea03ee2142a
Zeke Gabrielse <ezekg@yahoo.com>
2025-07-14 16:03:52 -0500

    add env var for health check status code

--- app/controllers/api/v1/health_controller.rb
+++ app/controllers/api/v1/health_controller.rb
@@ -4,16 +4,20 @@ require 'sidekiq/api'
 
 module Api::V1
   class HealthController < Api::V1::BaseController
+    # some platforms like GCP are very specific with health check status codes,
+    # so they may require a 200 even though 204 is more appropriate.
+    HTTP_HEALTH_CHECK_STATUS_CODE = ENV.fetch('HTTP_HEALTH_CHECK_STATUS_CODE', 204).to_i
+
     skip_verify_authorized
 
     def general_health
-      render status: 204
+      render status: HTTP_HEALTH_CHECK_STATUS_CODE
     end
 
     def webhook_health
       process_ok = Sidekiq::ProcessSet.new.size > 0
       latency_ok = Sidekiq::Queue.new.latency < SIDEKIQ_MAX_QUEUE_LATENCY
-      status = process_ok && latency_ok ? 204 : 500
+      status = process_ok && latency_ok ? HTTP_HEALTH_CHECK_STATUS_CODE : 500
 
       render status: status
     end
