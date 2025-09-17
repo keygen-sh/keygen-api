2203021b3b6103b271fb5f5225324f02ef69144b
Zeke Gabrielse <ezekg@yahoo.com>
2025-09-17 08:40:42 -0500

    add seeds for ecdsa keypairs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_09_12_151308) do
+ActiveRecord::Schema[7.2].define(version: 2025_09_12_151446) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
   enable_extension "pg_stat_statements"
