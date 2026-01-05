223493c735187bb30df8e275126a55f4ccf5deb8
Zeke Gabrielse <ezekg@yahoo.com>
2026-01-05 16:17:55 -0600

    fix delete resurrection for out-of-order jobs

--- lib/dual_writes/strategy/clickhouse.rb
+++ lib/dual_writes/strategy/clickhouse.rb
@@ -15,21 +15,21 @@ module DualWrites
     #
     class Clickhouse < Strategy
       def create(attributes, performed_at:)
-        replica_class.insert!(attributes.merge('is_deleted' => 0))
+        replica_class.insert!(with_metadata(attributes, is_deleted: 0, ver: performed_at))
       end
 
       def update(attributes, performed_at:)
-        replica_class.insert!(attributes.merge('is_deleted' => 0))
+        replica_class.insert!(with_metadata(attributes, is_deleted: 0, ver: performed_at))
       end
 
       def destroy(attributes, performed_at:)
         # Insert a tombstone row with is_deleted = 1
         # ReplacingMergeTree(ver, is_deleted) will handle cleanup
-        replica_class.insert!(attributes.merge('is_deleted' => 1))
+        replica_class.insert!(with_metadata(attributes, is_deleted: 1, ver: performed_at))
       end
 
       def insert_all(attributes, performed_at:)
-        attributes = attributes.map { it.merge('is_deleted' => 0) }
+        attributes = attributes.map { with_metadata(it, is_deleted: 0, ver: performed_at) }
 
         replica_class.insert_all!(attributes)
       end
@@ -37,6 +37,15 @@ module DualWrites
       def upsert_all(attributes, performed_at:)
         # For ClickHouse, upsert becomes insert (ReplacingMergeTree handles dedup)
         insert_all(attributes, performed_at:)
+      end
+
+      private
+
+      def with_metadata(attributes, is_deleted:, ver:)
+        attrs = attributes.dup
+        attrs['is_deleted'] = is_deleted if replica_class.column_names.include?('is_deleted')
+        attrs['ver'] = ver if replica_class.column_names.include?('ver')
+        attrs
       end
     end
   end
