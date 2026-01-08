a67695bc5e1cc8fe2490dd151957aac614c8aacf
Zeke Gabrielse <ezekg@yahoo.com>
2026-01-08 14:44:32 -0600

    add fixmes to ryow client identification

--- config/initializers/ryow.rb
+++ config/initializers/ryow.rb
@@ -8,12 +8,16 @@ ReadYourOwnWrites.configure do |config|
     %r{/search\z},
   ]
 
+  # NB(ezekg) This is run BEFORE the Rails app via Rails' DatabaseSelector
+  #           middleware, so things like route params are NOT available.
+  #
   # extract tenant (account) from request path for client identifier
   config.client_identifier = -> request {
-    account_id    = request.path[/^\/v\d+\/accounts\/([^\/]+)\//, 1]
-    session_authn = request.headers['Cookie']
-    token_authn   = request.headers['Authorization']
+    account_id    = request.path[/^\/v\d+\/accounts\/([^\/]+)\//, 1] # FIXME(ezekg) use account ID
+    session_authn = request.headers['Cookie']                        # FIXME(ezekg) use session ID
+    token_authn   = request.headers['Authorization']                 # FIXME(ezekg) use token ID
 
+    # FIXME(ezekg) use bearer ID?
     [account_id, session_authn, token_authn, request.remote_ip]
   }
 end
