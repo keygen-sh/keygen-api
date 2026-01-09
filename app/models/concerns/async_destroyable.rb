38b944ea1ca5b9b1e96a140f86181edbd3d5d5cc
Zeke Gabrielse <ezekg@yahoo.com>
2026-01-09 09:05:53 -0600

    add async destroyable and mutable concerns

---
+++ app/models/concerns/async_destroyable.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module AsyncDestroyable
+  extend ActiveSupport::Concern
+
+  def destroy_async
+    DestroyAsyncJob.perform_later(
+      class_name: self.class.name,
+      id:,
+    )
+  end
+
+  class DestroyAsyncJob < ActiveJob::Base
+    queue_as { ActiveRecord.queues[:default] }
+
+    discard_on ActiveJob::DeserializationError
+
+    def perform(class_name:, id:)
+      klass  = class_name.constantize
+      record = klass.find_by(klass.primary_key => id)
+      return if
+        record.nil?
+
+      record.destroy!
+    end
+  end
+end
