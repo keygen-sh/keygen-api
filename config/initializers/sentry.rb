50ebd0237efd8963e81c6356fcfde8f34806d582
Zeke Gabrielse <ezekg@yahoo.com>
2026-02-10 16:44:32 -0600

    stfu sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -7,5 +7,7 @@ unless ENV.key?('NO_SENTRY')
     config.profiles_sample_rate = ENV.fetch('SENTRY_PROFILES_SAMPLE_RATE') { 0.0 }.to_f
     config.excluded_exceptions += ENV.fetch('SENTRY_EXCLUDED_EXCEPTIONS')  { ''  }.split(',')
     config.environment          = Rails.env
+    config.sdk_logger           = Sentry::Logger.new(STDOUT)
+    config.sdk_logger.level     = Logger::WARN
   end
 end
