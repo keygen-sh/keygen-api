80554fc740bb36fa360c47fefb37c7350182332b
Zeke Gabrielse <ezekg@yahoo.com>
2026-02-16 12:20:09 -0600

    fix formatting

--- config/routes.rb
+++ config/routes.rb
@@ -488,10 +488,10 @@ Rails.application.routes.draw do
   concern :priv do
     scope constraints: MimeTypeConstraint.new(:jsonapi, :json, :binary, raise_on_no_match: true), defaults: { format: :json } do
       namespace :analytics do
-        get 'activities/:activity_id', to: 'activities#show', as: :activity, constraints: { activity_id: /.*/ }
-        get 'stats/:stat_id', to: 'stats#show', as: :stat
+        get 'activities/:activity_id',      to: 'activities#show',   as: :activity,    constraints: { activity_id: /.*/ }
         get 'leaderboards/:leaderboard_id', to: 'leaderboards#show', as: :leaderboard
-        get 'heatmaps/:heatmap_id', to: 'heatmaps#show', as: :heatmap
+        get 'heatmaps/:heatmap_id',         to: 'heatmaps#show',     as: :heatmap
+        get 'stats/:stat_id',               to: 'stats#show',        as: :stat
       end
     end
   end
