279b2d22d6e5094946095b9f57f830f6cdaee26e
Zeke Gabrielse <ezekg@yahoo.com>
2026-02-17 16:56:50 -0600

    refactor leaderboard sql

--- app/models/analytics/leaderboard/counters/urls.rb
+++ app/models/analytics/leaderboard/counters/urls.rb
@@ -4,44 +4,39 @@ module Analytics
   class Leaderboard
     module Counters
       class Urls
-        QUERY = <<~SQL.squish
-          SELECT
-            concat(method, ' ', url) AS identifier,
-            count(*) AS count
-          FROM request_logs
-          WHERE account_id = :account_id
-            AND environment_id %{environment_clause}
-            AND created_date BETWEEN :start_date AND :end_date
-            AND is_deleted = 0
-            AND url IS NOT NULL
-            AND method IS NOT NULL
-          GROUP BY identifier
-          ORDER BY count DESC
-          LIMIT :limit
-        SQL
-
         def initialize(account:, environment:)
           @account     = account
           @environment = environment
         end
 
         def count(start_date:, end_date:, limit:)
-          environment_clause = environment.nil? ? 'IS NULL' : '= :environment_id'
-
           binds = { account_id: account.id, environment_id: environment&.id, start_date:, end_date:, limit: }.compact
-          query = format(QUERY, environment_clause:)
-          res   = exec_sql([query, binds])
+          res   = exec_sql(<<~SQL.squish, **binds)
+            SELECT
+              concat(method, ' ', url) AS identifier,
+              count(*)                 AS count
+            FROM request_logs
+            WHERE account_id = :account_id
+              AND environment_id #{environment.nil? ? 'IS NULL' : '= :environment_id'}
+              AND created_date BETWEEN :start_date AND :end_date
+              AND is_deleted = 0
+              AND url IS NOT NULL
+              AND method IS NOT NULL
+            GROUP BY identifier
+            ORDER BY count DESC
+            LIMIT :limit
+          SQL
 
-          res['data']
+          res.rows
         end
 
         private
 
         attr_reader :account, :environment
 
-        def exec_sql(...)
-          RequestLog::Clickhouse.connection.execute(
-            RequestLog::Clickhouse.sanitize_sql(...),
+        def exec_sql(sql, **binds)
+          RequestLog::Clickhouse.connection.exec_query(
+            RequestLog::Clickhouse.sanitize_sql([sql, binds]),
           )
         end
       end
