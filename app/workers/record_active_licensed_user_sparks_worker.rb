b99be6450c06309fa8371687872ddca35758ffa5
Zeke Gabrielse <ezekg@yahoo.com>
2026-02-26 17:34:45 -0600

    update spark recording for paid accounts only

--- app/workers/record_active_licensed_user_sparks_worker.rb
+++ app/workers/record_active_licensed_user_sparks_worker.rb
@@ -5,7 +5,7 @@ class RecordActiveLicensedUserSparksWorker < BaseWorker
                   cronitor_enabled: true
 
   def perform
-    Account.unordered.active.find_each do |account|
+    Account.unordered.paid.find_each do |account|
       jitter = rand(0..30.minutes) # prevent a thundering herd effect
 
       RecordActiveLicensedUserSparkWorker.perform_in(jitter, account.id)
--- app/workers/record_machine_sparks_worker.rb
+++ app/workers/record_machine_sparks_worker.rb
@@ -5,7 +5,7 @@ class RecordMachineSparksWorker < BaseWorker
                   cronitor_enabled: true
 
   def perform
-    Account.unordered.active.find_each do |account|
+    Account.unordered.paid.find_each do |account|
       jitter = rand(0..30.minutes)
 
       RecordMachineSparkWorker.perform_in(jitter, account.id)
