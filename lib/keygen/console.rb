a50ef0c8c419b209a5661d813451ee9dce2598f6
Zeke Gabrielse <ezekg@yahoo.com>
2026-04-08 09:34:31 -0500

    add dev domain to cloud mode

--- lib/keygen/console.rb
+++ lib/keygen/console.rb
@@ -39,7 +39,7 @@ module Keygen
       puts " Rails: #{Rails.version} #{Rails.env} (RubyGems #{Gem::VERSION}, Rack #{Rack.release})"
       puts " Keygen: #{Keygen.version} (revision #{Keygen.revision&.first(7) || 'N/A'})"
       puts "   Mode: #{Keygen.mode}"
-      puts "   Edition: #{Keygen.edition}"
+      puts "   Edition: #{Keygen.edition}" + (Keygen.cloud? ? ' (Cloud)' : '')
       Keygen.ee do |key, lic|
         puts "   License: #{key.name || 'Unnamed'} (#{key.id})"
         puts "   Entitlements: #{key.entitlements.join(', ')}"
