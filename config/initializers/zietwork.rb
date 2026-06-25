4698523f7a13c83afd8a7a3892ae6223f00ca5f9
Zeke Gabrielse <ezekg@yahoo.com>
2026-06-25 09:55:52 -0500

    disable eager loading of clickhouse models in ce

--- config/initializers/zietwork.rb
+++ config/initializers/zietwork.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
 Rails.autoloaders.each do |autoloader|
-  # FIXME(ezekg) Should we rename these to follow conventions?
+  # NB(ezekg) avoid eager loading analytics models in environments that don't have clickhouse
+  unless Keygen.database.clickhouse_available? && Keygen.database.clickhouse_enabled?
+    autoloader.do_not_eager_load(
+      Rails.root.join('app/models/clickhouse_record.rb'),
+      Rails.root.join('app/models/analytics/'),
+      *Rails.root.glob('app/models/*_spark.rb'),
+      *Rails.root.glob('app/models/*_log.rb'),
+    )
+  end
+
+  # FIXME(ezekg) should we rename these to follow conventions?
   autoloader.inflector.inflect(
     'enumerator_io' => 'EnumeratorIO',
     'digest_io' => 'DigestIO',
